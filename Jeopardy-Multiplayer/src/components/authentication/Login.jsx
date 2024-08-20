96e810b138d46bff82a69c0e636e040db4447614
Krasen Boychev <krasenboychev11@gmail.com>
2024-08-20 16:13:27 +0100

    formik component added to Login and Register

--- Jeopardy-Multiplayer/src/components/authentication/Login.jsx
+++ Jeopardy-Multiplayer/src/components/authentication/Login.jsx
@@ -1,44 +1,66 @@
-import { Formik, Form, Field, ErrorMessage } from 'formik';
+import { Formik, Form, Field, ErrorMessage } from "formik";
 
-import './authentication.css'
+import "./authentication.css";
 
 export default function Login() {
+  return (
+    <div className="authentication">
+      <h1>Login</h1>
+      <Formik
+        initialValues={{ email: "", password: "" }}
+        validate={(values) => {
+          const errors = {};
+          if (!values.email) {
+            errors.email = "Email is required";
+          } else if (
+            !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+          ) {
+            errors.email = "Invalid email address";
+          }
 
-    return (
-        <div>
-            <h1>Login</h1>
-            <Formik
-                initialValues={{ email: '', password: '' }}
-                validate={values => {
-                    const errors = {};
-                    if (!values.email) {
-                        errors.email = 'Required';
-                    } else if (
-                        !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-                    ) {
-                        errors.email = 'Invalid email address';
-                    }
-                    return errors;
-                }}
-                onSubmit={(values, { setSubmitting }) => {
-                    setTimeout(() => {
-                        alert(JSON.stringify(values, null, 2));
-                        setSubmitting(false);
-                    }, 400);
-                }}
-            >
-                {({ isSubmitting }) => (
-                    <Form>
-                        <Field type="email" name="email" />
-                        <ErrorMessage name="email" component="div" />
-                        <Field type="password" name="password" />
-                        <ErrorMessage name="password" component="div" />
-                        <button type="submit" disabled={isSubmitting}>
-                            Submit
-                        </button>
-                    </Form>
-                )}
-            </Formik>
-        </div>
-    );
+          if (!values.password) {
+            errors.password = "Password is required";
+          }
+
+          return errors;
+        }}
+        onSubmit={(values, { setSubmitting }) => {
+          setTimeout(() => {
+            alert(JSON.stringify(values, null, 2));
+            setSubmitting(false);
+          }, 400);
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form className="authentication-form">
+            <Field
+              type="email"
+              name="email"
+              placeholder="Email"
+              className="authentication-input"
+            />
+            <ErrorMessage
+              name="email"
+              component="div"
+              className="authentication-error"
+            />
+            <Field
+              type="password"
+              name="password"
+              placeholder="Password"
+              className="authentication-input"
+            />
+            <ErrorMessage
+              name="password"
+              component="div"
+              className="authentication-error"
+            />
+            <button type="submit" disabled={isSubmitting}>
+              Login
+            </button>
+          </Form>
+        )}
+      </Formik>
+    </div>
+  );
 }
--- Jeopardy-Multiplayer/src/components/authentication/Register.jsx
+++ Jeopardy-Multiplayer/src/components/authentication/Register.jsx
@@ -1,46 +1,81 @@
-import { Formik, Form, Field, ErrorMessage } from 'formik';
+import { Formik, Form, Field, ErrorMessage } from "formik";
 
-import './authentication.css'
+import "./authentication.css";
 
 export default function Register() {
+  return (
+    <div className="authentication">
+      <div>
+        <h1>Register</h1>
+        <Formik
+          initialValues={{ email: "", password: "", rePass: "" }}
+          validate={(values) => {
+            const errors = {};
+            if (!values.email) {
+              errors.email = "Required";
+            } else if (
+              !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+            ) {
+              errors.email = "Invalid email address";
+            }
 
-    return (
-        <div>
-            <div>
-            <h1>Register</h1>
-            <Formik
-                initialValues={{ email: '', password: '' }}
-                validate={values => {
-                    const errors = {};
-                    if (!values.email) {
-                        errors.email = 'Required';
-                    } else if (
-                        !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-                    ) {
-                        errors.email = 'Invalid email address';
-                    }
-                    return errors;
-                }}
-                onSubmit={(values, { setSubmitting }) => {
-                    setTimeout(() => {
-                        alert(JSON.stringify(values, null, 2));
-                        setSubmitting(false);
-                    }, 400);
-                }}
-            >
-                {({ isSubmitting }) => (
-                    <Form>
-                        <Field type="email" name="email" />
-                        <ErrorMessage name="email" component="div" />
-                        <Field type="password" name="password" />
-                        <ErrorMessage name="password" component="div" />
-                        <button type="submit" disabled={isSubmitting}>
-                            Submit
-                        </button>
-                    </Form>
-                )}
-            </Formik>
-        </div>
-        </div>
-    );
+            if (!values.password) {
+              errors.password = "Password is required";
+            } else if (values.password != values.rePass) {
+              errors.rePass = "Passwords do not match";
+            }
+
+            return errors;
+          }}
+          onSubmit={(values, { setSubmitting }) => {
+            setTimeout(() => {
+              alert(JSON.stringify(values, null, 2));
+              setSubmitting(false);
+            }, 400);
+          }}
+        >
+          {({ isSubmitting }) => (
+            <Form className="authentication-form">
+              <Field
+                type="email"
+                name="email"
+                placeholder="Email"
+                className="authentication-input"
+              />
+              <ErrorMessage
+                name="email"
+                component="div"
+                className="authentication-error"
+              />
+              <Field
+                type="password"
+                name="password"
+                placeholder="Password"
+                className="authentication-input"
+              />
+              <ErrorMessage
+                name="password"
+                component="div"
+                className="authentication-error"
+              />
+              <Field
+                type="password"
+                name="rePass"
+                placeholder="Repeat Password"
+                className="authentication-input"
+              />
+              <ErrorMessage
+                name="rePass"
+                component="div"
+                className="authentication-error"
+              />
+              <button type="submit" disabled={isSubmitting}>
+                Register
+              </button>
+            </Form>
+          )}
+        </Formik>
+      </div>
+    </div>
+  );
 }
