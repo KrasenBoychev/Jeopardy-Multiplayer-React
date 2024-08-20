4a5c6d739981d379f3e57cad63ff6486b83ec832
Krasen Boychev <krasenboychev11@gmail.com>
2024-08-20 17:52:39 +0100

    Guards created and applied to App.js

--- Jeopardy-Multiplayer/src/App.jsx
+++ Jeopardy-Multiplayer/src/App.jsx
@@ -1,26 +1,48 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
 
-import './App.css'
+import "./App.css";
 
-import Home from './components/home/Home';
-import Header from './components/header/Header';
-import Footer from './components/footer/Footer';
-import Login from './components/authentication/Login';
-import Register from './components/authentication/Register';
+import { AuthContextProvider } from "./contexts/AuthContext";
+
+import PrivateGuard from "./common/PrivateGuard";
+import PublicGuard from "./common/PublicGuard";
+
+import Home from "./components/home/Home";
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+import Login from "./components/authentication/Login";
+import Register from "./components/authentication/Register";
+import Logout from "./components/authentication/Logout";
+import NotFound from "./components/notFound/NotFound";
 
 function App() {
   return (
     <>
-      <Header />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/register' element={<Register />} />
-      </Routes>
+      <Toaster />
 
-      <Footer />
+      <AuthContextProvider>
+        <Header />
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+
+          <Route element={<PublicGuard />}>
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Route>
+
+          <Route element={<PrivateGuard />}>
+            <Route path="/logout" element={<Logout />} />
+          </Route>
+
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+
+        <Footer />
+      </AuthContextProvider>
     </>
   );
 }
 
-export default App
+export default App;
