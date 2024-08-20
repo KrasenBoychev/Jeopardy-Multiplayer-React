17934c7f1d837202b21d675db0e5d23f301af29a
Krasen Boychev <krasenboychev11@gmail.com>
2024-08-20 12:54:36 +0100

    Created routing in App.js

--- Jeopardy-Multiplayer/src/App.jsx
+++ Jeopardy-Multiplayer/src/App.jsx
@@ -1,10 +1,22 @@
 import { useState } from 'react'
+import { Routes, Route } from 'react-router-dom';
+
 import './App.css'
+
+import Home from './components/home/Home';
+import Header from './components/header/Header';
+import Footer from './components/footer/Footer';
 
 function App() {
   return (
     <>
-      
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+
+      </Routes>
+
+      <Footer />
     </>
   );
 }
