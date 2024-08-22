9ad5f9e53a5860ce22aa7e58c8d081aecd8539d7
Krasen Boychev <krasenboychev11@gmail.com>
2024-08-22 18:22:16 +0100

    Game.jsx - sets two players to play together

--- client/src/components/game/game/Game.jsx
+++ client/src/components/game/game/Game.jsx
@@ -1,4 +1,18 @@
-export default function Game() {
+import { useState } from "react";
+
+export default function Game({ channel }) {
+  const [playersJoined, setPlayersJoined] = useState(
+    channel.state.watcher_count === 2
+  );
+
+  channel.on('user.watching.start', (event) => {
+    setPlayersJoined(event.watcher_count === 2);
+  });
+
+  if (!playersJoined) {
+    return <div>Waiting for other player to join...</div>;
+  }
+  
   return (
     <div>Game</div>
   );
