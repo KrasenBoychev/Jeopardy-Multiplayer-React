47dbb61d77e7424fdb54c1b546f3c3b8e5094247
Krasen Boychev <krasenboychev11@gmail.com>
2024-08-22 18:20:24 +0100

    game.js uses gameRouter; serverClient request created

--- server/src/config/configRoutes.js
+++ server/src/config/configRoutes.js
@@ -1,7 +1,9 @@
+const { gameRouter } = require('../controllers/game');
 const { userRouter } = require('../controllers/user');
 
 function configRoutes(app) {
   app.use('/users', userRouter);
+  app.use('/play', gameRouter);
 }
 
 module.exports = { configRoutes };
--- server/src/controllers/game.js
+++ server/src/controllers/game.js
@@ -1,8 +1,39 @@
+const { Router } = require('express');
 const { v4 } = require('uuid');
 const { StreamChat } = require('stream-chat');
 
-const api_key = 'juudbb2ng7uh';
-const api_secret =
-  'zbyzqx223mj4az4hhdpkjs59n2hjtkkskn7nv7yev4b7jpv84k85bmz78bwa8cvx';
-  
-const serverClient = StreamChat.getInstance(api_key, api_secret);
+const { api_key, api_secret } = require('../api-keys');
+
+const gameRouter = Router();
+
+gameRouter.get('/:username', async (req, res) => {
+  const serverClient = StreamChat.getInstance(api_key, api_secret);
+
+  try {
+    const username = req.params.username;
+
+    const { users } = await serverClient.queryUsers({ name: username });
+
+    let token;
+    let userIdResult;
+
+    if (users.length === 0) {
+      const userId = v4();
+      token = serverClient.createToken(userId);
+      userIdResult = userId;
+
+    } else {
+      token = serverClient.createToken(users[0].id);
+      userIdResult = users[0].id;
+    }
+
+    res.json({
+      token,
+      userId: userIdResult,
+    });
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+module.exports = { gameRouter };
