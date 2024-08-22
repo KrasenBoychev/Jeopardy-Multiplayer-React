719f6becd256b2897972235baf2e70dcb65c030e
Krasen Boychev <krasenboychev11@gmail.com>
2024-08-22 18:22:50 +0100

    Play.jsx uses usePlay hook

--- client/src/components/game/Play.jsx
+++ client/src/components/game/Play.jsx
@@ -1,33 +1,17 @@
-import { useEffect } from "react";
+
 
 import { Chat } from "stream-chat-react";
 
-import { useAuthContext } from "../../contexts/AuthContext";
-
 import JoinGame from "./joinGame/JoinGame";
+import { useAuthContext } from "../../contexts/AuthContext";
+import usePlay from "../../hooks/usePlay";
 
 export default function Play({ props }) {
-  const userData = useAuthContext();
-
   const { isAuth, setIsAuth, client } = props;
 
-  useEffect(() => {
-    (function setClient() {
-      if (userData.gameToken) {
-        const { userId, username, gameToken } = userData;
+  const { username } = useAuthContext();
 
-        client.connectUser(
-          {
-            id: userId,
-            username,
-          },
-          gameToken
-        );
-
-        setIsAuth(true);
-      }
-    })();
-  }, [userData.gameToken]);
+  usePlay(username, setIsAuth, client);
 
   return (
     <>
