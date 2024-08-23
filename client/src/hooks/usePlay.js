51390871352ddf733a034a762fec639d11760db5
Krasen Boychev <krasenboychev11@gmail.com>
2024-08-23 15:55:45 +0100

    usePlay hook - error handling added

--- client/src/hooks/usePlay.js
+++ client/src/hooks/usePlay.js
@@ -1,8 +1,12 @@
 import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 import { getGameToken } from "../../api/game-api";
+import toast from "react-hot-toast";
 
 export default function usePlay(username, setIsAuth, client) {
+  const navigate = useNavigate();
+
   useEffect(() => {
     let active = true;
     load();
@@ -12,22 +16,27 @@ export default function usePlay(username, setIsAuth, client) {
     };
 
     async function load() {
-      const { token, userId } = await getGameToken(username);
+      try {
+        const { token, userId } = await getGameToken(username);
 
-      client
-        .connectUser(
-          {
-            id: userId,
-            name: username,
-          },
-          token
-        )
-        .then((user) => {
-          setIsAuth(true);
-        });
+        client
+          .connectUser(
+            {
+              id: userId,
+              name: username,
+            },
+            token
+          )
+          .then((user) => {
+            setIsAuth(true);
+          });
 
-      if (!active) {
-        return;
+        if (!active) {
+          return;
+        }
+      } catch (error) {
+        toast.error('Can not play at this moment. Please send a message to our customer service team.');
+        navigate('/');
       }
     }
   }, []);
