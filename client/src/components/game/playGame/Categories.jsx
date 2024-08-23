469efa0a4b26262f90d9dcea6d18bff8720fd0f3
Krasen Boychev <krasenboychev11@gmail.com>
2024-08-23 20:15:28 +0100

    Created Categories.jsx

---
+++ client/src/components/game/playGame/Categories.jsx
@@ -0,0 +1,13 @@
+export default function Categories() {
+  return (
+    <div className="gameContainer">
+      <p>Categories</p>
+      <div className="categories-container">
+        <div>A</div>
+        <div>B</div>
+        <div>C</div> 
+        <div>D</div>
+      </div>
+    </div>
+  );
+}
