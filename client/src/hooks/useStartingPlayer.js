d37096140d201cf7d4f1a703ea602896067cc530
Krasen Boychev <krasenboychev11@gmail.com>
2024-08-25 10:24:54 +0100

    useStartingPlayer hook sets a first player and a second player

--- client/src/hooks/useStartingPlayer.js
+++ client/src/hooks/useStartingPlayer.js
@@ -2,7 +2,8 @@ import { useEffect, useState } from "react";
 
 export default function useStartingPlayer(channel) {
   const [counter, setCounter] = useState(3);
-  const [startingPlayer, setStartingPlayer] = useState("");
+  const [firstPlayer, setFirstPlayer] = useState("");
+  const [secondPlayer, setSecondPlayer] = useState("");
 
   const timer = setTimeout(() => {
     if (counter <= 1) {
@@ -23,12 +24,16 @@ export default function useStartingPlayer(channel) {
 
         const playersNames = [firstPlayerName, secondPlayerName];
 
-        const randomPlayer =
+        const startingPlayer =
           playersNames[Math.floor(Math.random() * playersNames.length)];
+
+        const indexOfStartingPlayer = playersNames.indexOf(startingPlayer);
+        playersNames.splice(indexOfStartingPlayer, 1)
+        const otherPlayer = playersNames[0];
 
         await channel.sendEvent({
           type: "set-first-player",
-          data: { randomPlayer },
+          data: { startingPlayer, otherPlayer },
         });
       }
     })();
@@ -36,9 +41,10 @@ export default function useStartingPlayer(channel) {
 
   channel.on((event) => {
     if (event.type == "set-first-player") {
-      setStartingPlayer(event.data.randomPlayer);
+      setFirstPlayer(event.data.startingPlayer);
+      setSecondPlayer(event.data.otherPlayer);
     }
   });
 
-  return [counter, startingPlayer];
+  return [counter, firstPlayer, secondPlayer];
 }
