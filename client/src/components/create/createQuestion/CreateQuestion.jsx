9aaa8d017aed475389251382a6248a70d342b7b5
Krasen Boychev <krasenboychev11@gmail.com>
2024-08-28 22:28:19 +0100

    CreateQuestion.jsx - used Formik library, not completed yet

---
+++ client/src/components/create/createQuestion/CreateQuestion.jsx
@@ -0,0 +1,212 @@
+import { useState } from "react";
+import { toast } from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+
+import { createQuestion } from "../../../../api/create-api";
+import TextareaField from "./chunks/TextareaField";
+
+export default function CreateQuestion() {
+  const [answersCorrectValues, setAnswersCorrectValues] = useState([
+    "false",
+    "false",
+    "false",
+    "false",
+  ]);
+
+  const changeCorrectAnswer = (e) => {
+    const clickedAnswer = Number(e.target.id.split("-")[1]);
+
+    const copyValues = answersCorrectValues.map((value, index) => {
+      if (clickedAnswer == index) {
+        return (answersCorrectValues[index] = "true");
+      } else {
+        return (answersCorrectValues[index] = "false");
+      }
+    });
+
+    setAnswersCorrectValues(copyValues);
+  };
+
+  return (
+    <div className="authentication create-question-div">
+      <h1>Category Question</h1>
+      <Formik
+        initialValues={{
+          category: "--- Choose Category ---",
+          points: "--- Choose Points ---",
+          name: "",
+          answerOne: "",
+          answerTwo: "",
+          answerThree: "",
+          answerFour: "",
+        }}
+        validate={(values) => {
+          const errors = {};
+          if (!values.name) {
+            errors.name = "Name is required";
+          }
+          if (!values.answerOne) {
+            errors.answerOne = "Answer One is required";
+          }
+
+          return errors;
+        }}
+        onSubmit={async (values) => {
+          try {
+            await createQuestion(values);
+          } catch (error) {
+            return toast.error(error.message);
+          }
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form className="authentication-form">
+            <Field
+              as="select"
+              name="category"
+              className="authentication-input category-select"
+            >
+              <option value="chooseCategory">--- Choose Category ---</option>
+              <option value="red">Red</option>
+              <option value="green">Green</option>
+              <option value="blue">Blue</option>
+            </Field>
+            <ErrorMessage
+              name="category"
+              component="div"
+              className="authentication-error"
+            />
+
+            <Field
+              as="select"
+              name="points"
+              className="authentication-input category-select"
+            >
+              <option value="choosePoints">--- Choose Points ---</option>
+              <option value="five">5</option>
+              <option value="ten">10</option>
+              <option value="fifteen ">15</option>
+              <option value="twenty ">20</option>
+            </Field>
+            <ErrorMessage
+              name="points"
+              component="div"
+              className="authentication-error"
+            />
+
+            <Field
+              name="name"
+              as={TextareaField}
+            />
+            <ErrorMessage
+              name="name"
+              component="div"
+              className="authentication-error"
+            />
+
+            <p className="create-questions-paragraph">
+              Choose the correct answer by clicking the button next to it
+            </p>
+
+            <div className="create-question-wrapper">
+              <Field
+                type="text"
+                name="answerOne"
+                placeholder="Answer One"
+                className="create-question-input"
+              />
+              <button
+                type="button"
+                id="correct-0"
+                className="create-question-button"
+                onClick={changeCorrectAnswer}
+              >
+                {answersCorrectValues[0]}
+              </button>
+            </div>
+            <ErrorMessage
+              name="answerOne"
+              component="div"
+              className="authentication-error"
+            />
+
+            <div className="create-question-wrapper">
+              <Field
+                type="text"
+                name="answerTwo"
+                placeholder="Answer Two"
+                className="create-question-input"
+              />
+              <button
+                type="button"
+                id="correct-1"
+                className="create-question-button"
+                onClick={changeCorrectAnswer}
+              >
+                {answersCorrectValues[1]}
+              </button>
+            </div>
+            <ErrorMessage
+              name="answerTwo"
+              component="div"
+              className="authentication-error"
+            />
+
+            <div className="create-question-wrapper">
+              <Field
+                type="text"
+                name="answerThree"
+                placeholder="Answer Three"
+                className="create-question-input"
+              />
+              <button
+                type="button"
+                id="correct-2"
+                className="create-question-button"
+                onClick={changeCorrectAnswer}
+              >
+                {answersCorrectValues[2]}
+              </button>
+            </div>
+            <ErrorMessage
+              name="answerThree"
+              component="div"
+              className="authentication-error"
+            />
+
+            <div className="create-question-wrapper">
+              <Field
+                type="text"
+                name="answerFour"
+                placeholder="Answer Four"
+                className="create-question-input"
+              />
+              <button
+                type="button"
+                id="correct-3"
+                className="create-question-button"
+                onClick={changeCorrectAnswer}
+              >
+                {answersCorrectValues[3]}
+              </button>
+            </div>
+            <ErrorMessage
+              name="answerFour"
+              component="div"
+              className="authentication-error"
+            />
+
+            <button
+              type="submit"
+              className="authentication-form-button"
+              disabled={isSubmitting}
+            >
+              Next Page
+            </button>
+          </Form>
+        )}
+      </Formik>
+    </div>
+  );
+}
