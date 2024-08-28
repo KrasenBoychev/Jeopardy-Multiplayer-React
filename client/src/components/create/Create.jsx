2427395f5d6a44e7c5c464a5da792e71fe2bc0c8
Krasen Boychev <krasenboychev11@gmail.com>
2024-08-28 22:26:56 +0100

    Create.jsx completed

--- client/src/components/create/Create.jsx
+++ client/src/components/create/Create.jsx
@@ -1,5 +1,26 @@
+import { useNavigate } from "react-router-dom";
+
+import "./create.css";
+
 export default function Create() {
+  const navigate = useNavigate();
+
+  const createCategoryHandler = () => {
+    navigate('/createCategory');
+  };
+
+  const createQuestionHandler = () => {
+    navigate('/createQuestion');
+  };
+
   return (
-    <div>Create</div>
-  )
+    <div className="create-section">
+      <div onClick={createCategoryHandler}>
+        <p>Create Category and Questions for it</p>
+      </div>
+      <div onClick={createQuestionHandler}>
+        <p>Create Question only</p>
+      </div>
+    </div>
+  );
 }
