57bdc0cb4f4c657f07a0b2ea47d70f6badedab57
Krasen Boychev <krasenboychev11@gmail.com>
2024-08-28 22:30:24 +0100

    createRouter added to configRoutes.js

--- server/src/config/configRoutes.js
+++ server/src/config/configRoutes.js
@@ -1,9 +1,11 @@
-const { gameRouter } = require('../controllers/game');
 const { userRouter } = require('../controllers/user');
+const { gameRouter } = require('../controllers/game');
+const { createRouter } = require('../controllers/create');
 
 function configRoutes(app) {
   app.use('/users', userRouter);
   app.use('/play', gameRouter);
+  app.use('/create', createRouter);
 }
 
 module.exports = { configRoutes };
