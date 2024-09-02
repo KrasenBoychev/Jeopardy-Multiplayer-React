340b97eb341c3f0886ee8a82d2c0465dbd9cdf8a
Krasen Boychev <krasenboychev11@gmail.com>
2024-09-02 20:59:33 +0100

    getCategory request created

--- client/api/game-api.js
+++ client/api/game-api.js
@@ -2,6 +2,10 @@ import * as api from './requester.js';
 
 const host = api.settings.host;
 
+export async function getCategory(categoryName) {
+    return await api.get(host + '/play/category/' + categoryName); 
+}
+
 export async function getGameToken(username) {
     return await api.get(host + '/play/' + username); 
 }
