810236dc94362d8cce72f9df59882711d76ba83e
Krasen Boychev <krasenboychev11@gmail.com>
2024-09-05 14:21:08 +0100

    Category.jsx uses useCreateCategory hook

--- client/src/components/create/createCategory/CreateCategory.jsx
+++ client/src/components/create/createCategory/CreateCategory.jsx
@@ -1,103 +1,50 @@
-import { useEffect, useState } from "react";
-import toast from "react-hot-toast";
-
-import { createCategory, createQuestion } from "../../../../api/create-api";
-import { getCategory } from "../../../../api/game-api";
+import useCreateCategory from "../../../hooks/useCreateCategory";
 
 import CreateQuestion from "../createQuestion/CreateQuestion";
 import Category from "./Category";
+import DotLoader from "react-spinners/DotLoader";
 
 export default function CreateCategory() {
-  const [moveToNextPage, setMoveToNextPage] = useState(0);
-  const [recordCategoryAndQuestions, setRecordCategoryAndQuestions] =
-    useState(false);
-
-  const [category, setCategory] = useState("");
-  const [questionOne, setQuestionOne] = useState({});
-  const [questionTwo, setQuestionTwo] = useState({});
-  const [questionThree, setQuestionThree] = useState({});
-  const [questionFour, setQuestionFour] = useState({});
-
-  useEffect(() => {
-    (async function record() {
-      if (recordCategoryAndQuestions) {
-        try {
-          await createCategory(category);
-        } catch (error) {
-          toast.error(error.message);
-          return;
-        }
-
-        // let createdCategory;
-
-        // try {
-        //   createdCategory = await getCategory(category);
-        // } catch (error) {
-        //   toast.error(error.message);
-        //   return;
-        // }
-
-        // try {
-        //   if (createCategory) {
-        //     await createQuestion(questionOne, createCategory.id);
-        //   }
-        // } catch (error) {
-        //   toast.error(error.message);
-        //   return;
-        // }
-
-        // try {
-        //   if (createCategory) {
-        //     await createQuestion(questionTwo, createCategory.id);
-        //   }
-        // } catch (error) {
-        //   toast.error(error.message);
-        //   return;
-        // }
-
-        // try {
-        //   if (createCategory) {
-        //     await createQuestion(questionThree, createCategory.id);
-        //   }
-        // } catch (error) {
-        //   toast.error(error.message);
-        //   return;
-        // }
-
-        // try {
-        //   if (createCategory) {
-        //     await createQuestion(questionFour, createCategory.id);
-        //   }
-        // } catch (error) {
-        //   toast.error(error.message);
-        //   return;
-        // }
-      }
-    })();
-  }, [recordCategoryAndQuestions]);
+  const [
+    moveToNextPage,
+    setMoveToNextPage,
+    category,
+    setCategory,
+    questionOne,
+    setQuestionOne,
+    questionTwo,
+    setQuestionTwo,
+    questionThree,
+    setQuestionThree,
+    questionFour,
+    setQuestionFour,
+    recordCategoryAndQuestions,
+    setRecordCategoryAndQuestions
+  ] = useCreateCategory();
 
   return (
     <>
       {moveToNextPage == 0 && (
-            <Category props={{ category, setCategory, setMoveToNextPage }} />
+        <Category props={{ category, setCategory, setMoveToNextPage }} />
       )}
 
       {moveToNextPage == 1 && (
         <CreateQuestion
           props={{
             category,
+            question: questionOne,
             setQuestion: setQuestionOne,
             move: moveToNextPage,
             setMove: setMoveToNextPage,
-            setRecordCategoryAndQuestions
           }}
         />
       )}
 
-      {/* {moveToNextPage == 2 && (
+      {moveToNextPage == 2 && (
         <CreateQuestion
           props={{
             category,
+            question: questionTwo,
             setQuestion: setQuestionTwo,
             move: moveToNextPage,
             setMove: setMoveToNextPage,
@@ -109,6 +56,7 @@ export default function CreateCategory() {
         <CreateQuestion
           props={{
             category,
+            question: questionThree,
             setQuestion: setQuestionThree,
             move: moveToNextPage,
             setMove: setMoveToNextPage,
@@ -120,13 +68,16 @@ export default function CreateCategory() {
         <CreateQuestion
           props={{
             category,
+            question: questionFour,
             setQuestion: setQuestionFour,
             move: moveToNextPage,
-            // setMove: setMoveToNextPage,
+            setMove: setMoveToNextPage,
             setRecordCategoryAndQuestions,
           }}
         />
-      )} */}
+      )}
+
+      {/* {recordCategoryAndQuestions && <DotLoader />} */}
     </>
   );
 }
