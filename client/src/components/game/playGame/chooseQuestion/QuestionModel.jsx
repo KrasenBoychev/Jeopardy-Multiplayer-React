403d18e41c345ac326ee8d5fa7388981622033f8
Krasen Boychev <krasenboychev11@gmail.com>
2024-09-06 20:17:19 +0100

    QuestionModel.jsx shows the question selected from the player

--- client/src/components/game/playGame/chooseQuestion/QuestionModel.jsx
+++ client/src/components/game/playGame/chooseQuestion/QuestionModel.jsx
@@ -1,9 +1,38 @@
+import { useChannelStateContext, useChatContext } from "stream-chat-react";
+
 export default function QuestionModel({ props }) {
+  const {
+    activePlayer,
+    categoryName,
+    question,
+  } = props;
+
+  const { client } = useChatContext();
+  const { channel } = useChannelStateContext();
+
+  const showQuestionClickHandler = async () => {
+    await channel.sendEvent({
+      type: "choose-question",
+      data: {
+        activePlayer,
+        categoryName,
+        question,
+      },
+    });
+  };
+
   return (
     <div
-      className={props.categoryValue ? "category-box" : "question-box"}
+      className={
+        !props.question
+          ? "category-box"
+          : client.user.name === activePlayer
+          ? "question-box active-box"
+          : "question-box"
+      }
+      onClick={showQuestionClickHandler}
     >
-      {props.categoryValue ? props.categoryValue.category : props.pointsQuestion}
+      {props.question ? question.points : props.categoryName}
     </div>
   );
 }
