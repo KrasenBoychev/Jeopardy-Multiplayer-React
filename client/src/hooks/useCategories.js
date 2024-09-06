bca61ade043ba345a5ffd600a90f5947b3b635f0
Krasen Boychev <krasenboychev11@gmail.com>
2024-09-06 20:18:06 +0100

    useCategories hook uses data from MongoDB to set the categories and the questions

--- client/src/hooks/useCategories.js
+++ client/src/hooks/useCategories.js
@@ -1,4 +1,8 @@
 import { useEffect, useState } from "react";
+import toast from "react-hot-toast";
+
+import { getAllCategories, getQuestions } from "../../api/game-api";
+import { points } from "../common/gamePoints";
 
 export default function useCategories() {
   const defaultOption = "--- Choose Category ---";
@@ -10,13 +14,9 @@ export default function useCategories() {
   const [catB, setCatB] = useState(defaultOption);
   const [catC, setCatC] = useState(defaultOption);
   const [catD, setCatD] = useState(defaultOption);
-  const [allCategories, setAllCategories] = useState([
-    defaultOption,
-    "Maths",
-    "Languages",
-    "Biology",
-    "History",
-  ]);
+  const [allCategories, setAllCategories] = useState([]);
+  const [allCategoriesInfo, setAllCategoriesInfo] = useState([]);
+  const [questions, setQuestions] = useState({});
 
   const categoriesInfo = {
     0: {
@@ -38,11 +38,63 @@ export default function useCategories() {
   };
 
   useEffect(() => {
-    (function changePage() {
+    (async function getCategories() {
+      if (allCategories.length == 0) {
+        const categories = await getAllCategories();
+
+        const categoriesNames = categories.map((c) => c.name);
+        setAllCategories([defaultOption, ...categoriesNames]);
+
+        setAllCategoriesInfo(categories);
+      }
+    })();
+  });
+
+  useEffect(() => {
+    (async function changePage() {
       if (currCategoryCount == 4) {
+        const categoriesIDs = allCategoriesInfo
+          .filter((c) => [catA, catB, catC, catD].includes(c.name))
+          .map((c) => c._id);
+
+        try {
+          const questionsResult = await getQuestions(categoriesIDs);
+
+          const gameQuestions = {};
+
+          [catA, catB, catC, catD].map((cat) => {
+            const currCatInfo = allCategoriesInfo.filter(
+              (catInfo) => catInfo.name == cat
+            );
+            const currCatID = currCatInfo[0]._id;
+
+            const catQuestions = [];
+
+            points.map((currPoints) => {
+              const questionsMatching = questionsResult.filter(
+                (question) =>
+                  question.categoryId == currCatID &&
+                  question.points == Number(currPoints)
+              );
+
+              const randomQuestion =
+                questionsMatching[
+                  Math.floor(Math.random() * questionsMatching.length)
+                ];
+              catQuestions.push(randomQuestion);
+            });
+
+            gameQuestions[cat] = catQuestions;
+          });
+  
+          setQuestions(gameQuestions);
+        } catch (error) {
+          return toast.error(error.message);
+        }
+
         setTimeout(() => {
           setMoveToNextPage(true);
-        }, 2000);
+        }, 500);
       }
     })();
   }, [currCategoryCount]);
@@ -54,6 +106,8 @@ export default function useCategories() {
     allCategories,
     setAllCategories,
     categoriesInfo,
-    defaultOption
+    defaultOption,
+    questions,
+    allCategoriesInfo,
   ];
 }
