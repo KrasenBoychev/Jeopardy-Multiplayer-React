e8ea922695b1debce57b3313d2263016cdecb3be
Krasen Boychev <krasenboychev11@gmail.com>
2024-09-09 18:25:53 +0100

    Home.jsx shows the player points; the style for the fields updated

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,34 +1,31 @@
 import { Link } from "react-router-dom";
 
-import './home.css';
+import "./home.css";
+import { useAuthContext } from "../../contexts/AuthContext";
 
 export default function Home() {
-    const data = ['test', 'test', 'test', 'test', 'test', 'test']
+  const { points } = useAuthContext();
+  const data = ["test", "test", "test", "test", "test", "test"];
 
-    return (
-        <main>
-            <div className='leaderboards'>
-                <div className="leaderboard-today">
-                    <p>Leaderboard Today</p>
-                    <ul>
-                        {data.map((item, index) => <li key={index}>{item}</li>)}
-                    </ul>
-                </div>
-                <div className="leaderboard-all-time">
-                    <p>Leaderboard All Time</p>
-                    <ul>
-                        {data.map((item, index) => <li key={index}>{item}</li>)}
-                    </ul>
-                </div>
-            </div>
+  return (
+    <main>
+      <div className="leaderboards">
+        <div className="points-play">
+          <p>Your Points: {points}</p>
 
-            <div className='play-button'>
-                <button>
-                    <Link to='/play'>
-                        Play
-                    </Link>
-                </button>
-            </div>
-        </main>
-    );
+          <Link to="/play">
+            <button className="play-button">Play</button>
+          </Link>
+        </div>
+        <div className="leaderboard-all-time">
+          <p>Leaderboard All Time</p>
+          <ul>
+            {data.map((item, index) => (
+              <li key={index}>{item}</li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </main>
+  );
 }
