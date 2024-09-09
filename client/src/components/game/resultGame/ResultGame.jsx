95033dcd460a9b32da6970e52de0ee20a4b7f288
Krasen Boychev <krasenboychev11@gmail.com>
2024-09-09 17:21:15 +0100

    ResultGame.jsx shows the final points for both players

--- client/src/components/game/resultGame/ResultGame.jsx
+++ client/src/components/game/resultGame/ResultGame.jsx
@@ -1,18 +1,28 @@
-import "./resultGame.css"
+import "./resultGame.css";
 export default function ResultGame({ props }) {
   const { firstPlayer, secondPlayer, pointsFirstPlayer, pointsSecondPlayer } =
     props;
   return (
     <div className="gameContainer">
-        <p className="result-winner answer-correct">
-      {pointsFirstPlayer == pointsSecondPlayer
-        ? `${firstPlayer} and ${secondPlayer} share the first place with ${pointsFirstPlayer} points!`
-        : pointsFirstPlayer > pointsSecondPlayer
-        ? `Winner: ${firstPlayer} with ${pointsFirstPlayer} points`
-        : `Winner: ${secondPlayer} with ${pointsSecondPlayer} points`
-       // : `${firstPlayer} and ${secondPlayer} share the first place with ${pointsFirstPlayer} points!`
-      }
+      <p className="result-winner answer-correct">
+        {pointsFirstPlayer == pointsSecondPlayer
+          ? `${firstPlayer} and ${secondPlayer} share the first place!`
+          : pointsFirstPlayer > pointsSecondPlayer
+          ? `Congratulations ${firstPlayer}!`
+          : `Congratulations ${secondPlayer}!`}
+      </p>
+      <div className="final-result">
+        <p>Final Result</p>
+        <p className="final-players">
+          <span className="final-first-player">
+            {firstPlayer}: {pointsFirstPlayer} points
+          </span>
+          <span className="final-vs">VS</span>
+          <span className="final-second-player">
+            {secondPlayer}: {pointsSecondPlayer} points
+          </span>
         </p>
+      </div>
     </div>
   );
 }
