7d4530c4ac9493bcc52d81d71e20f4702fd5a458
Krasen Boychev <krasenboychev11@gmail.com>
2024-09-14 14:38:27 +0100

    useCreateQuestion hook sets the correct answer index when 'Previous Question' button is clicked

--- client/src/hooks/useCreateQuestion.js
+++ client/src/hooks/useCreateQuestion.js
@@ -3,30 +3,31 @@ import { useEffect, useState } from "react";
 import { getAllCategories } from "../../api/game-api";
 
 export default function useCreateQuestion(category, question) {
-    const [answersCorrectValues, setAnswersCorrectValues] = useState([
-        "false",
-        "false",
-        "false",
-        "false",
-      ]);
-    
-      const [allCategories, setAllCategories] = useState([]);
-    
-      useEffect(() => {
-        (async function getCategories() {
-          if (!category) {
-            const categories = await getAllCategories();
-            setAllCategories(categories);
-          }
+  const answersValues = ["false", "false", "false", "false"];
 
-          if (question && question.answers) {
-            let copyValues = [...answersCorrectValues];
-            copyValues[question.answers.correctAnswer] = 'true';
-            
-            setAnswersCorrectValues(copyValues);
-          }
-        })();
-      }, []);
-    
-      return [answersCorrectValues, setAnswersCorrectValues, allCategories];
+  if (question && [0, 1, 2, 3].includes(question.correctIndex)) {
+    answersValues[question.correctIndex] = 'true';
+  }
+
+  const [answersCorrectValues, setAnswersCorrectValues] = useState(answersValues);
+
+  const [allCategories, setAllCategories] = useState([]);
+
+  useEffect(() => {
+    (async function getCategories() {
+      if (!category) {
+        const categories = await getAllCategories();
+        setAllCategories(categories);
+      }
+
+      if (question && question.answers) {
+        let copyValues = [...answersCorrectValues];
+        copyValues[question.answers.correctAnswer] = "true";
+
+        setAnswersCorrectValues(copyValues);
+      }
+    })();
+  }, []);
+
+  return [answersCorrectValues, setAnswersCorrectValues, allCategories];
 }
