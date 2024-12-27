911a458042484c79edf646312b05a3a5f90f24c1
Krasen Boychev <krasenboychev11@gmail.com>
2024-12-27 16:03:13 +0000

    new className used for the div element; click event moved to the paragraph

--- client/src/components/create/Create.jsx
+++ client/src/components/create/Create.jsx
@@ -1,26 +1,29 @@
 import { useNavigate } from "react-router-dom";
 
 import "./create.css";
+import DotLoader from "react-spinners/DotLoader";
 
 export default function Create() {
   const navigate = useNavigate();
 
   const createCategoryHandler = () => {
-    navigate('/createCategory');
+    navigate("/createCategory");
   };
 
   const createQuestionHandler = () => {
-    navigate('/createQuestion');
+    navigate("/createQuestion");
   };
 
   return (
-    <div className="create-section">
-      <div onClick={createCategoryHandler}>
-        <p>Create Category and Questions for it</p>
+    <main>
+      <div className="create-container">
+        <section>
+          <p onClick={createCategoryHandler}>Create Category and Questions for it</p>
+        </section>
+        <section>
+          <p onClick={createQuestionHandler}>Create Question only</p>
+        </section>
       </div>
-      <div onClick={createQuestionHandler}>
-        <p>Create Question only</p>
-      </div>
-    </div>
+    </main>
   );
 }
