bf0d4c7ad373acbcaaf98c1fba2f1adec0e99bb9
Krasen Boychev <krasenboychev11@gmail.com>
2024-12-27 11:53:56 +0000

    new elements and classes added

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -22,25 +22,45 @@ export default function Home() {
   return (
     <main>
       <div className="home-container">
-        <div className="points-play">
-          <p>{points != undefined ? `Your Points: ${points}` : "Play to win points!"}</p>
-
+        <section className="points-info-and-play-button">
+          <p>
+            {points != undefined
+              ? `Your Points: ${points}`
+              : "Win points and see your name in the Leaderboard!"}
+          </p>
           <Link to="/play">
-            <button className="play-button">Play</button>
+            <button className="play-button">
+              &gt;&gt;&gt; Play &lt;&lt;&lt;
+            </button>
           </Link>
-        </div>
-        <div className="leaderboard-all-time">
-          <p>Leaderboard All Time</p>
-          <ul>
-            {topPlayers.map((player, index) => (
-              <li key={player.username} className={index < 3 ? "leaderboard-all-time-player top-three" : "leaderboard-all-time-player"}>
-                <span className="leaderboard-position">{index + 1}.</span>
-                <span className="leaderboard-username">{player.username}</span>
-                <span className="leaderboard-points">{player.points} points</span>
-              </li>
-            ))}
-          </ul>
-        </div>
+        </section>
+        <section className="leaderboard-all-time">
+          <div className="leaderboard-wrapper">
+            <p>Leaderboard All Time</p>
+            <ul>
+              {topPlayers.map((player, index) => (
+                <li
+                  key={player.username}
+                  className={
+                    index < 3
+                      ? "leaderboard-all-time-player top-three"
+                      : "leaderboard-all-time-player"
+                  }
+                >
+                  <div className="leaderboard-player-info">
+                    <span className="leaderboard-position">{index + 1}.</span>
+                    <span className="leaderboard-username">
+                      {player.username}
+                    </span>
+                  </div>
+                  <span className="leaderboard-points">
+                    {player.points} points
+                  </span>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </section>
       </div>
     </main>
   );
