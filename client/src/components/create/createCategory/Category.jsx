78fa63a2ac4a6eb764225535a43ee30afe941aa2
Krasen Boychev <krasenboychev11@gmail.com>
2024-12-27 16:04:03 +0000

    Category component uses some new class names

--- client/src/components/create/createCategory/Category.jsx
+++ client/src/components/create/createCategory/Category.jsx
@@ -4,41 +4,49 @@ export default function Category({ props }) {
   const { category, setCategory, setMoveToNextPage } = props;
 
   return (
-    <div className="authentication">
-      <h1>Category Name</h1>
-      <Formik
-        initialValues={{ name: category }}
-        validate={(values) => {
-          const errors = {};
-          if (!values.name) {
-            errors.name = "Name is required";
-          }
+    <main>
+      <div className="authentication-container">
+        <div className="authentication-wrapper">
+          <h1>Category Name</h1>
+          <Formik
+            initialValues={{ name: category }}
+            validate={(values) => {
+              const errors = {};
+              if (!values.name) {
+                errors.name = "Name is required";
+              }
 
-          return errors;
-        }}
-        onSubmit={async (values) => {
-          setCategory(values.name);
-          setMoveToNextPage(oldValue => oldValue + 1);
-        }}
-      >
-        {({ isSubmitting }) => (
-          <Form className="authentication-form">
-            <Field type="text" name="name" className="authentication-input" />
-            <ErrorMessage
-              name="name"
-              component="div"
-              className="authentication-error"
-            />
-            <button
-              className="authentication-form-button"
-              type="submit"
-              disabled={isSubmitting}
-            >
-              Next Page
-            </button>
-          </Form>
-        )}
-      </Formik>
-    </div>
+              return errors;
+            }}
+            onSubmit={async (values) => {
+              setCategory(values.name);
+              setMoveToNextPage((oldValue) => oldValue + 1);
+            }}
+          >
+            {({ isSubmitting }) => (
+              <Form className="authentication-form">
+                <Field
+                  type="text"
+                  name="name"
+                  className="authentication-input"
+                />
+                <ErrorMessage
+                  name="name"
+                  component="div"
+                  className="authentication-error"
+                />
+                <button
+                  className="authentication-form-button"
+                  type="submit"
+                  disabled={isSubmitting}
+                >
+                  Next Page
+                </button>
+              </Form>
+            )}
+          </Formik>
+        </div>
+      </div>
+    </main>
   );
 }
