1d9779563d217105c79a1770dc45474188ea06be
Krasen Boychev <krasenboychev11@gmail.com>
2024-12-27 12:04:47 +0000

    new elements and classes added

--- client/src/components/notFound/NotFound.jsx
+++ client/src/components/notFound/NotFound.jsx
@@ -1,9 +1,15 @@
 import "./notFound.css";
+import { Link } from "react-router-dom";
 
 export default function NotFound() {
   return (
-    <div className="not-found-wrapper">
-      <p className="not-found">Page not found</p>
-    </div>
+    <main>
+      <div className="not-found-wrapper">
+        <p className="not-found-msg">Page not found</p>
+        <p className="not-found-get-back">
+          Get back to <Link to="/" className="not-found-link">Home Page</Link>
+        </p>
+      </div>
+    </main>
   );
 }
