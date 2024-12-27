5515cf3191e6b2d900426bc0ec78e6e450eb847a
Krasen Boychev <krasenboychev11@gmail.com>
2024-12-27 20:21:23 +0000

    header doesn't change when new game is started; not needed  props deleted

--- client/src/components/core/header/Header.jsx
+++ client/src/components/core/header/Header.jsx
@@ -1,17 +1,10 @@
 import { NavLink } from "react-router-dom";
-
 import { useAuthContext } from "../../../contexts/AuthContext.jsx";
 import { adminId } from "../../../common/credentials.js";
-
 import "./header.css";
 
-import LeaveGame from "../../leaveGame/LeaveGame.jsx";
-
-export default function Header(props) {
+export default function Header() {
   const { isAuthenticated, username, userId } = useAuthContext();
-
-  const { isNewGameStarted, setIsNewGameStarted } = props.game;
-  const client = props.client;
 
   return (
     <header>
@@ -20,51 +13,37 @@ export default function Header(props) {
           <li>Welcome, {isAuthenticated ? username : "guest"}</li>
         </ul>
 
-        {isNewGameStarted ? (
-          <ul className="profile">
+        <ul className="profile">
+          <li>
+            <NavLink to="/">Home</NavLink>
+          </li>
+          <li>
+            <NavLink to="/about">About</NavLink>
+          </li>
+          <li>
+            <NavLink to="/play">Play</NavLink>
+          </li>
+          {userId == adminId && (
             <li>
-              <LeaveGame
-                game={{ isNewGameStarted, setIsNewGameStarted }}
-                channel={props.channel}
-                client={client}
-              />
+              <NavLink to="/create">Create</NavLink>
             </li>
-          </ul>
-        ) : (
-          <>
-            <ul className="profile">
-              <li>
-                <NavLink to="/">Home</NavLink>
-              </li>
-              <li>
-                <NavLink to="/about">About</NavLink>
-              </li>
-              <li>
-                <NavLink to="/play">Play</NavLink>
-              </li>
-              {userId == adminId && (
-                <li>
-                  <NavLink to="/create">Create</NavLink>
-                </li>
-              )}
+          )}
 
-              {isAuthenticated ? (
-                <li>
-                  <NavLink to="/logout">Logout</NavLink>
-                </li>
-              ) : (
-                <>
-                  <li>
-                    <NavLink to="/login">Login</NavLink>
-                  </li>
-                  <li>
-                    <NavLink to="/register">Register</NavLink>
-                  </li>
-                </>
-              )}
-            </ul>
-          </>
-        )}
+          {isAuthenticated ? (
+            <li>
+              <NavLink to="/logout">Logout</NavLink>
+            </li>
+          ) : (
+            <>
+              <li>
+                <NavLink to="/login">Login</NavLink>
+              </li>
+              <li>
+                <NavLink to="/register">Register</NavLink>
+              </li>
+            </>
+          )}
+        </ul>
       </nav>
     </header>
   );
