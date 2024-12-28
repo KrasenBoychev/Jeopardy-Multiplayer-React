e70b87151bb6765b0b27f9322309384acafe21a2
Krasen Boychev <krasenboychev11@gmail.com>
2024-12-28 16:39:56 +0000

    new classes added for Question.jsx

--- client/src/components/game/playGame/chooseQuestion/chunks/RenderQuestions.jsx
+++ client/src/components/game/playGame/chooseQuestion/chunks/RenderQuestions.jsx
@@ -15,46 +15,67 @@ export default function RenderQuestions({ props }) {
   const { client } = useChatContext();
 
   return (
-    <div className="gameContainer">
-      <p
-        className={
-          client.user.name === activePlayer
-            ? "activeCat player-categories"
-            : "inactiveCat player-categories"
-        }
-      >
-        {activePlayer} chooses question
-      </p>
-      <p className="players-points">
-        <span className="points-first-player">{firstPlayer}: {pointsFirstPlayer} points</span>
-        <span className="points-second-player">{secondPlayer}: {pointsSecondPlayer} points</span>
-      </p>
-      <div className="categories-container">
-        {Object.keys(questions).map((categoryName) => {
-          return <QuestionModel key={categoryName} props={{ categoryName }} />;
-        })}
-      </div>
-      <div className="questions-container">
-        {Object.entries(questions).map((item, indexItem) => {
-          return (
-            <div key={indexItem}>
-              {Object.values(questions).map((question, indexQuestion) => {
-                return (
-                  <QuestionModel
-                    key={question + indexQuestion}
-                    props={{
-                      activePlayer,
-                      categoryName: Object.keys(questions)[indexQuestion],
-                      question: question[indexItem].question,
-                      questionAnswered: question[indexItem].answered,
-                    }}
-                  />
-                );
-              })}
-            </div>
-          );
-        })}
-      </div>
+    <div className="questions-page-wrapper">
+      <section>
+        <div className="players-points-wrapper">
+          <h3>Score</h3>
+          <div className="players-points">
+            <p className="points-first-player">
+              <span>{firstPlayer}:</span>
+              <span>{pointsFirstPlayer} points</span>
+            </p>
+            <p className="points-second-player">
+              <span>{secondPlayer}:</span>
+              <span>{pointsSecondPlayer} points</span>
+            </p>
+          </div>
+        </div>
+      </section>
+
+      <section>
+        <p
+          className={
+            client.user.name === activePlayer
+              ? "active-player player-categories"
+              : "player-categories"
+          }
+        >
+          {activePlayer} chooses question
+        </p>
+        <div className="categories-names">
+          {Object.keys(questions).map((categoryName) => {
+            return (
+              <QuestionModel key={categoryName} props={{ categoryName }} />
+            );
+          })}
+        </div>
+
+        <div className="questions-container">
+          {Object.entries(questions).map((item, indexItem) => {
+            return (
+              <div key={indexItem}>
+                {Object.values(questions).map((question, indexQuestion) => {
+                  return (
+                    <QuestionModel
+                      key={question + indexQuestion}
+                      props={{
+                        activePlayer,
+                        categoryName: Object.keys(questions)[indexQuestion],
+                        question: question[indexItem].question,
+                        questionAnswered: question[indexItem].answered,
+                      }}
+                    />
+                  );
+                })}
+              </div>
+            );
+          })}
+        </div>
+      </section>
+
+      <section>
+        <p>Timer</p>
+      </section>
     </div>
   );
 }
