b15078d19103f53f5256980444a5c0edbbf3d73d
Krasen Boychev <krasenboychev11@gmail.com>
2024-12-28 16:08:35 +0000

    api-keys removed from the .gitignore

---
+++ server/src/api-keys.js
@@ -0,0 +1,8 @@
+const api_key = 'tswxm74zz6uc';
+
+const api_secret =
+  'fw9gmt6mrujybps5qfkjmedwhrjnrrh79y44w8247zs939y4g79c4q8saut6uaqa';
+
+const adminId = '66cb5ecfe65e43087b6443c1';
+
+module.exports = { api_key, api_secret, adminId };
