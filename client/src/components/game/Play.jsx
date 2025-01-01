d7b7aaec7faba7fb08aa2164bfa43096be953d90
Krasen Boychev <krasenboychev11@gmail.com>
2025-01-01 16:33:07 +0000

    Play component doesn't use the Chat element anymore and the client is a result of the usePlay hook

--- client/src/components/game/Play.jsx
+++ client/src/components/game/Play.jsx
@@ -1,25 +1,15 @@
-import { Chat } from "stream-chat-react";
-
 import { useAuthContext } from "../../contexts/AuthContext";
 import usePlay from "../../hooks/usePlay";
-
 import JoinGame from "./startGame/joinGame/JoinGame";
 
 export default function Play(props) {
-  const { isNewGameStarted, setIsNewGameStarted } = props.game;
-  const client = props.client;
-
   const { username } = useAuthContext();
 
-  usePlay(username, setIsNewGameStarted, client);
+  const client = usePlay(username);
 
   return (
     <>
-      {isNewGameStarted && (
-        <Chat client={client}>
-          <JoinGame channel={props.channel} />
-        </Chat>
-      )}
+      <JoinGame game={props.game} client={client} />
     </>
   );
 }
