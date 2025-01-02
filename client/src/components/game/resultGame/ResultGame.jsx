ffc3258502818c67f54e3b80e47e9133b15af407
Krasen Boychev <krasenboychev11@gmail.com>
2025-01-02 19:11:21 +0000

    ResultGame connects the players to a new game or renders the JoinGame component and the player can a game with other player - not fully successful at the moment

--- client/src/components/game/resultGame/ResultGame.jsx
+++ client/src/components/game/resultGame/ResultGame.jsx
@@ -1,42 +1,64 @@
 import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
 import toast from "react-hot-toast";
 import { useGameContext } from "../../../contexts/GameContext";
 import { disconnectUser } from "../../../hooks/useLeaveGame";
 import ConnectPlayers from "../startGame/connectPlayers/ConnectPlayers";
+import JoinGame from "../startGame/joinGame/JoinGame";
+
+import "../exitGame/confirm/confirm.css";
 import "./resultGame.css";
 
 export default function ResultGame({ props }) {
   const { firstPlayer, secondPlayer, pointsFirstPlayer, pointsSecondPlayer } =
     props;
 
-  const { channel, setChannel, client, setIsNewGameStarted, rivalPlayer } =
-    useGameContext();
-  const navigate = useNavigate();
+  const {
+    channel,
+    setChannel,
+    client,
+    rivalPlayer,
+    isNewGameStarted,
+    setIsNewGameStarted,
+  } = useGameContext();
 
-  const [newGame, setNewGame] = useState(false);
+  const [newGameWithSamePlayer, setNewGameWithSamePlayer] = useState(false);
+  const [newGameWithOtherPlayer, setNewGameWithOtherPlayer] = useState(false);
   const [newGameMsg, setNewGameMsg] = useState(false);
   const [playerReceivingMsg, setPlayerReceivingMsg] = useState("");
+  const [playerShowingNotification, setPlayerShowingNotification] =
+    useState("");
 
   useEffect(() => {
     (function showMsg() {
       if (client.user.name === playerReceivingMsg) {
         setNewGameMsg(true);
-      } else {
+        setPlayerReceivingMsg("");
+      } else if (playerReceivingMsg !== "") {
         toast.success(`Invataion sent to ${rivalPlayer}!`);
+        setPlayerReceivingMsg("");
+      }
+
+      if (client.user.name === playerShowingNotification) {
+        toast.error(`${rivalPlayer} rejected the invatation`);
+        setPlayerShowingNotification("");
+      } else if (playerShowingNotification !== "") {
+        setNewGameMsg(false);
+        setPlayerShowingNotification("");
       }
     })();
-  }, [playerReceivingMsg]);
+  }, [playerReceivingMsg, playerShowingNotification]);
 
-  const playWithAnotherFriendClickHandler = () => {
-    disconnectUser(
+  const playWithOtherFriendClickHandler = async () => {
+    await disconnectUser(
       channel,
       setChannel,
-      client,
+      null,
       setIsNewGameStarted,
-      navigate,
-      "/play"
+      null,
+      null
     );
+
+    setNewGameWithOtherPlayer(true);
   };
 
   const playWithSameFriendClickHandler = async () => {
@@ -49,18 +71,16 @@ export default function ResultGame({ props }) {
   const startNewGameFalse = async () => {
     setNewGameMsg(false);
 
-    // await channel.sendEvent({
-    //   type: "new-game-rejection",
-    //   data: { rivalPlayer },
-    // });
+    await channel.sendEvent({
+      type: "new-game-rejection",
+      data: { rivalPlayer },
+    });
   };
 
   const startNewGameTrue = async () => {
     await channel.sendEvent({
       type: "start-new-game",
     });
-
-    setNewGame(true);
   };
 
   channel.on((event) => {
@@ -69,15 +89,23 @@ export default function ResultGame({ props }) {
     }
 
     if (event.type == "start-new-game") {
-      setNewGame(true);
+      setNewGameWithSamePlayer(true);
+    }
+
+    if (event.type == "new-game-rejection") {
+      setPlayerShowingNotification(event.data.rivalPlayer);
     }
   });
 
   return (
     <>
-      {newGame ? (
-        <ConnectPlayers />
-      ) : (
+      {newGameWithSamePlayer && <ConnectPlayers />}
+
+      {newGameWithOtherPlayer && (
+        <JoinGame game={{ isNewGameStarted, setIsNewGameStarted }} />
+      )}
+
+      {!newGameWithSamePlayer && !newGameWithOtherPlayer && (
         <div className="result-game-container">
           <p className="result-winner answer-correct">
             {pointsFirstPlayer == pointsSecondPlayer
@@ -105,16 +133,18 @@ export default function ResultGame({ props }) {
               Play another game with {rivalPlayer}
             </button>
 
-            <button onClick={playWithAnotherFriendClickHandler}>
-              Play with another friend
+            <button onClick={playWithOtherFriendClickHandler}>
+              Play with other friend
             </button>
           </div>
           {newGameMsg && (
-            <div className="new-game-msg-container">
-              <p>Do you want to start a new game with {rivalPlayer}?</p>
-              <div>
-                <button onClick={startNewGameTrue}>Yes</button>
-                <button onClick={startNewGameFalse}>No</button>
+            <div className="confirm-wrapper">
+              <div className="confirm-message">
+                <h2>Do you want to start a new game with {rivalPlayer}?</h2>
+                <div className="confirm-buttons">
+                  <button onClick={startNewGameTrue}>Yes</button>
+                  <button onClick={startNewGameFalse}>No</button>
+                </div>
               </div>
             </div>
           )}
