25518f9f86a2be37d5f9f53b3726fd9bcb394961
Krasen Boychev <krasenboychev11@gmail.com>
2025-01-09 08:18:38 +0000

    usePlay hook sets a client when the JoinGame component renders

--- client/src/hooks/usePlay.js
+++ client/src/hooks/usePlay.js
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
-// import { StreamChat } from "stream-chat";
+import { StreamChat } from "stream-chat";
 
 import { getGameToken } from "../../api/game-api";
 import toast from "react-hot-toast";
@@ -9,39 +9,39 @@ import toast from "react-hot-toast";
 export default function usePlay(username) {
   const navigate = useNavigate();
 
-  // const api_key = "tswxm74zz6uc";
-  // const client = StreamChat.getInstance(api_key);
+  const api_key = "tswxm74zz6uc";
+  const client = StreamChat.getInstance(api_key);
 
   useEffect(() => {
-    // let active = true;
-    // load();
+    let active = true;
+    load();
 
-    // return () => {
-    //   active = false;
-    //   client.disconnectUser();
-    // };
+    return () => {
+      active = false;
+      client.disconnectUser();
+    };
 
-    // async function load() {
-    //   try {
-    //     const { token, userId } = await getGameToken(username);
+    async function load() {
+      try {
+        const { token, userId } = await getGameToken(username);
 
-    //     client
-    //       .connectUser(
-    //         {
-    //           id: userId,
-    //           name: username,
-    //         },
-    //         token
-    //       );
+        client
+          .connectUser(
+            {
+              id: userId,
+              name: username,
+            },
+            token
+          );
 
-    //     if (!active) {
-    //       return;
-    //     }
-    //   } catch (error) {
-    //     toast.error('Can not play at this moment. Please send a message to our customer service team.');
-    //     navigate('/');
-    //   }
-    // }
+        if (!active) {
+          return;
+        }
+      } catch (error) {
+        toast.error('Can not play at this moment. Please send a message to our customer service team.');
+        navigate('/');
+      }
+    }
   }, []);
 
   return client;
