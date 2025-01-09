ecb3a89e296d82485703cf6192b31eea6e116358
Krasen Boychev <krasenboychev11@gmail.com>
2025-01-09 08:16:50 +0000

    trying to set a server and port for socket.io - not successful for the moment

--- server/src/index.js
+++ server/src/index.js
@@ -1,20 +1,32 @@
 // Code  for mongoose config in backend
 // Filename - backend/index.js
 
-const { connectDatabase } = require('./config/configDatabase');
-const { configExpress } = require('./config/configExpress');
-const { configRoutes } = require('./config/configRoutes');
-const express = require('express');
+const { connectDatabase } = require("./config/configDatabase");
+const { configExpress } = require("./config/configExpress");
+const { configRoutes } = require("./config/configRoutes");
+const express = require("express");
+const Server = require("socket.io");
 
+// const http = require("http");
 start();
 
 async function start() {
-    const app = express();
+  const app = express();
 
-    await connectDatabase();
-    configExpress(app);
-    configRoutes(app);
+  await connectDatabase();
+  configExpress(app);
+  configRoutes(app);
 
-    app.listen(5000);
+  //   const server = http.createServer(app);
+
+  app.listen(5000);
+  // server.listen(5000);
+
+  // const io = new Server({
+  //   cors: {
+  //     origin: "http://localhost:5173",
+  //   },
+  // });
+
+  // io.listen(5000);
 }
-
