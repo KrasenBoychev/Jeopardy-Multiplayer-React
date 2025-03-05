512348bd27164b4a06ec723c1114c3aedad83429
Krasen Boychev <krasenboychev11@gmail.com>
2025-03-05 18:00:47 +0000

    user is connected to a socket if they log in and disconnected if they log out

--- client/src/components/core/header/Header.jsx
+++ client/src/components/core/header/Header.jsx
@@ -3,16 +3,19 @@ import { NavLink } from "react-router-dom";
 import { useAuthContext } from "../../../contexts/AuthContext.jsx";
 import { adminId } from "../../../common/credentials.js";
 import { io } from "socket.io-client";
+
 import "./header.css";
 
-export default function Header() {
+export default function Header({ socketProps }) {
+  const { socket, setSocket } = socketProps;
   const { isAuthenticated, username, userId } = useAuthContext();
 
-  const [socket, setSocket] = useState(null);
-
-  useEffect(() => {;
+  useEffect(() => {
     if (isAuthenticated) {
       setSocket(io("http://localhost:5000"));
+    } else if (socket) {
+      socket.disconnect();
+      setSocket(null);
     }
   }, [isAuthenticated]);
 
