f6b30c05a107b8b6bc6adb330dfaa2d0ad867bf0
Krasen Boychev <krasenboychev11@gmail.com>
2025-03-07 11:29:19 +0000

    configSocket.js created

--- server/src/config/configSocket.js
+++ server/src/config/configSocket.js
@@ -1,50 +1,39 @@
-import { Server } from 'socket.io';
+const { Server } = require('socket.io');
+const { addNewOnlineUser, removeOnlineUser } = require('../services/onlineUsers');
 
-const io = new Server({
-  cors: {
-    origin: 'http://localhost:5173',
-  },
-});
+function configSocket(server) {
+  const io = new Server(server, {
+    cors: {
+      origin: 'http://localhost:5173',
+      methods: ['GET', 'POST'],
+    },
+  });
+
+  io.on('connection', (socket) => {
+    socket.on('newUser', async (username) => {
+      await addNewOnlineUser(username, socket.id);
+    });
+
+    socket.on('sendNotification', ({ senderName, receiverName }) => {
+      const receiver = getUser(receiverName);
+
+      if (receiver) {
+        io.to(receiver.socketId).emit('getNotification', {
+          senderName,
+        });
+      }
+    });
+
+    socket.on('disconnect', async () => {
+      await removeOnlineUser(socket.id);
+    });
+  });
+}
 
 let onlineUsers = [];
-
-const addNewUser = (username, socketId) => {
-  !onlineUsers.some((user) => user.username === username) &&
-    onlineUsers.push({ username, socketId });
-};
-
-const removeUser = (socketId) => {
-  onlineUsers = onlineUsers.filter((user) => user.socketId !== socketId);
-};
 
 const getUser = (username) => {
   return onlineUsers.find((user) => user.username === username);
 };
 
-io.on('connection', (socket) => {
-  socket.on('newUser', (username) => {
-    addNewUser(username, socket.id);
-  });
-
-  socket.on('sendNotification', ({ senderName, receiverName, type }) => {
-    const receiver = getUser(receiverName);
-    io.to(receiver.socketId).emit('getNotification', {
-      senderName,
-      type,
-    });
-  });
-
-  socket.on('sendText', ({ senderName, receiverName, text }) => {
-    const receiver = getUser(receiverName);
-    io.to(receiver.socketId).emit('getText', {
-      senderName,
-      text,
-    });
-  });
-
-  socket.on('disconnect', () => {
-    removeUser(socket.id);
-  });
-});
-
-io.listen(5000);
+module.exports = { configSocket, onlineUsers };
