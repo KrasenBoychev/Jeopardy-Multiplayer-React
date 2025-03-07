cc29c9abaa45e61e5796803c591622702ab9086b
Krasen Boychev <krasenboychev11@gmail.com>
2025-03-07 11:29:52 +0000

    onlineUsersRouter imported to configRoutes.js

--- server/src/config/configRoutes.js
+++ server/src/config/configRoutes.js
@@ -1,11 +1,13 @@
 const { userRouter } = require('../controllers/user');
 const { gameRouter } = require('../controllers/game');
 const { createRouter } = require('../controllers/create');
+const { onlineUsersRouter } = require('../controllers/onlineUsers');
 
 function configRoutes(app) {
   app.use('/users', userRouter);
   app.use('/play', gameRouter);
   app.use('/create', createRouter);
+  app.use('/onlineUsers', onlineUsersRouter);
 }
 
 module.exports = { configRoutes };
