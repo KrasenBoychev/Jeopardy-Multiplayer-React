faca0224216ff2ebf609aa77c93b76b70e0426d8
Krasen Boychev <krasenboychev11@gmail.com>
2025-03-07 16:43:48 +0000

    index.js doesn't have the scoket logic implemented - it only imports and run configSocket func

--- server/src/index.js
+++ server/src/index.js
@@ -4,68 +4,21 @@
 const { connectDatabase } = require('./config/configDatabase');
 const { configExpress } = require('./config/configExpress');
 const { configRoutes } = require('./config/configRoutes');
-const express = require('express');
+const { configSocket } = require('./config/configSocket');
 
+const express = require('express');
 const http = require('http');
-const { Server } = require('socket.io');
+
 start();
 
 async function start() {
   const app = express();
+  const server = http.createServer(app);
 
   await connectDatabase();
   configExpress(app);
   configRoutes(app);
-
-  const server = http.createServer(app);
-
-  const io = new Server(server, {
-    cors: {
-      origin: 'http://localhost:5173',
-      methods: ['GET', 'POST'],
-    },
-  });
-
-  let onlineUsers = [];
-
-  const addNewUser = (username, socketId) => {
-    !onlineUsers.some((user) => user.username === username) &&
-      onlineUsers.push({ username, socketId });
-  };
-
-  const removeUser = (socketId) => {
-    onlineUsers = onlineUsers.filter((user) => user.socketId !== socketId);
-  };
-
-  const getUser = (username) => {
-    return onlineUsers.find((user) => user.username === username);
-  };
-
-  io.on('connection', (socket) => {
-    socket.on('newUser', (username) => {
-      addNewUser(username, socket.id);
-    });
-
-    socket.on('sendNotification', ({ senderName, receiverName }) => {
-      const receiver = getUser(receiverName);
-
-      if (receiver) {
-        io.to(receiver.socketId).emit('getNotification', {
-          senderName,
-        });
-      }
-    });
-
-    socket.on('disconnect', () => {
-      removeUser(socket.id);
-      console.log('disconnected');
-    });
-  });
+  configSocket(server);
 
   server.listen(5000);
-
-  // app.listen(5000);
-  // server.listen(5000);
-
-  // io.listen(5000);
 }
