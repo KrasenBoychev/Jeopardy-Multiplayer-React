c9b804ecfafd517c248dfa90447abb7e7e0af538
Krasen Boychev <krasenboychev11@gmail.com>
2025-03-08 20:36:46 +0000

    controller to get friendsOnline created

--- server/src/controllers/onlineUsers.js
+++ server/src/controllers/onlineUsers.js
@@ -1,12 +1,39 @@
 const { Router } = require('express');
 const { parseError } = require('../util');
-const { addNewOnlineUser } = require('../services/onlineUsers');
+const { addNewOnlineUser, getOnlineFriends } = require('../services/onlineUsers');
+const { getUserFriendsList } = require('../services/user');
 
 const onlineUsersRouter = Router();
 
 onlineUsersRouter.get('/addNewUser', async (req, res) => {
   try {
     await addNewOnlineUser();
+  } catch (err) {
+    const parsed = parseError(err);
+    res.status(400).json({ code: 400, message: parsed.message });
+  }
+});
+
+onlineUsersRouter.get('/friendsOnline/:userId', async (req, res) => {
+  try {
+    const userFriendsList = await getUserFriendsList(req.params.userId);
+    const onlineFriends = await getOnlineFriends(userFriendsList);
+    
+    const friendsInfo = userFriendsList.map((friend) => {
+      const findFriend = onlineFriends.find((onlineUser) => onlineUser.username == friend);
+      
+      const friendObj = { username: friend };
+      if (findFriend) {
+        friendObj.online = true;
+        friendObj.socketId = findFriend.socketId;
+      } else {
+        friendObj.online = false;
+      }
+
+      return friendObj;
+    });
+
+    res.json(friendsInfo);
   } catch (err) {
     const parsed = parseError(err);
     res.status(400).json({ code: 400, message: parsed.message });
