537e8abfb8643455e944b1ddeb44d5ca8db8bdae
Krasen Boychev <krasenboychev11@gmail.com>
2025-03-08 20:37:46 +0000

    getOnlineFriends() service created

--- server/src/services/onlineUsers.js
+++ server/src/services/onlineUsers.js
@@ -1,7 +1,17 @@
 const { OnlineUsers } = require('../models/OnlineUsers');
 
+async function getOnlineUsers() {
+  return await OnlineUsers.findById('67cc1e18fa8c045b3ea31034');
+}
+
+async function getOnlineFriends(friendsList) {
+  return await OnlineUsers.find({ "onlineUsers.username": { $in: friendsList } },
+    { "onlineUsers.username": 1 }
+  ).distinct("onlineUsers");
+}
+
 async function addNewOnlineUser(username, socketId) {
-  const record = await OnlineUsers.findById('67cabdb2203cb80f915b8cff');
+  const record = await getOnlineUsers();
 
   !record.onlineUsers.some((user) => user.username === username) &&
     record.onlineUsers.push({ username, socketId });
@@ -12,13 +22,13 @@ async function addNewOnlineUser(username, socketId) {
 }
 
 async function removeOnlineUser(socketId) {
-    const record = await OnlineUsers.findById('67cabdb2203cb80f915b8cff');
-  
-    record.onlineUsers = record.onlineUsers.filter((user) => user.socketId !== socketId);
-  
-    await record.save();
-  
-    return record;
-  }
+  const record = await getOnlineUsers();
 
-module.exports = { addNewOnlineUser, removeOnlineUser };
+  record.onlineUsers = record.onlineUsers.filter((user) => user.socketId !== socketId);
+
+  await record.save();
+
+  return record;
+}
+
+module.exports = { getOnlineUsers, getOnlineFriends, addNewOnlineUser, removeOnlineUser };
