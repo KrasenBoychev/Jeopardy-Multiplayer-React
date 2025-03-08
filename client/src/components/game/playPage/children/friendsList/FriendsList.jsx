98b5438e9310e727242872eadde0516ca4e46f61
Krasen Boychev <krasenboychev11@gmail.com>
2025-03-08 20:40:28 +0000

    getting all friends when component renders; sending request to online friends that the user is online

--- client/src/components/game/playPage/children/friendsList/FriendsList.jsx
+++ client/src/components/game/playPage/children/friendsList/FriendsList.jsx
@@ -1,52 +1,57 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useAuthContext } from "../../../../../contexts/AuthContext";
+import { getFriendsOnline } from "../../../../../../api/requester";
 import "./friendsList.css";
 
-export default function FriendsList() {
-  const [friendOnline, setFriendOnline] = useState(true);
+export default function FriendsList({ socket }) {
+  const { userId, username, isAuthenticated } = useAuthContext();
+  const [friendsList, setFriendsList] = useState([]);
+
+
+  useEffect(() => {
+    (async function getFriends() {
+      if (isAuthenticated) {
+        const allFriends = await getFriendsOnline(userId);
+        setFriendsList(allFriends);
+
+        const onlineFriends = allFriends.filter((friend) => friend.online == true);
+        if (onlineFriends.length > 0) {
+          await socket.emit("sendUserIsOnline", {
+            senderInfo: { username, socketId: socket.id },
+            receiverFriends: onlineFriends,
+          });
+        }
+      }
+    })();
+  }, []);
+
+  // update state dinamically when log out
+  // check how to avoid the error when refreshing the page
+
+  useEffect(() => {
+    socket?.on("getFriendIsOnline", ({ senderInfo }) => {
+      setFriendsList((prevFriendList) => {
+        const newUpdatedFriendsLst = prevFriendList.filter((friend) => friend.username !== senderInfo.username);
+        newUpdatedFriendsLst.push({ username: senderInfo.username, online: true, socketId: senderInfo.socketId });
+        return newUpdatedFriendsLst;
+      });
+    });
+  }, [socket]);
+
 
   return (
     <div className="friends_list_wrapper">
       <h3>Friends List</h3>
       <ul>
-        <li className={friendOnline ? "friend_online" : "friend_offline"}>
-          <span className="friend_username">
-            Someoneeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
-          </span>
-          {friendOnline && <span className="friend_online_play">Invite</span>}
-          <span className="friend_status">Online</span>
-        </li>
-
-        <li className={!friendOnline ? "friend_online" : "friend_offline"}>
-          <span className="friend_username">
-            Someoneeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
-          </span>
-          {!friendOnline && <span className="friend_online_play">Invite</span>}
-          <span className="friend_status">Offline</span>
-        </li>
-
-        <li className={friendOnline ? "friend_online" : "friend_offline"}>
-          <span className="friend_username">
-            Someoneeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
-          </span>
-          {friendOnline && <span className="friend_online_play">Invite</span>}
-          <span className="friend_status">Online</span>
-        </li>
-
-        <li className={!friendOnline ? "friend_online" : "friend_offline"}>
-          <span className="friend_username">
-            Someoneeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
-          </span>
-          {!friendOnline && <span className="friend_online_play">Invite</span>}
-          <span className="friend_status">Offline</span>
-        </li>
-
-        <li className={friendOnline ? "friend_online" : "friend_offline"}>
-          <span className="friend_username">
-            Someoneeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
-          </span>
-          {friendOnline && <span className="friend_online_play">Invite</span>}
-          <span className="friend_status">Online</span>
-        </li>
+        {friendsList.map((friend) => {
+          return <li key={friend.username} className={friend.online ? "friend_online" : "friend_offline"}>
+            <span className="friend_username">
+              {friend.username}
+            </span>
+            {friend.online && <span className="friend_online_play">Invite</span>}
+            <span className="friend_status">{friend.online ? 'Online' : 'Offline'}</span>
+          </li>
+        })}
       </ul>
       <p className="add_friend">
         <input type="text" placeholder="Friend Username" />
