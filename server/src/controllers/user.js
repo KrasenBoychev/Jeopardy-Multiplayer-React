fe6dbbbc416bdc7872748dd327d41499ecb0a668
Krasen Boychev <krasenboychev11@gmail.com>
2025-03-09 19:12:30 +0000

    checkUser controller check if the friendUsername exists, if yes, it check if the friend is online; in any case it sends the correct response

--- server/src/controllers/user.js
+++ server/src/controllers/user.js
@@ -5,6 +5,7 @@ const {
   getPlayerPoints,
   getUserByUsername,
 } = require('../services/user');
+const { checkIfUserIsOnline } = require('../services/onlineUsers');
 
 const userRouter = Router();
 
@@ -30,8 +31,41 @@ userRouter.get('/playerPoints/:userId', async (req, res) => {
 
 userRouter.get('/checkUser/:username', async (req, res) => {
   try {
-    const user = await getUserByUsername(req.params.username);
-    res.json(user);
+    const result = {status: '', msg: ''};
+
+    const friendUsername = req.params.username;
+    const userUsername = req.user.username;
+
+    const user = await getUserByUsername(friendUsername);
+
+    if (user.length > 0) {
+      const friend = user[0];
+
+      if (friend.friendRequests.includes(userUsername)) {
+        result.status = 'error';
+        result.msg = 'Invitation has already been sent to ' + friendUsername;
+
+      } else {
+        const userOnline = await checkIfUserIsOnline(friendUsername);
+
+        if (userOnline) {
+          result.status = 'send invitation';
+          result.msg = userOnline.onlineUsers[0];
+        } else {
+          friend.friendRequests.push(userUsername);
+          await friend.save();
+
+          result.status = 'success';
+          result.msg = 'Invitation sent to ' + friendUsername;
+        }
+
+      }
+    } else {
+      result.status = 'error';
+      result.msg = friendUsername + ' does not exist';
+    }
+
+    res.json(result);
   } catch (err) {
     const parsed = parseError(err);
     res.status(400).json({ code: 400, message: parsed.message });
