2d6bfba652569d03b2cceb41688f6cd16f90eda9
Krasen Boychev <krasenboychev11@gmail.com>
2025-03-09 12:26:13 +0000

    Logout.jsx notifies online friends that the user has logged out

--- client/src/components/authentication/Logout.jsx
+++ client/src/components/authentication/Logout.jsx
@@ -1,10 +1,25 @@
 import { Navigate } from "react-router-dom";
-
 import { useLogout } from "../../hooks/useAuth";
+import { getFriends } from "../../hooks/useSocket";
+import { useAuthContext } from "../../contexts/AuthContext";
+import { useEffect } from "react";
 
-export default function Logout() {
+export default function Logout(props) {
+  const { socket, setSocket } = props.socketProps;
+  const { friendsList, setFriendsList } = props.friendsProps;
+
+  const { username, userId } = useAuthContext();
   const logout = useLogout();
-  logout();
+  const logoutValue = true;
 
+  useEffect(() => {
+    (async function logoutUser() {
+      await getFriends(socket, setFriendsList, userId, username, logoutValue, friendsList);
+      await socket.disconnect();
+      setSocket(null);
+      logout();
+    })();
+  }, [])
+  
   return <Navigate to="/" />;
 }
