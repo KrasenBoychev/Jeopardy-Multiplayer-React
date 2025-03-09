3e11a1cd303bf55eaa3312c4c947bf6c33206069
Krasen Boychev <krasenboychev11@gmail.com>
2025-03-09 12:25:31 +0000

    FriendsList.jsx accepts only friendsList as prop

--- client/src/components/game/playPage/PlayPage.jsx
+++ client/src/components/game/playPage/PlayPage.jsx
@@ -1,14 +1,16 @@
+import { useState } from "react";
 import FriendsList from "./children/friendsList/FriendsList";
 import StartGameWithOtherPlayer from "./children/startGameWithOtherPlayer/StartGameWithOtherPlayer";
 import GameRoom from "./children/gameRoom/GameRoom";
 
 import "./playPage.css";
 
-export default function PlayPage({ socket }) {
+export default function PlayPage(props) {
+
   return (
     <div className="play_page_container">
       <section>
-        <FriendsList socket={socket} />
+        <FriendsList friendsList={props.friendsProps.friendsList} />
         <StartGameWithOtherPlayer />
       </section>
       <section>
