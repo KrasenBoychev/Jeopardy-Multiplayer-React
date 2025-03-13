bbc2d5e8927c7869cd56ae3b6198d55c02f16c4c
Krasen Boychev <krasenboychev11@gmail.com>
2025-03-13 15:59:26 +0000

    aceppt and reject btns send a server request

--- client/src/components/core/header/notificationsBox/NotificationsBox.jsx
+++ client/src/components/core/header/notificationsBox/NotificationsBox.jsx
@@ -1,6 +1,28 @@
+import { sendFriendResponse } from "../../../../../api/user-api";
 import "./notificationsBox.css";
 
 export default function NotificationsBox({ notificationsList }) {
+
+  const acceptNotification = async (e) => {
+    if (e.target.value == 'friendRequest') {
+     const response = await sendFriendResponse(JSON.stringify({username: e.target.id, status: 'friendRequestAccepted'}));
+    }
+    // check if the friend is online and send socketrequest to update their friendList in the component
+    // if not online, send them a notification saying your accepted their request
+  }
+
+  const rejectNotification = async (e) => {
+    if (e.target.value == 'friendRequest') {
+      const response = await sendFriendResponse(JSON.stringify({username: e.target.id, status: 'friendRequestRejected'}));
+    }
+    // check if the friend is online and send socketrequest to update their friendList in the component
+    // if not online, send them a notification saying your rejected their request
+  }
+
+  //send invitation - check if the other is in your fRequests => if yes, don't send a fr request
+
+  // add new button - Read - only for standart notification which don't have to accepted or rejected
+
   return (
     <div className="notifications_box_container">
       {notificationsList.length > 0
@@ -10,8 +32,8 @@ export default function NotificationsBox({ notificationsList }) {
             return <li key={notification.username}>
               {notification.username + notification.content}
               <div className="notifications_box_btns">
-                <button>Accept</button>
-                <button>Reject</button>
+                <button value={notification.type} id={notification.username} onClick={acceptNotification}>Accept</button>
+                <button value={notification.type} id={notification.username} onClick={rejectNotification}>Reject</button>
               </div>
             </li>
           })}
