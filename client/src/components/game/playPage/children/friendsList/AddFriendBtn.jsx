92df7bd19d5c88531a11bfcb3ecbcbebb8b2d183
Krasen Boychev <krasenboychev11@gmail.com>
2025-03-14 18:28:22 +0000

    AddFriend.jsx created

---
+++ client/src/components/game/playPage/children/friendsList/AddFriendBtn.jsx
@@ -0,0 +1,53 @@
+import { toast } from "react-hot-toast";
+import { sendFriendRequest } from "../../../../../../api/user-api";
+import { useAuthContext } from "../../../../../contexts/AuthContext";
+
+export default function AddFriendBtn({ addFriendUsername, socket, friendsList }) {
+    const { username } = useAuthContext();
+
+    const sendFriendInvitation = async () => {
+        if (!addFriendUsername.trim()) {
+            return;
+        } else if (addFriendUsername == username) {
+            toast.error('Cannot add yourself');
+            return;
+        }
+
+        let isUsernameInFriendList = false;
+        friendsList.forEach(friend => {
+            if (friend.username == addFriendUsername) {
+                toast.error(addFriendUsername + ' is in your Friends List');
+                isUsernameInFriendList = true;
+                return;
+            }
+        });
+
+        if (isUsernameInFriendList) {
+            return;
+        }
+
+        try {
+            const friendCheckResponse = await sendFriendRequest(addFriendUsername);
+
+            if (friendCheckResponse.status == 'success') {
+                toast.success(friendCheckResponse.msg);
+
+            } else if (friendCheckResponse.status == 'error') {
+                toast.error(friendCheckResponse.msg);
+
+            } else if (friendCheckResponse.status == 'send invitation') {
+                await socket.emit("sendNotification", {
+                    receiverSocketId: friendCheckResponse.friendDetails.socketId,
+                });
+                toast.success(friendCheckResponse.msg);
+            }
+
+        } catch (error) {
+            toast.error(error.message);
+        }
+    }
+
+    return (
+        <button onClick={sendFriendInvitation}>Add Friend</button>
+    );
+}
