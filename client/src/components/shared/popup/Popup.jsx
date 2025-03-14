642915d40364358da969bf95d78dfffa7bafa90f
Krasen Boychev <krasenboychev11@gmail.com>
2025-03-14 18:29:48 +0000

    Popup.jsx and popup.css created

---
+++ client/src/components/shared/popup/Popup.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import Popup from "reactjs-popup";
+import "./popup.css";
+
+export default function PopupComp({ openBtnName, heading, content }) {
+  return (
+    <Popup
+      trigger={<button className="button">{openBtnName}</button>}
+      modal
+      nested
+    >
+      {(close) => (
+        <div className="modal">
+          <button className="close" onClick={close}>
+            &times;
+          </button>
+          <div className="header">{heading}</div>
+          <div className="content">{content}</div>
+          <div className="actions">
+            <button
+              className="button"
+              onClick={() => {
+                close();
+              }}
+            >
+              Close
+            </button>
+          </div>
+        </div>
+      )}
+    </Popup>
+  );
+}
