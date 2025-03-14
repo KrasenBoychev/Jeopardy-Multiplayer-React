679b81f9b596884f7c760f22beb1e28a1e69ecd3
Krasen Boychev <krasenboychev11@gmail.com>
2025-03-14 09:07:45 +0000

    getUserNotificationsList service created

--- server/src/services/user.js
+++ server/src/services/user.js
@@ -1,6 +1,10 @@
 const { adminId } = require('../api-keys');
 const { User } = require('../models/User');
 
+async function getUserByUsername(username) {
+  return await User.find({ username });
+}
+
 async function getTopPlayers() {
   return await User.find({ _id: { $nin: [adminId] } })
     .sort({ points: -1 })
@@ -19,14 +23,15 @@ async function getUserFriendRequests(userId) {
   return await User.find({ _id: userId }).distinct('friendRequests');
 }
 
-async function getUserByUsername(username) {
-  return await User.find({ username });
+async function getUserNotificationsList(username) {
+  return await User.find({ username }).distinct('notificationsList');
 }
 
 module.exports = {
+  getUserByUsername,
   getTopPlayers,
   getPlayerPoints,
   getUserFriendsList,
   getUserFriendRequests,
-  getUserByUsername,
+  getUserNotificationsList,
 };
