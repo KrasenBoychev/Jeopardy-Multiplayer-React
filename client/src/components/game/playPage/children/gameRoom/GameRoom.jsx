cd69324fb65af22f9ecaf1d681ed8a433d1dee28
Krasen Boychev <krasenboychev11@gmail.com>
2025-03-14 18:30:12 +0000

    InviteFriend.jsx and Popup.jsx imported - logic completed

--- client/src/components/game/playPage/children/gameRoom/GameRoom.jsx
+++ client/src/components/game/playPage/children/gameRoom/GameRoom.jsx
@@ -1,24 +1,38 @@
+import { useAuthContext } from "../../../../../contexts/AuthContext";
+import InviteFriend from "./inviteFriend/InviteFriendBtn";
 import "./gameRoom.css";
 
-export default function GameRoom() {
+export default function GameRoom({ socket, friendProps }) {
+  const { friendInvited, gameFriendResponse } = friendProps;
+  const { username } = useAuthContext();
+
   return (
     <div className="game_room_wrapper">
       <h3>Game Room</h3>
       <div className="game_room_players">
         <p>
           <span>Player 1:</span>
-          <span>
-            asdaaacadasdasdasdasdadasdasdasdasdadasdasdasdasdasdasdasd
-          </span>
+          <span>{username}</span>
         </p>
         <p>
           <span>Player 2:</span>
           <span>
-            asdaaacadasdasdasdasdadasdasdasdasdadasdasdasdasdasdasdasd
+            {friendInvited ? (
+              friendInvited
+            ) : (
+              <InviteFriend socket={socket} friendProps={friendProps} />
+            )}
+
+            {friendInvited && !gameFriendResponse && <button>'Cancel'</button>}
           </span>
         </p>
       </div>
-      <button className="game_room_btn">Play</button>
+      <p className="game_room_btn">
+        {friendInvited &&
+          !gameFriendResponse &&
+          `Waiting for ${friendInvited} to respond`}
+        {friendInvited && gameFriendResponse && "Loading Game"}
+      </p>
     </div>
   );
 }
