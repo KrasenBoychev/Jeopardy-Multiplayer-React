6b1172f7fdc81bde4cd7be31c48a3189076cd15b
Krasen Boychev <krasenboychev11@gmail.com>
2025-03-15 20:32:30 +0000

    InvitedFriendBtn.jsx updated

--- client/src/components/game/playPage/children/gameRoom/inviteFriend/InviteFriendBtn.jsx
+++ client/src/components/game/playPage/children/gameRoom/inviteFriend/InviteFriendBtn.jsx
@@ -1,14 +1,33 @@
 import { useEffect, useState } from "react";
 import PopupComp from "../../../../../shared/popup/Popup";
 import "./inviteFriend.css";
+import toast from "react-hot-toast";
+import { useAuthContext } from "../../../../../../contexts/AuthContext";
 
 export default function InviteFriend({ socket, friendProps }) {
   const { friendsList, friendInvited, setFriendInvited, gameFriendResponse } =
     friendProps;
 
-  const inviteFriendToGameRoom = (e) => {    
-    // send socket request - invitation
-    setFriendInvited(e.target.id);
+  const { username } = useAuthContext();
+
+  const inviteFriendToGameRoom = async (e) => {
+    const friendUsername = e.target.id;
+    const findFriend = friendsList.find(
+      (friend) => friend.username == friendUsername
+    );
+
+    if (findFriend && findFriend.online == true) {
+      await socket.emit("sendGameInvitation", {
+        receiverSocketId: findFriend.socketId,
+        data: { username, socketId: socket.id },
+      });
+
+      setFriendInvited(friendUsername);
+    } else {
+      toast.error(
+        `Sending invitation to ${friendUsername} was not successful. Please refresh the page`
+      );
+    }
   };
 
   const openBtnName = "+";
@@ -18,7 +37,15 @@ export default function InviteFriend({ socket, friendProps }) {
     <ul>
       {friendsList.map((friend) => {
         if (friend.online) {
-          return <li key={friend.username} id={friend.username} onClick={inviteFriendToGameRoom}>{friend.username}</li>;
+          return (
+            <li
+              key={friend.username}
+              id={friend.username}
+              onClick={inviteFriendToGameRoom}
+            >
+              {friend.username}
+            </li>
+          );
         }
       })}
     </ul>
@@ -33,12 +60,6 @@ export default function InviteFriend({ socket, friendProps }) {
           content={popupContent}
         />
       )}
-
-      {/* (
-         <button className="friend_online_play" onClick={inviteFriendToGameRoom}>
-           Invite
-         </button>
-      ) */}
     </>
   );
 }
