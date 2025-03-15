42b39aa11fd1ea03df384f44275b0d0f71920981
Krasen Boychev <krasenboychev11@gmail.com>
2025-03-15 20:31:32 +0000

    services added and updated

--- server/src/services/onlineUsers.js
+++ server/src/services/onlineUsers.js
@@ -1,11 +1,12 @@
-const { OnlineUsers } = require('../models/OnlineUsers');
+const { OnlineUsers } = require("../models/OnlineUsers");
 
 async function getOnlineUsers() {
-  return await OnlineUsers.findById('67cc1e18fa8c045b3ea31034');
+  return await OnlineUsers.findById("67cc1e18fa8c045b3ea31034");
 }
 
 async function getOnlineFriends(friendsList) {
-  return await OnlineUsers.find({ "onlineUsers.username": { $in: friendsList } },
+  return await OnlineUsers.find(
+    { "onlineUsers.username": { $in: friendsList } },
     { "onlineUsers.username": 1 }
   ).distinct("onlineUsers");
 }
@@ -14,17 +15,32 @@ async function addNewOnlineUser(username, socketId) {
   const record = await getOnlineUsers();
 
   !record.onlineUsers.some((user) => user.username === username) &&
-    record.onlineUsers.push({ username, socketId });
+    record.onlineUsers.push({ username, socketId, gameInProgress: false });
 
   await record.save();
 
   return record;
 }
 
+async function changeOnlineUserGameInProgress(usernames, socketId) {
+  const friends = await getOnlineFriends(usernames);
+
+  friends = friends.map((friend) => friend.gameInProg)
+
+  // !record.onlineUsers.some((user) => user.username === username) &&
+  //   record.onlineUsers.push({ username, socketId, gameInProgress: false });
+
+  // await record.save();
+
+  // return record;
+}
+
 async function removeOnlineUser(socketId) {
   const record = await getOnlineUsers();
 
-  record.onlineUsers = record.onlineUsers.filter((user) => user.socketId !== socketId);
+  record.onlineUsers = record.onlineUsers.filter(
+    (user) => user.socketId !== socketId
+  );
 
   await record.save();
 
@@ -32,11 +48,14 @@ async function removeOnlineUser(socketId) {
 }
 
 async function checkIfUserIsOnline(username) {
-  return await OnlineUsers.findOne({
-    "onlineUsers.username": username
-  }, {
-    "onlineUsers.$": 1
-  });
+  return await OnlineUsers.findOne(
+    {
+      "onlineUsers.username": username,
+    },
+    {
+      "onlineUsers.$": 1,
+    }
+  );
 }
 
 module.exports = {
@@ -45,4 +64,5 @@ module.exports = {
   addNewOnlineUser,
   removeOnlineUser,
   checkIfUserIsOnline,
+  changeOnlineUserGameInProgress,
 };
