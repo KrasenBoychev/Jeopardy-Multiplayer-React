c43e8e8e0f2a98171b317b6b232a5bbaa33ebada
Krasen Boychev <krasenboychev11@gmail.com>
2025-03-15 20:37:09 +0000

    PlayPage.jsx updated

--- client/src/components/game/playPage/PlayPage.jsx
+++ client/src/components/game/playPage/PlayPage.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import FriendsList from "./children/friendsList/FriendsList";
 import StartGameWithOtherPlayer from "./children/startGameWithOtherPlayer/StartGameWithOtherPlayer";
 import GameRoom from "./children/gameRoom/GameRoom";
@@ -6,13 +6,27 @@ import GameRoom from "./children/gameRoom/GameRoom";
 import "./playPage.css";
 
 export default function PlayPage({ socket, friendsProps }) {
-  const { friendsList, gameFriendResponse } = friendsProps;
-  const [friendInvited, setFriendInvited] = useState(null);
+  const {
+    friendsList,
+    gameFriendResponse,
+    setGameFriendResponse,
+    friendInvited,
+    setFriendInvited,
+  } = friendsProps;
 
   return (
     <div className="play_page_container">
       <section>
-        <GameRoom socket={socket} friendProps={{ friendsList, friendInvited, setFriendInvited, gameFriendResponse }} />
+        <GameRoom
+          socket={socket}
+          friendProps={{
+            friendsList,
+            friendInvited,
+            setFriendInvited,
+            gameFriendResponse,
+            setGameFriendResponse
+          }}
+        />
       </section>
       <section>
         <FriendsList
