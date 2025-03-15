482e37d973a792e6f9de4c726bc0cf5bfbc94227
Krasen Boychev <krasenboychev11@gmail.com>
2025-03-15 20:32:12 +0000

    useSocket updated

--- client/src/hooks/useSocket.js
+++ client/src/hooks/useSocket.js
@@ -4,50 +4,72 @@ import { getFriendsOnline } from "../../api/user-api";
 import { useAuthContext } from "../contexts/AuthContext";
 
 export default function useSocket(socket, setSocket, setFriendsList) {
-  const { isAuthenticated, userId, username } = useAuthContext();
+  const { isAuthenticated, username } = useAuthContext();
 
-    useEffect(() => {
-        (async function socketService() {
-          if (isAuthenticated) {
-            const createSocket = io("http://localhost:5000");
-            const logout = false;
-            await getFriends(createSocket, setFriendsList, userId, username, logout);
-            setSocket(createSocket);
-          }
-        })();
-      }, [isAuthenticated]);
-    
-      useEffect(() => {
-        socket?.emit("newUser", username);
-    
-        socket?.on("getFriendStatus", ({ senderInfo, logout }) => {
-          setFriendsList((prevFriendList) => {
-            const newUpdatedFriendsLst = prevFriendList.filter((friend) => friend.username !== senderInfo.username);
-            if (!logout) {
-              newUpdatedFriendsLst.push({ username: senderInfo.username, online: true, socketId: senderInfo.socketId });
-            } else {
-              newUpdatedFriendsLst.push({ username: senderInfo.username, online: false});
-            }
-            return newUpdatedFriendsLst;
+  useEffect(() => {
+    (async function socketService() {
+      if (isAuthenticated) {
+        const createSocket = io("http://localhost:5000");
+        const action = "";
+        await getFriends(createSocket, setFriendsList, username, action);
+        setSocket(createSocket);
+      }
+    })();
+  }, [isAuthenticated]);
+
+  useEffect(() => {
+    socket?.emit("newUser", username);
+
+    socket?.on("getFriendStatus", ({ senderInfo, action }) => {
+      setFriendsList((prevFriendList) => {
+        const newUpdatedFriendsLst = prevFriendList.filter(
+          (friend) => friend.username !== senderInfo.username
+        );
+        if (action == "") {
+          newUpdatedFriendsLst.push({
+            username: senderInfo.username,
+            online: true,
+            socketId: senderInfo.socketId,
           });
-        });
-      }, [socket]);
+        } else if (action == "logout") {
+          newUpdatedFriendsLst.push({
+            username: senderInfo.username,
+            online: false,
+          });
+        } else if (action == "changeToGameInProgress") {
+          newUpdatedFriendsLst.push({
+            username: senderInfo.username,
+            gameInProgress: 'gameInPrgress',
+            socketId: senderInfo.socketId,
+          });
+        }
+
+        return newUpdatedFriendsLst;
+      });
+    });
+  }, [socket]);
 }
 
-export async function getFriends(socket, setFriendsList, userId, username, logout, friendsList) {
-    let allFriends = friendsList;
-  
-    if (!logout) {
-      allFriends = await getFriendsOnline();
-      setFriendsList(allFriends);
-    }
-  
-    const onlineFriends = allFriends.filter((friend) => friend.online == true);
-    if (onlineFriends.length > 0) {
-      await socket.emit("sendUserStatus", {
-        senderInfo: { username, socketId: socket.id },
-        receiverFriends: onlineFriends,
-        logout
-      });
-    }
+export async function getFriends(
+  socket,
+  setFriendsList,
+  username,
+  action,
+  friendsList
+) {
+  let allFriends = friendsList;
+
+  if (action == '') {
+    allFriends = await getFriendsOnline();
+    setFriendsList(allFriends);
   }
+
+  const onlineFriends = allFriends.filter((friend) => friend.online == true);
+  if (onlineFriends.length > 0) {
+    await socket.emit("sendUserStatus", {
+      senderInfo: { username, socketId: socket.id },
+      receiverFriends: onlineFriends,
+      action,
+    });
+  }
+}
