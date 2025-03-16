d279c7598ad83ccdee6a5a7ff00d6666a60260da
Krasen Boychev <krasenboychev11@gmail.com>
2025-03-16 20:02:55 +0000

    onlineUsers service updated

--- server/src/services/onlineUsers.js
+++ server/src/services/onlineUsers.js
@@ -1,31 +1,45 @@
-const { OnlineUsers } = require("../models/OnlineUsers");
+const { OnlineUser } = require("../models/OnlineUsers");
 
-async function getOnlineUsers() {
-  return await OnlineUsers.findById("67cc1e18fa8c045b3ea31034");
+async function getOnlineUserDetails(username) {
+  //used
+  return await OnlineUser.findOne({ username });
 }
 
-async function getOnlineFriends(friendsList) {
-  return await OnlineUsers.find(
-    { "onlineUsers.username": { $in: friendsList } },
-    { "onlineUsers.username": 1 }
-  ).distinct("onlineUsers");
+async function getOnlineUsers(friendsList) {
+  //used
+  return await OnlineUser.find({ username: { $in: friendsList } });
 }
 
 async function addNewOnlineUser(username, socketId) {
-  const record = await getOnlineUsers();
+  //used
+  const existingOnlineUser = await OnlineUser.findOne({ username });
 
-  !record.onlineUsers.some((user) => user.username === username) &&
-    record.onlineUsers.push({ username, socketId, gameInProgress: false });
+  if (existingOnlineUser) {
+    existingOnlineUser.socketId = socketId;
+    await existingOnlineUser.save();
 
-  await record.save();
+    return existingOnlineUser;
+  } else {
+    const userOnline = new OnlineUser({
+      username,
+      socketId,
+      gameInProgress: false,
+    });
+    await userOnline.save();
 
-  return record;
+    return userOnline;
+  }
+}
+
+async function deleteOnlineUser(username) {
+  //used
+  await OnlineUser.deleteOne({ username });
 }
 
 async function changeOnlineUserGameInProgress(usernames, socketId) {
   const friends = await getOnlineFriends(usernames);
 
-  friends = friends.map((friend) => friend.gameInProg)
+  friends = friends.map((friend) => friend.gameInProg);
 
   // !record.onlineUsers.some((user) => user.username === username) &&
   //   record.onlineUsers.push({ username, socketId, gameInProgress: false });
@@ -36,33 +50,19 @@ async function changeOnlineUserGameInProgress(usernames, socketId) {
 }
 
 async function removeOnlineUser(socketId) {
-  const record = await getOnlineUsers();
-
-  record.onlineUsers = record.onlineUsers.filter(
-    (user) => user.socketId !== socketId
-  );
-
-  await record.save();
-
-  return record;
-}
-
-async function checkIfUserIsOnline(username) {
-  return await OnlineUsers.findOne(
-    {
-      "onlineUsers.username": username,
-    },
-    {
-      "onlineUsers.$": 1,
-    }
-  );
+  // const record = await getOnlineUser();
+  // record.onlineUsers = record.onlineUsers.filter(
+  //   (user) => user.socketId !== socketId
+  // );
+  // await record.save();
+  // return record;
 }
 
 module.exports = {
+  getOnlineUserDetails,
   getOnlineUsers,
-  getOnlineFriends,
   addNewOnlineUser,
+  deleteOnlineUser,
   removeOnlineUser,
-  checkIfUserIsOnline,
   changeOnlineUserGameInProgress,
 };
