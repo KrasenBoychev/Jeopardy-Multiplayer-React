2d7f83572299b8680818fc486fa43641678d5bca
Krasen Boychev <krasenboychev11@gmail.com>
2025-03-16 20:07:53 +0000

    configRoutes updated

--- server/src/config/configRoutes.js
+++ server/src/config/configRoutes.js
@@ -2,14 +2,14 @@ const { authRouter } = require('../controllers/auth');
 const { userRouter } = require('../controllers/user');
 const { gameRouter } = require('../controllers/game');
 const { createRouter } = require('../controllers/create');
-const { onlineUsersRouter } = require('../controllers/onlineUsers');
+const { friendsRouter } = require('../controllers/friends');
 
 function configRoutes(app) {
   app.use('/auth', authRouter)
   app.use('/users', userRouter);
   app.use('/play', gameRouter);
   app.use('/create', createRouter);
-  app.use('/onlineUsers', onlineUsersRouter);
+  app.use('/friends', friendsRouter);
 }
 
 module.exports = { configRoutes };
