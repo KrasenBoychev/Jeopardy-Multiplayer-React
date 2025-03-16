e73492232baa8fd00ea97f69899ce061828a19ae
Krasen Boychev <krasenboychev11@gmail.com>
2025-03-16 20:04:10 +0000

    auth service uses user services

--- server/src/services/auth.js
+++ server/src/services/auth.js
@@ -1,14 +1,15 @@
 const { User } = require('../models/User');
 const bcrypt = require('bcrypt');
+const { getUserByEmail, getUserByUsername } = require('./user');
 
 async function register(email, username, password) {
-    const existingEmail = await User.findOne({ email });
+    const existingEmail = await getUserByEmail(email)
 
     if (existingEmail) {
         throw new Error('This email is already in use');
     }
 
-    const existingUsername = await User.findOne({ username });
+    const existingUsername = await getUserByUsername(username);
 
     if (existingUsername) {
         throw new Error('This username is already in use');
@@ -27,7 +28,7 @@ async function register(email, username, password) {
 }
 
 async function login(email, password) {
-    const user = await User.findOne({ email });
+    const user = await getUserByEmail(email);
 
     if (!user) {
         throw new Error('Incorrect email or password');
