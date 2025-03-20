893cda17a75495f7d4c84109641a8af2b31b8b46
Krasen Boychev <krasenboychev11@gmail.com>
2025-03-20 12:49:47 +0000

    user-api updated

--- client/api/user-api.js
+++ client/api/user-api.js
@@ -3,30 +3,28 @@ import * as api from "./requester.js";
 const host = api.settings.host;
 
 export async function getTopPlayers() {
-  //used
   return await api.get(host + "/users/topPlayers");
 }
 
 export async function getPlayerPoints() {
-  //used
   return await api.get(host + `/users/playerPoints`);
 }
 
 export async function recordUserInOnlineUsers(username, socketId) {
-  //used
   return await api.post(host + `/users/recordNewUser`, { username, socketId });
 }
 
 export async function deleteUserInOnlineUsers() {
-  //used
   await api.del(host + `/users/deleteUser`);
 }
 
 export async function getUserNotifications() {
-  //used
   return await api.get(host + `/users/getUserNotifications`);
 }
 
-export async function removeNotification(friendUsername) {
-  return await api.get(host + `/users/removeNotification/${friendUsername}`);
+export async function removeNotification(friendUsername, type) {
+  return await api.put(host + `/users/removeNotification`, {
+    friendUsername,
+    type,
+  });
 }
