d4b2dd0afd9eaf360ca4115c092edcb42b8cf7fb
Krasen Boychev <krasenboychev11@gmail.com>
2025-03-20 12:49:31 +0000

    user services updated

--- server/src/services/user.js
+++ server/src/services/user.js
@@ -2,34 +2,39 @@ const { adminId } = require("../api-keys");
 const { User } = require("../models/User");
 
 async function getUserByEmail(email) {
-  //used
   return await User.findOne({ email });
 }
 
 async function getUserByUsername(username) {
-  //used
   return await User.findOne({ username });
 }
 
 async function getTopPlayers() {
-  //used
   return await User.find({ _id: { $nin: [adminId] } })
     .sort({ points: -1 })
     .limit(10);
 }
 
 async function getPlayerPoints(username) {
-  //used
   return await User.find({ username }).distinct("points");
-}
-
-async function getUserFriendsList(username) {
-  //used
-  return await User.find({ username }).distinct("friendsList");
 }
 
 async function getUserNotificationsList(username) {
   return await User.find({ username }).distinct("notificationsList");
+}
+
+async function addNotification(userUsername, notification) {
+  return await User.updateOne(
+    { username: userUsername },
+    { $push: { notificationsList: notification } }
+  );
+}
+
+async function removeNotification(userUsername, type, friendUsername) {
+  return await User.updateOne(
+    { username: userUsername },
+    { $pull: { notificationsList: { username: friendUsername, type } } }
+  );
 }
 
 module.exports = {
@@ -37,7 +42,7 @@ module.exports = {
   getUserByUsername,
   getTopPlayers,
   getPlayerPoints,
-  getUserFriendsList,
-  // getUserFriendRequests,
   getUserNotificationsList,
+  addNotification,
+  removeNotification,
 };
