c67a1102226a97521f15ede7f39b08e38140bef9
Krasen Boychev <krasenboychev11@gmail.com>
2025-03-20 22:04:51 +0000

    inviteFriendToGameRoom logic expanded and almost completed

--- client/src/components/game/playPage/children/gameRoom/inviteFriend/InviteFriendBtn.jsx
+++ client/src/components/game/playPage/children/gameRoom/inviteFriend/InviteFriendBtn.jsx
@@ -1,8 +1,8 @@
-import { useEffect, useState } from "react";
-import PopupComp from "../../../../../shared/popup/Popup";
-import "./inviteFriend.css";
 import toast from "react-hot-toast";
 import { useAuthContext } from "../../../../../../contexts/AuthContext";
+import { updateGameInProgress } from "../../../../../../../api/user-api";
+import PopupComp from "../../../../../shared/popup/Popup";
+import "./inviteFriend.css";
 
 export default function InviteFriend({ socket, friendProps }) {
   const { friendsList, friendInvited, setFriendInvited, gameFriendResponse } =
@@ -16,27 +16,41 @@ export default function InviteFriend({ socket, friendProps }) {
       (friend) => friend.username == friendUsername
     );
 
-    if (findFriend && findFriend.online == true) {
+    if (findFriend && findFriend.online && !findFriend.gameInProgress) {
+      await updateGameInProgress();
+
+      const receiverFriends = friendsList.filter(
+        (friend) =>
+          friend.username !== friendUsername &&
+          friend.online &&
+          !friend.gameInProgress
+      );
+
+      await socket.emit("sendUserStatus", {
+        senderInfo: { username, gameInProgress: true },
+        receiverFriends,
+        action: "changeGameInProgress",
+      });
+
       await socket.emit("sendGameInvitation", {
         receiverSocketId: findFriend.socketId,
-        data: { username, socketId: socket.id },
+        username,
       });
 
       setFriendInvited(friendUsername);
     } else {
       toast.error(
-        `Sending invitation to ${friendUsername} was not successful. Please refresh the page`
+        `${friendUsername} is either offline or in game. Please refresh the page`
       );
     }
   };
 
   const openBtnName = "+";
   const popupHeading = "Friends Online";
-
   const popupContent = (
     <ul>
       {friendsList.map((friend) => {
-        if (friend.online) {
+        if (friend.online && !friend.gameInProgress) {
           return (
             <li
               key={friend.username}
