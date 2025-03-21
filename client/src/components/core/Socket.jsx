bdc1c22c3608c793fec05fa8c387ee9e3d2a86e5
Krasen Boychev <krasenboychev11@gmail.com>
2025-03-21 18:24:00 +0000

    useNewGameStarted imported and props passed

--- client/src/components/core/Socket.jsx
+++ client/src/components/core/Socket.jsx
@@ -1,10 +1,14 @@
+import useNewGameStarted from "../../hooks/useNewGameStarted";
 import useSocket from "../../hooks/useSocket";
 
 export default function Socket(props) {
   const { socket, setSocket } = props.socketProps;
   const { friendsList, setFriendsList } = props.friendsProps;
   const { setNotificationsList } = props.setNotificationsList;
+  const { isNewGameStarted, setIsNewGameStarted } = props.newGameStartedProps;
+  const friendInvited = props.friendInvited;
 
   useSocket(socket, setSocket, setFriendsList, setNotificationsList);
+  useNewGameStarted(socket, friendsList, friendInvited, isNewGameStarted);
   return;
 }
