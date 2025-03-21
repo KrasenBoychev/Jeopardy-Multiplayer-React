a4a7c09354d831cbd41a1bdd3e6190daa3d7ce5d
Krasen Boychev <krasenboychev11@gmail.com>
2025-03-21 18:29:05 +0000

    removeNotificationFromNotificationsList imported and props passed; value added to the button element

--- client/src/components/core/notificationsBox/buttons/ReadNotification.jsx
+++ client/src/components/core/notificationsBox/buttons/ReadNotification.jsx
@@ -1,15 +1,22 @@
 import toast from "react-hot-toast";
 import { removeNotification } from "../../../../../api/user-api";
+import { removeNotificationFromNotificationsList } from "../NotificationsBox";
 
 export default function ReadNotification({ props }) {
-  const { notification, setUpdateNotifications } = props;
+  const { notification, setNotificationsList } = props;
 
   const readNotification = async (e) => {
     try {
       const friendUsername = e.target.id;
+      const notificationType = e.target.value;
+
       await removeNotification(friendUsername, "friendResponse");
 
-      setUpdateNotifications(true);
+      removeNotificationFromNotificationsList(
+        setNotificationsList,
+        friendUsername,
+        notificationType
+      );
     } catch (error) {
       toast.error(error.message);
     }
@@ -17,6 +24,7 @@ export default function ReadNotification({ props }) {
   return (
     <button
       className="notification_btn_read"
+      value={notification.type}
       id={notification.username}
       onClick={readNotification}
     >
