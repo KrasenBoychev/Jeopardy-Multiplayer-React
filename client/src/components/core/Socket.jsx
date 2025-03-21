cbdbc83193c1c087810310e4b56782c5b9e81d80
Krasen Boychev <krasenboychev11@gmail.com>
2025-03-21 14:00:21 +0000

    props updated

--- client/src/components/core/Socket.jsx
+++ client/src/components/core/Socket.jsx
@@ -3,7 +3,8 @@ import useSocket from "../../hooks/useSocket";
 export default function Socket(props) {
   const { socket, setSocket } = props.socketProps;
   const { friendsList, setFriendsList } = props.friendsProps;
+  const { setNotificationsList } = props.setNotificationsList;
 
-  useSocket(socket, setSocket, setFriendsList);
+  useSocket(socket, setSocket, setFriendsList, setNotificationsList);
   return;
 }
