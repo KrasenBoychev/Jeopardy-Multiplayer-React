b5ba0e7a1cc5a729493dc9cbb54fcad751c13c9f
Krasen Boychev <krasenboychev11@gmail.com>
2025-03-22 16:43:00 +0000

    playPage.css and PlayPage.jsx moved to new folder

---
+++ client/src/components/game/01. play_page/PlayPage.jsx
@@ -0,0 +1,84 @@
+import { useEffect, useState } from "react";
+import StartGameWithOtherPlayer from "./children/startGameWithOtherPlayer/StartGameWithOtherPlayer";
+import FriendsList from "./children/friendsList/FriendsList";
+import GameRoom from "./children/gameRoom/GameRoom";
+import Counter from "../02. counter/Counter";
+import ExitGame from "../exitGame/ExitGame";
+
+import "./playPage.css";
+
+export default function PlayPage({
+  socket,
+  friendsProps,
+  notifications,
+  playersProps,
+  gameRoomNameProps,
+}) {
+  const {
+    friendsList,
+    friendInvited,
+    setFriendInvited,
+    isNewGameStarted,
+    setIsNewGameStarted,
+  } = friendsProps;
+  const { firstPlayer, secondPlayer } = playersProps;
+  const { gameRoomName, setGameRoomName } = gameRoomNameProps;
+  const { notificationsList } = notifications;
+
+  const [renderStartingPlayer, setRenderStartingPlayer] = useState(false);
+
+  useEffect(() => {
+    if (isNewGameStarted) {
+      setTimeout(() => {
+        setRenderStartingPlayer(true);
+      }, 2000);
+    }
+  }, [isNewGameStarted]);
+
+  return (
+    <>
+      {renderStartingPlayer ? (
+        <>
+          <Counter props={{ friendInvited, firstPlayer, secondPlayer }} />
+          <ExitGame
+            props={{
+              socket,
+              friendsList,
+              setFriendInvited,
+              gameRoomName,
+              setGameRoomName,
+              setRenderStartingPlayer,
+              setIsNewGameStarted,
+              firstPlayer,
+              secondPlayer,
+            }}
+          />
+          {/* TO DO: write the logic in ExitGame.jsx */}
+          {/* setRenderStartingPlayer should be set to false in ExitGame ??? */}
+        </>
+      ) : (
+        <div className="play_page_container">
+          <section>
+            <GameRoom
+              socket={socket}
+              friendProps={{
+                friendsList,
+                friendInvited,
+                setFriendInvited,
+                isNewGameStarted,
+                firstPlayer,
+                secondPlayer,
+              }}
+              notificationsList={notificationsList}
+            />
+          </section>
+          <section>
+            <FriendsList socket={socket} friendsList={friendsList} />
+            {/* Change the component to StartGameWithRandomPlayer - write the code for it */}
+            {/* <StartGameWithOtherPlayer /> */}
+          </section>
+        </div>
+      )}
+    </>
+  );
+}
