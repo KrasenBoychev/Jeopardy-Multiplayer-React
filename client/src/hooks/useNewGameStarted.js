b3c8d8b1ffd1e885193bb2795f63ff7baf277f09
Krasen Boychev <krasenboychev11@gmail.com>
2025-03-22 16:35:29 +0000

    changeGameInProgress created and exported

--- client/src/hooks/useNewGameStarted.js
+++ client/src/hooks/useNewGameStarted.js
@@ -1,33 +1,53 @@
 import { useEffect } from "react";
 import { updateGameInProgress } from "../../api/user-api";
 import { useAuthContext } from "../contexts/AuthContext";
+import { useLocation, useNavigate } from "react-router-dom";
 
 export default function useNewGameStarted(
   socket,
   friendsList,
-  friendInvited,
-  isNewGameStarted
+  isNewGameStarted,
 ) {
   const { username } = useAuthContext();
+  const location = useLocation();
+  const navigate = useNavigate();
 
   useEffect(() => {
     if (isNewGameStarted) {
-      (async function newGameStarts() {
-        await updateGameInProgress();
-
-        const receiverFriends = friendsList.filter(
-          (friend) =>
-            friend.username !== friendInvited &&
-            friend.online &&
-            !friend.gameInProgress
+      (async function newGameStarted() {
+        const gameInProgressValue = true;
+        await changeGameInProgress(
+          socket,
+          friendsList,
+          username,
+          gameInProgressValue,
+          navigate,
+          location
         );
-
-        await socket.emit("sendUserStatus", {
-          senderInfo: { username, gameInProgress: true },
-          receiverFriends,
-          action: "changeGameInProgress",
-        });
       })();
     }
   }, [isNewGameStarted]);
+}
+
+export async function changeGameInProgress(
+  socket,
+  friendsList,
+  username,
+  gameInProgressValue,
+  navigate,
+  location
+) {
+  await updateGameInProgress();
+
+  const receiverFriends = friendsList.filter((friend) => friend.online);
+
+  await socket.emit("sendUserStatus", {
+    senderInfo: { username, gameInProgress: gameInProgressValue },
+    receiverFriends,
+    action: "changeGameInProgress",
+  });
+
+  if (location && location.pathname != "/play") {
+    navigate("/play");
+  }
 }
