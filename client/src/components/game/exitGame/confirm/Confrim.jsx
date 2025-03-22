08dc4cb113355107ba459a412d94b5d0312586ed
Krasen Boychev <krasenboychev11@gmail.com>
2025-03-22 16:48:58 +0000

    new logic added to Confirm.jsx based on socket requests

--- client/src/components/game/exitGame/confirm/Confrim.jsx
+++ client/src/components/game/exitGame/confirm/Confrim.jsx
@@ -1,7 +1,59 @@
+import { useNavigate } from "react-router-dom";
+import { useAuthContext } from "../../../../contexts/AuthContext";
+import { getPlayersDetails } from "../../../../utils/gameUtils";
+import { changeGameInProgress } from "../../../../hooks/useNewGameStarted";
 import "./confirm.css";
 
 export default function Confrim({ props }) {
-  const { confirmLeaving, declineLeaving } = props;
+  const {
+    socket,
+    friendsList,
+    setShowConfirmMessage,
+    setFriendInvited,
+    gameRoomName,
+    setGameRoomName,
+    setRenderStartingPlayer,
+    setIsNewGameStarted,
+    firstPlayer,
+    secondPlayer,
+  } = props;
+  const { username } = useAuthContext();
+  const navigate = useNavigate();
+
+  const confirmLeaving = async () => {
+    const { friendUsernameDetails } = getPlayersDetails(
+      username,
+      firstPlayer,
+      secondPlayer
+    );
+
+    await socket.emit("setExitGame", {
+      receiverSocketId: friendUsernameDetails.socketId,
+      userUsername: username,
+      gameRoomName,
+    });
+
+    const gameInProgressValue = false;
+    await changeGameInProgress(
+      socket,
+      friendsList,
+      username,
+      gameInProgressValue,
+      navigate,
+      null
+    );
+
+    setRenderStartingPlayer(false);
+    setGameRoomName(null);
+    setFriendInvited(null);
+    setIsNewGameStarted(false);
+    window.location.reload();
+    navigate("/play");
+  };
+
+  const declineLeaving = () => {
+    setShowConfirmMessage(false);
+  };
 
   return (
     <div className="confirm-wrapper">
