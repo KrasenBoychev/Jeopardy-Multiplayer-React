aac62e81774574e57802accd559a86f6845f0ddb
Krasen Boychev <krasenboychev11@gmail.com>
2025-03-27 22:07:38 +0000

    CategoriesHeader.jsx created

---
+++ client/src/components/game/04. categories/CategoriesHeader.jsx
@@ -0,0 +1,20 @@
+import { useAuthContext } from "../../../contexts/AuthContext";
+
+export default function CategoriesHeader({ props }) {
+  const { activePlayer, currCategoryCount } = props;
+  const { username } = useAuthContext();
+
+  return (
+    <p
+      className={
+        username == activePlayer && currCategoryCount < 4
+          ? "active_player player_categories"
+          : "player_categories"
+      }
+    >
+      {currCategoryCount < 4
+        ? `${activePlayer} chooses category`
+        : "Loading Questions..."}
+    </p>
+  );
+}
