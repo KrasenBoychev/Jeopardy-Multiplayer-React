f9f0235b70e1893df52bbeb277799ad214673b11
Krasen Boychev <krasenboychev11@gmail.com>
2025-03-27 22:01:47 +0000

    ShowQuestion.jsx changed to Answers.jsx and code updated

--- client/src/components/game/06. answers/Answers.jsx
+++ client/src/components/game/06. answers/Answers.jsx
@@ -1,22 +1,24 @@
+import { useAuthContext } from "../../../contexts/AuthContext";
 import { useGameContext } from "../../../contexts/GameContext";
 import "../game.css";
-import "./showQuestion.css";
+import "./answers.css";
 
-export default function ({ props }) {
+export default function Answers({ props }) {
   const {
     activePlayer,
     currCategory,
     currQuestion,
-    firstPlayer,
     pointsFirstPlayer,
     pointsSecondPlayer,
     isAnswerCorrect,
     isAnswerClicked,
   } = props;
 
-  const { channel, client } = useGameContext();
+  const { username } = useAuthContext();
+  const { firstPlayerUsername } = useGameContext();
 
   const answerQuestionClickHandler = async (e) => {
+    // TO DO
     let pointsWon = 0;
     let totalPoints = 0;
 
@@ -24,33 +26,24 @@ export default function ({ props }) {
       pointsWon = currQuestion.points;
     }
 
-    if (activePlayer == firstPlayer) {
+    if (activePlayer == firstPlayerUsername) {
       totalPoints = pointsFirstPlayer + pointsWon;
     } else {
       totalPoints = pointsSecondPlayer + pointsWon;
     }
-
-    await channel.sendEvent({
-      type: "choose-answer",
-      data: {
-        activePlayer,
-        pointsWon,
-        totalPoints,
-      },
-    });
   };
 
   return (
-    <div className="show-question-container">
+    <div className="show_question_container">
       <p
         className={
           isAnswerCorrect == null
-            ? client.user.name === activePlayer
-              ? "player-categories active-player"
-              : "player-categories"
+            ? username === activePlayer
+              ? "player_categories active_player"
+              : "player_categories"
             : isAnswerCorrect
-            ? "answer-correct player-categories "
-            : "player-categories answer-wrong"
+            ? "answer_correct player_categories "
+            : "player_categories answer_wrong"
         }
       >
         {isAnswerCorrect == null
@@ -60,16 +53,16 @@ export default function ({ props }) {
           : "Wrong Answer"}
       </p>
 
-      <div className="show-question-category">
+      <div className="show_question_category">
         <h3>
           {currCategory} - {currQuestion.points} points
         </h3>
       </div>
 
-      <div className="show-question-wrapper activeCat">
+      <div className="show_question_wrapper activeCat">
         <h1>{currQuestion.name}</h1>
 
-        <div className="answers-wrapper">
+        <div className="answers_wrapper">
           {Object.values(currQuestion.answers).map((answer) => {
             return (
               <button
@@ -78,16 +71,16 @@ export default function ({ props }) {
                 onClick={answerQuestionClickHandler}
                 className={
                   isAnswerCorrect == null
-                    ? client.user.name === activePlayer
-                      ? "answer-box answer-box-active"
-                      : "answer-box"
+                    ? username === activePlayer
+                      ? "answer_box answer_box_active"
+                      : "answer_box"
                     : isAnswerCorrect && currQuestion.correctAnswer == answer
-                    ? "answer-box answer-correct"
+                    ? "answer_box answer_correct"
                     : !isAnswerCorrect
                     ? currQuestion.correctAnswer == answer
-                      ? "answer-box answer-correct"
-                      : "answer-box answer-wrong"
-                    : "answer-box inactive-player"
+                      ? "answer_box answer_correct"
+                      : "answer_box answer_wrong"
+                    : "answer_box inactive_player"
                 }
               >
                 {answer}
