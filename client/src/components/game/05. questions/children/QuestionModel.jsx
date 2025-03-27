4a268ea6ea8fd262409476ab1d613e059707952c
Krasen Boychev <krasenboychev11@gmail.com>
2025-03-27 22:07:11 +0000

    showQuestionClickHandler logic updated

--- client/src/components/game/05. questions/children/QuestionModel.jsx
+++ client/src/components/game/05. questions/children/QuestionModel.jsx
@@ -1,15 +1,33 @@
 import { useAuthContext } from "../../../../contexts/AuthContext";
+import { useGameContext } from "../../../../contexts/GameContext";
 
 export default function QuestionModel({ props }) {
-  const { activePlayer, categoryName, question, questionAnswered } = props;
+  const {
+    activePlayer,
+    categoryName,
+    question,
+    questionAnswered,
+    setShowQuestion,
+    setCurrCategory,
+    setCurrQuestion,
+  } = props;
   const { username } = useAuthContext();
+  const { socket, friendSocketId } = useGameContext();
 
   const showQuestionClickHandler = async () => {
     if (questionAnswered) {
       return;
-
-      //TO DO
     }
+
+    setShowQuestion(true);
+    setCurrCategory(categoryName);
+    setCurrQuestion(question);
+
+    await socket.emit("sendQuestionOpened", {
+      receiverSocketId: friendSocketId,
+      categoryName,
+      question,
+    });
   };
 
   return (
