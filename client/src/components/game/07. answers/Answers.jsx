858dd24e2eb472ca991d974e9480f05d192648ee
Krasen Boychev <krasenboychev11@gmail.com>
2025-03-29 19:34:27 +0000

    Answers.jsx imports useAnswerClicked hook, AnswerModel.jsx and AnswersHeader.jsx

--- client/src/components/game/07. answers/Answers.jsx
+++ client/src/components/game/07. answers/Answers.jsx
@@ -1,5 +1,6 @@
-import { useAuthContext } from "../../../contexts/AuthContext";
-import { useGameContext } from "../../../contexts/GameContext";
+import useAnswerClicked from "../../../hooks/game_hooks/useAnswerClicked";
+import AnswerModel from "./children/AnswerModel";
+import AnswersHeader from "./children/AnswersHeader";
 import "../game.css";
 import "./answers.css";
 
@@ -8,87 +9,57 @@ export default function Answers({ props }) {
     activePlayer,
     currCategory,
     currQuestion,
-    pointsFirstPlayer,
-    pointsSecondPlayer,
-    isAnswerCorrect,
     isAnswerClicked,
+    isAnswerCorrect,
+    setActivePlayer,
+    setPointsFirstPlayer,
+    setPointsSecondPlayer,
+    setIsAnswerClicked,
+    setIsAnswerCorrect,
+    setRecordAnswer,
   } = props;
 
-  const { username } = useAuthContext();
-  const { firstPlayerUsername } = useGameContext();
-
-  const answerQuestionClickHandler = async (e) => {
-    // TO DO
-    let pointsWon = 0;
-    let totalPoints = 0;
-
-    if (e.target.textContent == currQuestion.correctAnswer) {
-      pointsWon = currQuestion.points;
-    }
-
-    if (activePlayer == firstPlayerUsername) {
-      totalPoints = pointsFirstPlayer + pointsWon;
-    } else {
-      totalPoints = pointsSecondPlayer + pointsWon;
-    }
-  };
+  const setAnswerClicked = useAnswerClicked(
+    activePlayer,
+    currQuestion,
+    isAnswerClicked,
+    setActivePlayer,
+    setPointsFirstPlayer,
+    setPointsSecondPlayer,
+    setIsAnswerClicked,
+    setIsAnswerCorrect,
+    setRecordAnswer
+  );
 
   return (
-    <div className="show_question_container">
-      <p
-        className={
-          isAnswerCorrect == null
-            ? username === activePlayer
-              ? "player_categories active_player"
-              : "player_categories"
-            : isAnswerCorrect
-            ? "answer_correct player_categories "
-            : "player_categories answer_wrong"
-        }
-      >
-        {isAnswerCorrect == null
-          ? `${activePlayer} answers question`
-          : isAnswerCorrect
-          ? "Correct Answer"
-          : "Wrong Answer"}
-      </p>
+    <section className="show_question_container">
+      <AnswersHeader
+        props={{ activePlayer, isAnswerCorrect, currCategory, currQuestion }}
+      />
 
-      <div className="show_question_category">
-        <h3>
-          {currCategory} - {currQuestion.points} points
-        </h3>
-      </div>
-
-      <div className="show_question_wrapper activeCat">
+      <div className="show_question_wrapper">
         <h1>{currQuestion.name}</h1>
 
         <div className="answers_wrapper">
-          {Object.values(currQuestion.answers).map((answer) => {
+          {Object.values(currQuestion.answers).map((answer, index) => {
             return (
-              <button
-                key={answer}
-                disabled={isAnswerClicked ? true : false}
-                onClick={answerQuestionClickHandler}
-                className={
-                  isAnswerCorrect == null
-                    ? username === activePlayer
-                      ? "answer_box answer_box_active"
-                      : "answer_box"
-                    : isAnswerCorrect && currQuestion.correctAnswer == answer
-                    ? "answer_box answer_correct"
-                    : !isAnswerCorrect
-                    ? currQuestion.correctAnswer == answer
-                      ? "answer_box answer_correct"
-                      : "answer_box answer_wrong"
-                    : "answer_box inactive_player"
-                }
-              >
-                {answer}
-              </button>
+              <AnswerModel
+                key={answer + index}
+                props={{
+                  answer,
+                  index,
+                  setAnswerClicked,
+                  activePlayer,
+                  currQuestion,
+                  isAnswerClicked,
+                  setIsAnswerClicked,
+                  isAnswerCorrect,
+                }}
+              />
             );
           })}
         </div>
       </div>
-    </div>
+    </section>
   );
 }
