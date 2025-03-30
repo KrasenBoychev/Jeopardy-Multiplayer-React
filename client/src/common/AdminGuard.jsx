0ccdac2a2c31a9dc0b5125cdc65c561a9b581e40
Krasen Boychev <krasenboychev11@gmail.com>
2025-03-30 17:07:44 +0100

    imports updated

--- client/src/common/AdminGuard.jsx
+++ client/src/common/AdminGuard.jsx
@@ -1,13 +1,10 @@
-import { Outlet } from 'react-router-dom';
-import { useAuthContext } from '../contexts/AuthContext';
-import { adminId } from './credentials.js';
-
-import NotFound from '../components/core/notFound/NotFound';
+import { useAuthContext } from "../contexts/AuthContext.tsx";
+import { Outlet } from "react-router-dom";
+import { adminId } from "./credentials.ts";
+import NotFound from "../components/core/notFound/NotFound.jsx";
 
 export default function AdminGuard() {
   const { isAuthenticated, userId } = useAuthContext();
 
-  return isAuthenticated && userId == adminId
-      ? <Outlet />
-      : <NotFound />;
+  return isAuthenticated && userId == adminId ? <Outlet /> : <NotFound />;
 }
--- client/src/components/create/Create.jsx
+++ client/src/components/create/Create.jsx
@@ -1,7 +1,5 @@
 import { useNavigate } from "react-router-dom";
-
 import "./create.css";
-import DotLoader from "react-spinners/DotLoader";
 
 export default function Create() {
   const navigate = useNavigate();
@@ -18,7 +16,9 @@ export default function Create() {
     <main>
       <div className="create-container">
         <section>
-          <p onClick={createCategoryHandler}>Create Category and Questions for it</p>
+          <p onClick={createCategoryHandler}>
+            Create Category and Questions for it
+          </p>
         </section>
         <section>
           <p onClick={createQuestionHandler}>Create Question only</p>
