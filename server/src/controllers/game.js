055d6224be1a81d01fef03bff0ea362e5b185784
Krasen Boychev <krasenboychev11@gmail.com>
2025-03-30 16:57:51 +0100

    game controller responsible to set Stream Chat client deleted - not in use anymore because of the usage of sockets

--- server/src/controllers/game.js
+++ server/src/controllers/game.js
@@ -1,53 +1,18 @@
-const { Router } = require('express');
-const { body, validationResult } = require('express-validator');
-const { v4 } = require('uuid');
-const { StreamChat } = require('stream-chat');
-
-const { isUser } = require('../middlewares/guards');
-
-const { api_key, api_secret } = require('../api-keys');
-
-const { parseError } = require('../util');
+const { Router } = require("express");
+const { body, validationResult } = require("express-validator");
+const { isUser } = require("../middlewares/guards");
+const { parseError } = require("../util");
 
 const {
   getAllCategories,
   getCategory,
   getQuestions,
   updatePoints,
-} = require('../services/game');
+} = require("../services/game");
 
 const gameRouter = Router();
 
-gameRouter.get('/:username', async (req, res) => {
-  const serverClient = StreamChat.getInstance(api_key, api_secret);
-
-  try {
-    const username = req.params.username;
-
-    const { users } = await serverClient.queryUsers({ name: username });
-
-    let token;
-    let userIdResult;
-
-    if (users.length === 0) {
-      const userId = v4();
-      token = serverClient.createToken(userId);
-      userIdResult = userId;
-    } else {
-      token = serverClient.createToken(users[0].id);
-      userIdResult = users[0].id;
-    }
-
-    res.json({
-      token,
-      userId: userIdResult,
-    });
-  } catch (error) {
-    res.json(error);
-  }
-});
-
-gameRouter.get('/categories/all', async (req, res) => {
+gameRouter.get("/categories/all", async (req, res) => {
   try {
     const data = await getAllCategories();
     res.json(data);
@@ -57,7 +22,7 @@ gameRouter.get('/categories/all', async (req, res) => {
   }
 });
 
-gameRouter.get('/category/:categoryName', async (req, res) => {
+gameRouter.get("/category/:categoryName", async (req, res) => {
   try {
     const data = await getCategory(req.params.categoryName);
     res.json(data);
@@ -67,10 +32,10 @@ gameRouter.get('/category/:categoryName', async (req, res) => {
   }
 });
 
-gameRouter.get('/questions/:categoriesIDs', async (req, res) => {
+gameRouter.get("/questions/:categoriesIDs", async (req, res) => {
   try {
     const categoriesIDs = req.params.categoriesIDs;
-    const categoriesIDsArr = categoriesIDs.split(',');
+    const categoriesIDsArr = categoriesIDs.split(",");
     const data = await getQuestions(categoriesIDsArr);
     res.json(data);
   } catch (err) {
@@ -80,13 +45,13 @@ gameRouter.get('/questions/:categoriesIDs', async (req, res) => {
 });
 
 gameRouter.put(
-  '/result/:userId',
+  "/result/:userId",
   isUser(),
-  body('points')
+  body("points")
     .trim()
     .notEmpty()
     .isNumeric()
-    .withMessage('Points should be a number'),
+    .withMessage("Points should be a number"),
   async (req, res) => {
     try {
       const validation = validationResult(req);
