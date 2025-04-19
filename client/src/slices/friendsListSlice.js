2147d3eb4a035922696ac577c82bf34d4d479533
Krasen Boychev <krasenboychev11@gmail.com>
2025-04-19 11:42:42 +0100

    friendsListSlice.js created

---
+++ client/src/slices/friendsListSlice.js
@@ -0,0 +1,16 @@
+// import { createSlice } from "@reduxjs/toolkit";
+// import { getUserFriendsAndTheirStatus } from "../../api/friends-api";
+
+// const initialState = {
+//   friendsList: [],
+// };
+
+// export const friendsListSlice = createSlice({
+//   name: "friendsList",
+//   initialState,
+//   reducers: {
+//     getFriendsListData: async (state) => {
+//       state = await getUserFriendsAndTheirStatus();
+//     }
+//   },
+// });
