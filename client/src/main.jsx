ec2552907e95c6f204522cefa6876520e20ee0db
Krasen Boychev <krasenboychev11@gmail.com>
2025-04-19 11:44:11 +0100

    main.jsx creates a new socket

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,6 +4,9 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import { store } from "./app/store.js";
 import { Provider } from "react-redux";
+import { createSocket } from "./slices/socketSlice.js";
+
+store.dispatch(createSocket());
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
