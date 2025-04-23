989eac4dd2e640417962365a24678e7fc7dadc99
Krasen Boychev <krasenboychev11@gmail.com>
2025-04-23 17:39:28 +0100

    createSocket dispatch function removed

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,16 +4,13 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import { store } from "./app/store.js";
 import { Provider } from "react-redux";
-import { createSocket } from "./slices/socketSlice.js";
-
-store.dispatch(createSocket());
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
+  // <StrictMode>
     <Provider store={store}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
     </Provider>
-  </StrictMode>
+  // </StrictMode>
 );
