051a1911622f8df285ab660a4db142152f986d74
Krasen Boychev <krasenboychev11@gmail.com>
2025-04-23 17:42:23 +0100

    setIsUserAuthenticated sets to false when an users loggs out and users is deleted from online_users in the DB

--- client/src/components/authentication/Logout.jsx
+++ client/src/components/authentication/Logout.jsx
@@ -3,23 +3,33 @@ import { useLogout } from "../../hooks/useAuth";
 import { sendUpdateToOnlineFriends } from "../../hooks/useSocket";
 import { useAuthContext } from "../../contexts/AuthContext";
 import { useEffect } from "react";
+import { socket } from "../../app/socket";
+import toast from "react-hot-toast";
+import { deleteUserInOnlineUsers } from "../../../api/user-api";
 
 export default function Logout(props) {
-  const { socket, setSocket } = props.socketProps;
+  // const { socket, setSocket } = props.socketProps;
   const { friendsList, setFriendsList } = props.friendsProps;
   const { setFriendInvited } = props.setFriendInvited;
+  const setIsUserAuthenticated = props.setIsUserAuthenticated;
 
   const { username } = useAuthContext();
-  const logout = useLogout();
+  const logout = useLogout(setIsUserAuthenticated);
   const action = "friendIsOffline";
 
   useEffect(() => {
     (async function logoutUser() {
-      await sendUpdateToOnlineFriends(socket, username, friendsList, action);
-      await socket.disconnect();
-      setSocket(null);
-      setFriendInvited(null);
-      logout();
+      try {
+        await deleteUserInOnlineUsers();
+
+        setFriendInvited(null);
+        logout();
+      } catch (error) {
+        toast.error(error.message);
+      }
+      // await sendUpdateToOnlineFriends(socket, username, friendsList, action);
+      // socket.disconnect();
+      // setSocket(null);
     })();
   }, []);
 
