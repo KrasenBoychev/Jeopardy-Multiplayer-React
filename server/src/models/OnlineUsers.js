e15cc13dd441d0289775b63eb3c9afaf389b4172
Krasen Boychev <krasenboychev11@gmail.com>
2025-04-24 20:58:31 +0100

    OnlineUsers.js deleted

--- server/src/models/OnlineUsers.js
+++ server/src/models/OnlineUsers.js
@@ -1,23 +1,23 @@
-const { Schema, model } = require("mongoose");
+// const { Schema, model } = require("mongoose");
 
-const OnlineUserSchema = new Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  socketId: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  gameInProgress: {
-    type: Boolean,
-    required: true,
-  },
-});
+// const OnlineUserSchema = new Schema({
+//   username: {
+//     type: String,
+//     required: true,
+//     unique: true,
+//   },
+//   socketId: {
+//     type: String,
+//     required: true,
+//     unique: true,
+//   },
+//   gameInProgress: {
+//     type: Boolean,
+//     required: true,
+//   },
+// });
 
-const OnlineUser = model("online_users", OnlineUserSchema);
-OnlineUser.createIndexes();
+// const OnlineUser = model("online_users", OnlineUserSchema);
+// OnlineUser.createIndexes();
 
-module.exports = { OnlineUser };
+// module.exports = { OnlineUser };
