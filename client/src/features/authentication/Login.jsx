70e489e6e7761a1d42732f610ef8845740e3c4f1
Krasen Boychev <krasenboychev11@gmail.com>
2025-04-27 20:29:14 +0100

    Login.jsx uses useLoginMutation from redux store

--- client/src/features/authentication/Login.jsx
+++ client/src/features/authentication/Login.jsx
@@ -1,19 +1,25 @@
 import { useRef } from "react";
 import { useNavigate, Link } from "react-router-dom";
-import { Formik, Form, Field, ErrorMessage } from "formik";
+import { useDispatch } from "react-redux";
 import { toast } from "react-hot-toast";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import { useLoginMutation } from "./authApiSlice";
 
 import "./authentication.css";
+import { setCredentials } from "./authSlice";
 
 // import { useLogin } from "../../hooks/useAuth";
 
 export default function Login({ setIsUserAuthenticated }) {
   const navigate = useNavigate();
+  const [login, { isLoading }] = useLoginMutation();
+  const dispatch = useDispatch();
   // const login = useLogin(setIsUserAuthenticated);
 
   return (
     <div className="authentication-container">
       <div className="authentication-wrapper">
+        <div>{isLoading && <h1>Loading...</h1>}</div>
         <h1>Login</h1>
         <Formik
           initialValues={{ email: "", password: "" }}
@@ -35,11 +41,40 @@ export default function Login({ setIsUserAuthenticated }) {
           }}
           onSubmit={async (values) => {
             try {
-              await login(values.email, values.password);
+              const userData = await login({
+                email: values.email,
+                password: values.password,
+              }).unwrap();
+
+              dispatch(setCredentials(userData));
 
               navigate("/");
-            } catch (error) {
-              return toast.error(error.message);
+            } catch (err) {
+              // if (!err?.originalStatus) {
+              //   // isLoading: true until timeout occurs
+              //   toast.error("No Server Response");
+              // } else if (err.originalStatus === 400) {
+              //   toast.error("Missing Username or Password");
+              // } else if (err.originalStatus === 401) {
+              //   toast.error("Unauthorized");
+              // } else if (err.originalStatus === 403) {
+              //   toast.error("Incorrect email or password");
+              // } else {
+              //   toast.error("Login Failed");
+              // }
+
+              if (!err?.status) {
+                // isLoading: true until timeout occurs
+                toast.error("No Server Response");
+              } else if (err.status === 400) {
+                toast.error("Missing Username or Password");
+              } else if (err.status === 401) {
+                toast.error("Unauthorized");
+              } else if (err.status === 403) {
+                toast.error("Incorrect email or password");
+              } else {
+                toast.error("Login Failed");
+              }
             }
           }}
         >
