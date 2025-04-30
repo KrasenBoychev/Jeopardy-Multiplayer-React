9dc2de83ae0ac95189acdaee6165ecbbb14207b0
Krasen Boychev <krasenboychev11@gmail.com>
2025-04-30 20:21:54 +0100

    friendsReducer imported in store.js

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { apiSlice } from "./api/apiSlice";
 import authReducer from "../features/authentication/authSlice";
+import friendsReducer from "../features/friendsSlice";
 
 export const store = configureStore({
   reducer: {
     [apiSlice.reducerPath]: apiSlice.reducer,
     auth: authReducer,
+    friends: friendsReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(apiSlice.middleware),
