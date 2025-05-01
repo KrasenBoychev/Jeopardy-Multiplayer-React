64458b5e750e4cecee647001df19609661d980e7
Krasen Boychev <krasenboychev11@gmail.com>
2025-05-01 17:44:40 +0100

    notificationsReducer declared in store.js

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import { apiSlice } from "./api/apiSlice";
 import authReducer from "../features/authentication/authSlice";
 import friendsReducer from "../features/game/01. play_page/children/friendsList/friendsSlice";
+import notificationsReducer from "../components/notifications/notificationsSlice";
 
 export const store = configureStore({
   reducer: {
     [apiSlice.reducerPath]: apiSlice.reducer,
     auth: authReducer,
     friends: friendsReducer,
+    notifications: notificationsReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(apiSlice.middleware),
