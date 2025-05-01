acf987ec6509edfffb1a4ba425bed1bba54a2430
Krasen Boychev <krasenboychev11@gmail.com>
2025-05-01 15:41:07 +0100

    Logout.jsx sends an update to all online friends and sends server user req to update the db

--- client/src/features/authentication/Logout.jsx
+++ client/src/features/authentication/Logout.jsx
@@ -1,21 +1,45 @@
 import { Navigate } from "react-router-dom";
-import { useDispatch } from "react-redux";
-import { deleteCredentials } from "./authSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { deleteCredentials, selectCurrentUser } from "./authSlice";
 import { useEffect } from "react";
 import toast from "react-hot-toast";
 import { useLogOutMutation } from "./authApiSlice";
+import { selectFriends } from "../game/01. play_page/children/friendsList/friendsSlice";
+import { useChangeOnlineStatusMutation } from "../socket_connection/socketApiSlice";
 
-export default function Logout() {
+export default function Logout({ socket }) {
   const [logOut, { isLoading }] = useLogOutMutation();
   const dispatch = useDispatch();
+  const [changeOnlineStatus] = useChangeOnlineStatusMutation();
+  const friends = useSelector(selectFriends);
+  const user = useSelector(selectCurrentUser);
 
   useEffect(() => {
     (async function logout() {
       try {
+        if (friends) {
+          const onlineFriends = friends.filter(
+            (friend) => friend.online == true
+          );
+
+          if (onlineFriends.length > 0) {
+            socket.emit("sendUserStatus", {
+              senderInfo: {
+                username: user.username,
+                socketId: "",
+              },
+              receiverFriends: onlineFriends,
+              // action,
+            });
+          }
+        }
+
+        await changeOnlineStatus({ username: user.username, socketId: "" });
+
         await logOut();
         dispatch(deleteCredentials());
       } catch (err) {
-        toast.error('Logout Failed')
+        toast.error("Logout Failed");
       }
     })();
   }, []);
