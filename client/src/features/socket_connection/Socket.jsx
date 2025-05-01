734c0518e3047f2f32c3e1a48caffa1ac3640cb3
Krasen Boychev <krasenboychev11@gmail.com>
2025-05-01 12:00:41 +0100

    Socket.jsx uses useConnection and useFriendsStatus hooks

--- client/src/features/socket_connection/Socket.jsx
+++ client/src/features/socket_connection/Socket.jsx
@@ -1,74 +1,14 @@
-import { socket } from "../../app/socket";
-import { useEffect } from "react";
-import toast from "react-hot-toast";
-import { useDispatch, useSelector } from "react-redux";
-import {
-  selectCurrentUser,
-  updateOnlineStatus,
-} from "../authentication/authSlice";
-import {
-  useChangeOnlineStatusMutation,
-  useGetOnlineFriendsDetailsMutation,
-} from "./socketApiSlice";
+import { io } from "socket.io-client";
+import { baseURL } from "../../app/api/baseURL";
+import { useState } from "react";
+
+import useConnection from "./socket hooks/useConnection";
+import useFriendsStatus from "./socket hooks/useFriendsStatus";
 
 export default function Socket() {
-  const user = useSelector(selectCurrentUser);
-  const dispatch = useDispatch();
-  const [changeOnlineStatus] = useChangeOnlineStatusMutation();
-  const [getOnlineFriendsDetails] = useGetOnlineFriendsDetailsMutation();
+  const [socket, setSocket] = useState(io(baseURL));
 
-  useEffect(() => {
-    (async function updateStatus() {
-      await changeOnlineStatus({
-        username: user.username,
-        socketId: socket.id,
-      });
-
-      dispatch(updateOnlineStatus(socket.id));
-
-      const onlineFriends = await getOnlineFriendsDetails(
-        user.gameDetails.friendsList
-      );
-      console.log(onlineFriends);
-    })();
-
-    return async () => {
-      try {
-        await changeOnlineStatus({ username: user.username, socketId: "" });
-        socket.removeAllListeners();
-      } catch (err) {
-        toast.error("Can not disconnect the user");
-      }
-    };
-  }, []);
-
-  useEffect(() => {
-    // socket.on("connect", addOnlineUser);
-
-    // async function addOnlineUser() {
-    //   try {
-    //     console.log(user);
-    //   } catch (error) {
-    //     toast.error(error.message);
-    //   }
-    // }
-
-    return async () => {};
-  }, []);
-
-  // useSocket();
-  // useNewGameStarted(socket, friendsList, isNewGameStarted);
+  useConnection(socket);
+  useFriendsStatus(socket);
   return;
 }
-
-// after the User model is changed, the server services have to be updated
-// const friendsListResponse = await getUserFriendsAndTheirStatus();
-// setFriendsList(friendsListResponse);
-
-//   const action = "friendIsOnline";
-//   await sendUpdateToOnlineFriends(
-//     socket,
-//     username,
-//     friendsListResponse,
-//     action
-//   );
