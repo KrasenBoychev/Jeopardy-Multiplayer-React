e39964753e8d036db85c1f20001d6c728bd89aba
Krasen Boychev <krasenboychev11@gmail.com>
2025-05-01 11:57:18 +0100

    socket.js not in use - code commented - if the code works properly, will b deleted

--- client/src/app/socket.js
+++ client/src/app/socket.js
@@ -1,7 +1,7 @@
-import { io, Socket } from 'socket.io-client';
-import { baseURL } from './api/baseURL';
+// import { io, Socket } from 'socket.io-client';
+// import { baseURL } from './api/baseURL';
 
-// "undefined" means the URL will be computed from the `window.location` object
-const URL = process.env.NODE_ENV === 'production' ? undefined : baseURL;
+// // "undefined" means the URL will be computed from the `window.location` object
+// const URL = process.env.NODE_ENV === 'production' ? undefined : baseURL;
 
-export const socket = io(URL);
+// export const socket = io(URL);
