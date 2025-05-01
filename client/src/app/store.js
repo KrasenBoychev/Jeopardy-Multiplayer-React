8fe71f4593d4d6c55363f5dcc41c21dfce9cea94
Krasen Boychev <krasenboychev11@gmail.com>
2025-05-01 15:36:13 +0100

    import for friendsReducer updated

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,7 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { apiSlice } from "./api/apiSlice";
 import authReducer from "../features/authentication/authSlice";
-import friendsReducer from "../features/friendsSlice";
+import friendsReducer from "../features/game/01. play_page/children/friendsList/friendsSlice";
 
 export const store = configureStore({
   reducer: {
