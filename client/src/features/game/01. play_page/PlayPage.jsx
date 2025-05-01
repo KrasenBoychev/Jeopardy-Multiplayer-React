91d4005582665dbdc4840ac24e3964e3740a048d
Krasen Boychev <krasenboychev11@gmail.com>
2025-05-01 17:36:59 +0100

    not needed code deleted for Play.jsx and code not in use commented

--- client/src/features/game/01. play_page/PlayPage.jsx
+++ client/src/features/game/01. play_page/PlayPage.jsx
@@ -1,57 +1,35 @@
-import { useEffect, useState } from "react";
-import { useAuthContext } from "../../../contexts/AuthContext";
-import { GameContext } from "../../../contexts/GameContext";
-import StartGameWithOtherPlayer from "./children/startGameWithOtherPlayer/StartGameWithOtherPlayer";
+// import { useEffect, useState } from "react";
+// import { useAuthContext } from "../../../contexts/AuthContext";
+// import { GameContext } from "../../../contexts/GameContext";
+// import StartGameWithOtherPlayer from "./children/startGameWithOtherPlayer/StartGameWithOtherPlayer";
 import FriendsList from "./children/friendsList/FriendsList";
-import GameRoom from "./children/gameRoom/GameRoom";
-import Counter from "../02. counter/Counter";
-import ExitGame from "../exitGame/ExitGame";
+// import GameRoom from "./children/gameRoom/GameRoom";
+// import Counter from "../02. counter/Counter";
+// import ExitGame from "../exitGame/ExitGame";
 import "./playPage.css";
 
-export default function PlayPage({
-  socket,
-  friendsProps,
-  notifications,
-  playersProps,
-  gameRoomNameProps,
-}) {
-  const {
-    friendsList,
-    friendInvited,
-    setFriendInvited,
-    isNewGameStarted,
-    setIsNewGameStarted,
-  } = friendsProps;
-  const { firstPlayer, secondPlayer } = playersProps;
-  const { gameRoomName, setGameRoomName } = gameRoomNameProps;
-  const { notificationsList } = notifications;
+export default function PlayPage() {
+  //   const { firstPlayer, secondPlayer } = playersProps;
+  //   const { gameRoomName, setGameRoomName } = gameRoomNameProps;
+  //   const { notificationsList } = notifications;
 
-  const [renderStartingPlayer, setRenderStartingPlayer] = useState(false);
+  //   const [renderStartingPlayer, setRenderStartingPlayer] = useState(false);
 
-  const { username } = useAuthContext();
-  const friendDetails =
-    firstPlayer.username != username ? firstPlayer : secondPlayer;
+  //   const friendDetails =
+  //     firstPlayer.username != username ? firstPlayer : secondPlayer;
 
-  useEffect(() => {
-    if (isNewGameStarted) {
-      setTimeout(() => {
-        setRenderStartingPlayer(true);
-      }, 2000);
-    }
-  }, [isNewGameStarted]);
+  // useEffect(() => {
+  //   if (isNewGameStarted) {
+  //     setTimeout(() => {
+  //       setRenderStartingPlayer(true);
+  //     }, 2000);
+  //   }
+  // }, [isNewGameStarted]);
 
   return (
     <>
-      {renderStartingPlayer ? (
-        <GameContext.Provider
-          value={{
-            socket,
-            friendUsername: friendDetails.username,
-            friendSocketId: friendDetails.socketId,
-            firstPlayerUsername: firstPlayer.username,
-            secondPlayerUsername: secondPlayer.username,
-          }}
-        >
+      {/* {renderStartingPlayer ? (
+        <>
           <Counter />
           <ExitGame
             props={{
@@ -63,10 +41,10 @@ export default function PlayPage({
               setIsNewGameStarted,
             }}
           />
-        </GameContext.Provider>
-      ) : (
-        <div className="play_page_container">
-          <section>
+        </>
+      ) : ( */}
+      <div className="play_page_container">
+        {/* <section>
             <GameRoom
               socket={socket}
               friendProps={{
@@ -79,14 +57,14 @@ export default function PlayPage({
               }}
               notificationsList={notificationsList}
             />
-          </section>
-          <section>
-            <FriendsList socket={socket} friendsList={friendsList} />
-            {/* Change the component to StartGameWithRandomPlayer - write the code for it */}
-            {/* <StartGameWithOtherPlayer /> */}
-          </section>
-        </div>
-      )}
+          </section> */}
+        <section>
+          <FriendsList />
+          {/* Change the component to StartGameWithRandomPlayer - write the code for it */}
+          {/* <StartGameWithOtherPlayer /> */}
+        </section>
+      </div>
+      {/* )} */}
     </>
   );
 }
