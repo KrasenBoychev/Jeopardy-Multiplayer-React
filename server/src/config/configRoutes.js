c562196b2db0a7b40c3dbb72cdcfd975e662c244
Krasen Boychev <krasenboychev11@gmail.com>
2025-05-02 15:21:27 +0100

    friendsRouter in use again

--- server/src/config/configRoutes.js
+++ server/src/config/configRoutes.js
@@ -2,14 +2,14 @@ const { authRouter } = require("../controllers/auth");
 const { userRouter } = require("../controllers/user");
 // const { gameRouter } = require('../controllers/game');
 // const { createRouter } = require('../controllers/create');
-// const { friendsRouter } = require('../controllers/friends');
+const { friendsRouter } = require('../controllers/friends');
 
 function configRoutes(app) {
   app.use("/auth", authRouter);
   app.use("/users", userRouter);
   // app.use('/play', gameRouter);
   // app.use('/create', createRouter);
-  // app.use('/friends', friendsRouter);
+  app.use('/friends', friendsRouter);
 }
 
 module.exports = { configRoutes };
