52efacde6dc89c9a15164cd21a23ffd03797f222
Krasen Boychev <krasenboychev11@gmail.com>
2025-05-02 15:24:56 +0100

    FriendsList renders the friends list and AddFriendBtn.jsx

--- client/src/features/game/01. play_page/children/friendsList/FriendsList.jsx
+++ client/src/features/game/01. play_page/children/friendsList/FriendsList.jsx
@@ -1,11 +1,9 @@
-import { useState } from "react";
 import AddFriendBtn from "./AddFriendBtn";
-import "./friendsList.css";
 import { useSelector } from "react-redux";
 import { selectFriends } from "./friendsSlice";
+import "./friendsList.css";
 
 export default function FriendsList() {
-  const [addFriendUsername, setAddFriendUsername] = useState("");
   const friends = useSelector(selectFriends);
 
   return (
@@ -38,22 +36,7 @@ export default function FriendsList() {
       ) : (
         <p>Invite friends and earn points!</p>
       )}
-      <p className="add_friend">
-        <input
-          type="text"
-          placeholder="Friend Username"
-          value={addFriendUsername}
-          onChange={(event) => {
-            setAddFriendUsername(event.target.value);
-          }}
-        />
-        {/* <AddFriendBtn
-          addFriendUsername={addFriendUsername}
-          socket={socket}
-          friends={friends}
-          setAddFriendUsername={setAddFriendUsername}
-        /> */}
-      </p>
+      <AddFriendBtn />
     </div>
   );
 }
