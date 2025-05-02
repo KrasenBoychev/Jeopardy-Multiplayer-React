a840d334f57df669e1746698eab05971d0f7a0eb
Krasen Boychev <krasenboychev11@gmail.com>
2025-05-02 15:25:58 +0100

    redux store imports socketReducer

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,6 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { apiSlice } from "./api/apiSlice";
 import authReducer from "../features/authentication/authSlice";
+import socketReducer from "../features/socket_connection/socketSlice";
 import friendsReducer from "../features/game/01. play_page/children/friendsList/friendsSlice";
 import notificationsReducer from "../components/notifications/notificationsSlice";
 
@@ -8,6 +9,7 @@ export const store = configureStore({
   reducer: {
     [apiSlice.reducerPath]: apiSlice.reducer,
     auth: authReducer,
+    socket: socketReducer,
     friends: friendsReducer,
     notifications: notificationsReducer,
   },
