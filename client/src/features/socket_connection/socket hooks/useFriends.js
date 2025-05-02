ea272fe50c7e337e43c82007c1cf03c7627c90cd
Krasen Boychev <krasenboychev11@gmail.com>
2025-05-02 15:22:30 +0100

    friendReqReceived socket listener created

--- client/src/features/socket_connection/socket hooks/useFriends.js
+++ client/src/features/socket_connection/socket hooks/useFriends.js
@@ -1,13 +1,18 @@
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { updateFriendStatus } from "../../game/01. play_page/children/friendsList/friendsSlice";
+import toast from "react-hot-toast";
 
-export default function useFriendsStatus(socket) {
+export default function useFriends(socket) {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    socket?.on("getFriendStatus", ({ senderInfo }) => {    
+    socket?.on("getFriendStatus", ({ senderInfo }) => {
       dispatch(updateFriendStatus(senderInfo));
+    });
+
+    socket?.on("friendReqReceived", () => {
+      toast.success("new friend request received");
     });
   }, [socket]);
 }
