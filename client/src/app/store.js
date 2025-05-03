eabc19b5afabd4fa3aa984dfc6deee5f3bd6fb2f
Krasen Boychev <krasenboychev11@gmail.com>
2025-05-03 18:50:09 +0100

    redux store imports gameReducer

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -3,6 +3,7 @@ import { apiSlice } from "./api/apiSlice";
 import authReducer from "../features/authentication/authSlice";
 import socketReducer from "../features/socket_connection/socketSlice";
 import friendsReducer from "../features/game/01. play_page/children/friendsList/friendsSlice";
+import gameReducer from "../features/game/gameSlice";
 
 export const store = configureStore({
   reducer: {
@@ -10,6 +11,7 @@ export const store = configureStore({
     auth: authReducer,
     socket: socketReducer,
     friends: friendsReducer,
+    game: gameReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(apiSlice.middleware),
