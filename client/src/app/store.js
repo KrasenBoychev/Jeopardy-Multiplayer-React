a3e9f35b03ee13c59050d9db2f9712fd2d2e96e3
Krasen Boychev <krasenboychev11@gmail.com>
2025-05-03 07:19:47 +0100

    notificationsSlice.js deleted because it is not in use

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -3,7 +3,6 @@ import { apiSlice } from "./api/apiSlice";
 import authReducer from "../features/authentication/authSlice";
 import socketReducer from "../features/socket_connection/socketSlice";
 import friendsReducer from "../features/game/01. play_page/children/friendsList/friendsSlice";
-import notificationsReducer from "../components/notifications/notificationsSlice";
 
 export const store = configureStore({
   reducer: {
@@ -11,7 +10,6 @@ export const store = configureStore({
     auth: authReducer,
     socket: socketReducer,
     friends: friendsReducer,
-    notifications: notificationsReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(apiSlice.middleware),
