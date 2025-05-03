45d2db49ad1dbd4d13ac6d8fad60ca27cbed8350
Krasen Boychev <krasenboychev11@gmail.com>
2025-05-03 10:40:58 +0100

    acceptFriendReq and rejectFriendReq added to notifTypes

--- client/src/features/notifications/notificationsType.js
+++ client/src/features/notifications/notificationsType.js
@@ -3,4 +3,12 @@ export const notifTypes = {
     content: " sent friend request",
     btns: "Accept/Reject",
   },
+  acceptFriendReq: {
+    content: " accepted your friend request",
+    btns: "Mark as Read",
+  },
+  rejectFriendReq: {
+    content: " rejected your friend request",
+    btns: "Mark as Read",
+  },
 };
