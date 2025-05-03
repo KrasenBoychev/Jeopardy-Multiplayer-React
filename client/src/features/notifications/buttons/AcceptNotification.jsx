6c2b250ec503b1c56a6142e81b6628c911153628
Krasen Boychev <krasenboychev11@gmail.com>
2025-05-03 10:42:38 +0100

    AcceptNotification.jsx uses the redux hooks

--- client/src/features/notifications/buttons/AcceptNotification.jsx
+++ client/src/features/notifications/buttons/AcceptNotification.jsx
@@ -1,115 +1,91 @@
 import toast from "react-hot-toast";
-import { useAuthContext } from "../../../contexts/AuthContext";
-import { sendFriendResponse } from "../../../../api/friends-api";
-import { removeNotificationFromNotificationsList } from "../Notifications";
+import { useDispatch, useSelector } from "react-redux";
+import { useSendFriendResMutation } from "../../game/01. play_page/children/friendsList/friendsApiSlice";
+import { setSocketReq } from "../../socket_connection/socketSlice";
+import { selectCurrentUser } from "../../authentication/authSlice";
+import { addNewFriend } from "../../game/01. play_page/children/friendsList/friendsSlice";
+import { useGetNotificationsQuery } from "../notificationsApiSlice";
 
-export default function AcceptNotification({ props }) {
-  const {
-    socket,
-    notification,
-    friendsList,
-    setFriendsList,
-    friendInvited,
-    setFriendInvited,
-    setNotificationsList,
-    setIsNewGameStarted,
-    setFirstPlayer,
-    setSecondPlayer,
-    setGameRoomName,
-  } = props;
-  const { username } = useAuthContext();
+export default function AcceptNotification({ notification }) {
+  const user = useSelector(selectCurrentUser);
+  const dispatch = useDispatch();
+  const [sendFriendRes] = useSendFriendResMutation();
+  const { refetch } = useGetNotificationsQuery("getNotifications");
 
-  const acceptNotification = async (e) => {
+  const acceptNotificationClickHandler = async (e) => {
     const friendUsername = e.target.id;
     const notificationType = e.target.value;
 
-    if (notificationType == "friendRequest") {
-      try {
-        const response = await sendFriendResponse(
+    try {
+      if (notificationType == "addFriendReq") {
+        const serverResponse = await sendFriendRes({
           friendUsername,
-          "friendRequestAccepted"
-        );
-
-        if (response.status == "online") {
-          await socket.emit("sendNotification", {
-            receiverSocketId: response.socketId,
-            msg: "friendRequestAccepted",
-            data: {
-              friendsListUpdate: {
-                username,
-                online: true,
-                socketId: socket.id,
-                gameInProgress: friendInvited ? true : false,
-              },
-              notificationInfo: {
-                friendUsername: username,
-                content: " accepted your friend request",
-                btns: "Mark as read",
-              },
-            },
-          });
-
-          setFriendsList((prev) => [
-            ...prev,
-            {
-              username: friendUsername,
-              online: true,
-              socketId: response.socketId,
-              gameInProgress: response.gameInProgress,
-            },
-          ]);
-        } else {
-          setFriendsList((prev) => [
-            ...prev,
-            { username: friendUsername, online: false },
-          ]);
-        }
-
-        removeNotificationFromNotificationsList(
-          setNotificationsList,
-          friendUsername,
-          notificationType
-        );
-      } catch (error) {
-        toast.error(error.message);
-      }
-    } else if (notificationType == "gameInvitation") {
-      const findFriend = friendsList.find(
-        (friend) => friend.username == friendUsername
-      );
-
-      if (findFriend && findFriend.online && !findFriend.gameInProgress) {
-        const usernameDetais = { username, socketId: socket.id };
-        const friendDetails = {
-          username: friendUsername,
-          socketId: findFriend.socketId,
-        };
-
-        const { startingPlayerDetails, otherPlayerDetails } = setPlayers(
-          usernameDetais,
-          friendDetails,
-          setFirstPlayer,
-          setSecondPlayer
-        );
-
-        const roomName = `${friendUsername}-${username}`;
-
-        await socket.emit("setAcceptGameInvitation", {
-          receiverSocketId: findFriend.socketId,
-          userUsername: username,
-          roomName,
-          playersInfo: { startingPlayerDetails, otherPlayerDetails },
+          response: "accepted",
         });
 
-        setFriendInvited(friendUsername);
-        setGameRoomName(roomName);
-        setIsNewGameStarted(true);
-      } else {
-        // If a bug occurs, then this message will show
-        toast.error(
-          friendUsername + " is no longer online - please refresh the page"
-        );
+        const friendDetails = serverResponse.data;
+
+        if (friendDetails.online === true) {
+          dispatch(
+            setSocketReq({
+              socketReqName: "setFriendReqAccepted",
+              socketData: {
+                receiverSocketId: friendDetails.socketId,
+                userDetails: {
+                  username: user.username,
+                  online: user.gameDetails.online,
+                  socketId: user.gameDetails.socketId,
+                  gameInProgress: user.gameDetails.gameInProgress,
+                },
+              },
+            })
+          );
+        }
+
+        dispatch(addNewFriend(friendDetails));
       }
+      // else if (notificationType == "gameInvitation") {
+      //   const findFriend = friendsList.find(
+      //     (friend) => friend.username == friendUsername
+      //   );
+
+      //   if (findFriend && findFriend.online && !findFriend.gameInProgress) {
+      //     const usernameDetais = { username, socketId: socket.id };
+      //     const friendDetails = {
+      //       username: friendUsername,
+      //       socketId: findFriend.socketId,
+      //     };
+
+      //     const { startingPlayerDetails, otherPlayerDetails } = setPlayers(
+      //       usernameDetais,
+      //       friendDetails,
+      //       setFirstPlayer,
+      //       setSecondPlayer
+      //     );
+
+      //     const roomName = `${friendUsername}-${username}`;
+
+      //     await socket.emit("setAcceptGameInvitation", {
+      //       receiverSocketId: findFriend.socketId,
+      //       userUsername: username,
+      //       roomName,
+      //       playersInfo: { startingPlayerDetails, otherPlayerDetails },
+      //     });
+
+      //     setFriendInvited(friendUsername);
+      //     setGameRoomName(roomName);
+      //     setIsNewGameStarted(true);
+      //   } else {
+      //     // If a bug occurs, then this message will show
+      //     toast.error(
+      //       friendUsername + " is no longer online - please refresh the page"
+      //     );
+      //   }
+      // }
+
+      refetch();
+    } catch (error) {
+      toast.error(error.message);
     }
   };
 
@@ -117,32 +93,32 @@ export default function AcceptNotification({ props }) {
     <button
       className="notification_btn_accept"
       value={notification.type}
-      id={notification.username}
-      onClick={acceptNotification}
+      id={notification.sentBy}
+      onClick={acceptNotificationClickHandler}
     >
       Accept
     </button>
   );
 }
 
-function setPlayers(
-  usernameDetais,
-  friendDetails,
-  setFirstPlayer,
-  setSecondPlayer
-) {
-  const playersNames = [usernameDetais, friendDetails];
+// function setPlayers(
+//   usernameDetais,
+//   friendDetails,
+//   setFirstPlayer,
+//   setSecondPlayer
+// ) {
+//   const playersNames = [usernameDetais, friendDetails];
 
-  const startingPlayerDetails =
-    playersNames[Math.floor(Math.random() * playersNames.length)];
+//   const startingPlayerDetails =
+//     playersNames[Math.floor(Math.random() * playersNames.length)];
 
-  const indexOfStartingPlayer = playersNames.indexOf(startingPlayerDetails);
+//   const indexOfStartingPlayer = playersNames.indexOf(startingPlayerDetails);
 
-  playersNames.splice(indexOfStartingPlayer, 1);
-  const otherPlayerDetails = playersNames[0];
+//   playersNames.splice(indexOfStartingPlayer, 1);
+//   const otherPlayerDetails = playersNames[0];
 
-  setFirstPlayer(startingPlayerDetails);
-  setSecondPlayer(otherPlayerDetails);
+//   setFirstPlayer(startingPlayerDetails);
+//   setSecondPlayer(otherPlayerDetails);
 
-  return { startingPlayerDetails, otherPlayerDetails };
-}
+//   return { startingPlayerDetails, otherPlayerDetails };
+// }
