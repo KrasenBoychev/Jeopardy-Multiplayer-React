bf3ac30c586cd2f7344721d462ab95879dd80a1a
Krasen Boychev <krasenboychev11@gmail.com>
2025-05-03 11:38:53 +0100

    socket listeners based on whether the user is in game mode

--- client/src/features/socket_connection/socket hooks/useListeners.js
+++ client/src/features/socket_connection/socket hooks/useListeners.js
@@ -1,27 +1,31 @@
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import {
   addNewFriend,
   updateFriendStatus,
 } from "../../game/01. play_page/children/friendsList/friendsSlice";
 import { useGetNotificationsQuery } from "../../../features/notifications/notificationsApiSlice";
+import { selectCurrentUser } from "../../authentication/authSlice";
 
 export default function useListeners(socket) {
+  const user = useSelector(selectCurrentUser);
   const { refetch } = useGetNotificationsQuery("getNotifications");
   const dispatch = useDispatch();
 
   useEffect(() => {
-    socket?.on("getFriendStatus", ({ senderInfo }) => {
-      dispatch(updateFriendStatus(senderInfo));
-    });
+    if (!user.gameDetails.gameInProgress) {
+      socket?.on("getFriendStatus", ({ senderInfo }) => {
+        dispatch(updateFriendStatus(senderInfo));
+      });
 
-    socket?.on("getUpdateNotifications", () => {
-      refetch();
-    });
+      socket?.on("getUpdateNotifications", () => {
+        refetch();
+      });
 
-    socket?.on("getFriendReqAccepted", ({ userDetails }) => {
-      dispatch(addNewFriend(userDetails));
-      refetch();
-    });
+      socket?.on("getFriendReqAccepted", ({ userDetails }) => {
+        dispatch(addNewFriend(userDetails));
+        refetch();
+      });
+    }
   }, [socket]);
 }
