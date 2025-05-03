0b938e119cf054ec1604e6ee71fede821f98b0ed
Krasen Boychev <krasenboychev11@gmail.com>
2025-05-03 10:40:14 +0100

    deleteSocket reducer created

--- client/src/features/socket_connection/socketSlice.js
+++ client/src/features/socket_connection/socketSlice.js
@@ -9,10 +9,14 @@ const socketSlice = createSlice({
       state.socketReqName = socketReqName;
       state.socketData = socketData;
     },
+    deleteSocket: (state, action) => {
+      state.socketReqName = null;
+      state.socketData = null;
+    },
   },
 });
 
-export const { setSocketReq } = socketSlice.actions;
+export const { setSocketReq, deleteSocket } = socketSlice.actions;
 
 export default socketSlice.reducer;
 
