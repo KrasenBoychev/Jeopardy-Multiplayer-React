f548d7eeecad7215a1eb147afbf8221b868c74b7
Krasen Boychev <krasenboychev11@gmail.com>
2025-05-03 18:53:13 +0100

    GameReqBtns.jsx used in FriendsList.jsx

--- client/src/features/game/01. play_page/children/friendsList/FriendsList.jsx
+++ client/src/features/game/01. play_page/children/friendsList/FriendsList.jsx
@@ -1,10 +1,13 @@
-import AddFriendBtn from "./AddFriendBtn";
 import { useSelector } from "react-redux";
 import { selectFriends } from "./friendsSlice";
+import { selectGameReqSentBy } from "../../../gameSlice";
+import AddFriendBtn from "./children/AddFriendBtn";
+import GameReqBtns from "./children/GameReqBtns";
 import "./friendsList.css";
 
 export default function FriendsList() {
   const friends = useSelector(selectFriends);
+  const gameReqSentBy = useSelector(selectGameReqSentBy);
 
   return (
     <div className="friends_list_wrapper">
@@ -24,11 +27,17 @@ export default function FriendsList() {
                 }
               >
                 <span className="friend_username">{friend.username}</span>
-                <span className="friend_status">
-                  {friend.online && !friend.gameInProgress && "Online"}
-                  {friend.online && friend.gameInProgress && "Game in progress"}
-                  {!friend.online && "Offline"}
-                </span>
+                {gameReqSentBy.includes(friend.username) ? (
+                  <GameReqBtns friendUsername={friend.username} />
+                ) : (
+                  <span className="friend_status">
+                    {friend.online && !friend.gameInProgress && "Online"}
+                    {friend.online &&
+                      friend.gameInProgress &&
+                      "Game in progress"}
+                    {!friend.online && "Offline"}
+                  </span>
+                )}
               </li>
             );
           })}
