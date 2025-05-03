6e55a50b8ae2250bd990fc1935ec7e47a3b46626
Krasen Boychev <krasenboychev11@gmail.com>
2025-05-03 18:49:15 +0100

    receiveGameReq and receiveRejectGameRes socket client listeners created; updateGameReqSentBy reducer used in getFriendStatus  socket client listener

--- client/src/features/socket_connection/socket hooks/useListeners.js
+++ client/src/features/socket_connection/socket hooks/useListeners.js
@@ -6,6 +6,8 @@ import {
 } from "../../game/01. play_page/children/friendsList/friendsSlice";
 import { useGetNotificationsQuery } from "../../../features/notifications/notificationsApiSlice";
 import { selectCurrentUser } from "../../authentication/authSlice";
+import { updateGameReqSentBy } from "../../game/gameSlice";
+import toast from "react-hot-toast";
 
 export default function useListeners(socket) {
   const user = useSelector(selectCurrentUser);
@@ -16,6 +18,12 @@ export default function useListeners(socket) {
     if (!user.gameDetails.gameInProgress) {
       socket?.on("getFriendStatus", ({ senderInfo }) => {
         dispatch(updateFriendStatus(senderInfo));
+        dispatch(
+          updateGameReqSentBy({
+            username: senderInfo.username,
+            updateType: "remove",
+          })
+        );
       });
 
       socket?.on("getUpdateNotifications", () => {
@@ -25,6 +33,14 @@ export default function useListeners(socket) {
       socket?.on("getFriendReqAccepted", ({ userDetails }) => {
         dispatch(addNewFriend(userDetails));
         refetch();
+      });
+
+      socket?.on("receiveGameReq", ({ username }) => {
+        dispatch(updateGameReqSentBy({ username, updateType: "add" }));
+      });
+
+      socket?.on("receiveRejectGameRes", ({ username }) => {
+        toast.error(username + " rejected your game request");
       });
     }
   }, [socket]);
