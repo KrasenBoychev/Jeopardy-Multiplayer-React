30d527468f69e59a4f51ccca7dc116aaeb86b3e3
Krasen Boychev <krasenboychev11@gmail.com>
2025-05-04 18:54:53 +0100

    Confrim.jsx uses redux hooks and states

--- client/src/features/game/exitGame/confirm/Confrim.jsx
+++ client/src/features/game/exitGame/confirm/Confrim.jsx
@@ -1,24 +1,29 @@
-import { useNavigate } from "react-router-dom";
-import { useAuthContext } from "../../../../contexts/AuthContext";
-import { useGameContext } from "../../../../contexts/GameContext";
-import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { selectCurrentUser } from "../../../authentication/authSlice";
+import { selectRivalPlayer } from "../../gameSlice";
+import { setSocketReq } from "../../../socket_connection/socketSlice";
 import "./confirm.css";
 
 export default function Confrim({ props }) {
-  const { setShowConfirmMessage, gameRoomName, setIsGameLeft } = props;
-  const { username } = useAuthContext();
-  const { socket, friendSocketId } = useGameContext();
+  const { setShowConfirmMessage, setIsGameLeft } = props;
+  const user = useSelector(selectCurrentUser);
+  const rivalPlayer = useSelector(selectRivalPlayer);
+  const dispatch = useDispatch();
 
-  const declineLeaving = () => {
+  const declineLeavingClickHandler = () => {
     setShowConfirmMessage(false);
   };
 
-  const confirmLeaving = async () => {
-    await socket.emit("setExitGame", {
-      receiverSocketId: friendSocketId,
-      userUsername: username,
-      gameRoomName,
-    });
+  const confirmLeavingClickHandler = async () => {
+    dispatch(
+      setSocketReq({
+        socketReqName: "setExitGame",
+        socketData: {
+          receiverSocketId: rivalPlayer.socketId,
+          username: user.username,
+        },
+      })
+    );
     setIsGameLeft(true);
   };
 
@@ -27,8 +32,8 @@ export default function Confrim({ props }) {
       <div className="confirm-message">
         <h2>Are you sure you want to leave the game?</h2>
         <div className="confirm-buttons">
-          <button onClick={confirmLeaving}>Yes</button>
-          <button onClick={declineLeaving}>No</button>
+          <button onClick={confirmLeavingClickHandler}>Yes</button>
+          <button onClick={declineLeavingClickHandler}>No</button>
         </div>
       </div>
     </div>
