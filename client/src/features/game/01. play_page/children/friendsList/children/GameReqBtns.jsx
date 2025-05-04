29fe3cdac9a44e2cd396bbda9466c7f0ccb579f2
Krasen Boychev <krasenboychev11@gmail.com>
2025-05-04 16:23:51 +0100

    GameReqBtns.jsx completed

--- client/src/features/game/01. play_page/children/friendsList/children/GameReqBtns.jsx
+++ client/src/features/game/01. play_page/children/friendsList/children/GameReqBtns.jsx
@@ -2,36 +2,57 @@ import { useDispatch, useSelector } from "react-redux";
 import { setSocketReq } from "../../../../../socket_connection/socketSlice";
 import { selectFriends } from "../friendsSlice";
 import { selectCurrentUser } from "../../../../../authentication/authSlice";
-import { updateGameReqSentBy, updateRivalPlayer } from "../../../../gameSlice";
+import {
+  updateGameReqSentBy,
+  updateIsNewGameStarted,
+  updateRivalPlayer,
+} from "../../../../gameSlice";
+import { useChangeGameInProgressMutation } from "../../../../gameApiSlice";
+import { setGameInProgress } from "../../gameRoom/setGameFunc";
 
 export default function GameReqBtns({ friendUsername }) {
-  const dispatch = useDispatch();
   const user = useSelector(selectCurrentUser);
   const friends = useSelector(selectFriends);
+  const [changeGameInProgress] = useChangeGameInProgressMutation();
+  const dispatch = useDispatch();
 
-  const findFriend = friends.find(
-    (friend) => friend.username == friendUsername
-  );
+  const acceptGameReqClickHandler = async () => {
+    const findFriend = friends.find(
+      (friend) => friend.username == friendUsername
+    );
 
-  const acceptGameReqClickHandler = () => {
+    dispatch(
+      updateRivalPlayer({
+        username: findFriend.username,
+        socketId: findFriend.socketId,
+        updateType: "add",
+      })
+    );
+
+    dispatch(updateIsNewGameStarted());
+
+    await setGameInProgress(
+      user.username,
+      friends,
+      changeGameInProgress,
+      dispatch
+    );
+
     dispatch(
       setSocketReq({
         socketReqName: "sendAcceptGameRes",
         socketData: {
           receiverSocketId: findFriend.socketId,
-          username: user.username,
         },
       })
     );
-
-    dispatch(
-      updateRivalPlayer({ username: friendUsername, updateType: "add" })
-    );
-
-    //To be completed...
   };
 
   const rejectGameReqClickHandler = () => {
+    const findFriend = friends.find(
+      (friend) => friend.username == friendUsername
+    );
+
     dispatch(
       setSocketReq({
         socketReqName: "sendRejectGameRes",
@@ -63,59 +84,3 @@ export default function GameReqBtns({ friendUsername }) {
     </span>
   );
 }
-
-// function setPlayers(
-//   usernameDetais,
-//   friendDetails,
-//   setFirstPlayer,
-//   setSecondPlayer
-// ) {
-//   const playersNames = [usernameDetais, friendDetails];
-//   const startingPlayerDetails =
-//     playersNames[Math.floor(Math.random() * playersNames.length)];
-//   const indexOfStartingPlayer = playersNames.indexOf(startingPlayerDetails);
-//   playersNames.splice(indexOfStartingPlayer, 1);
-//   const otherPlayerDetails = playersNames[0];
-//   setFirstPlayer(startingPlayerDetails);
-//   setSecondPlayer(otherPlayerDetails);
-//   return { startingPlayerDetails, otherPlayerDetails };
-// }
-
-// else if (notificationType == "gameInvitation") {
-//   const findFriend = friendsList.find(
-//     (friend) => friend.username == friendUsername
-//   );
-
-//   if (findFriend && findFriend.online && !findFriend.gameInProgress) {
-//     const usernameDetais = { username, socketId: socket.id };
-//     const friendDetails = {
-//       username: friendUsername,
-//       socketId: findFriend.socketId,
-//     };
-
-//     const { startingPlayerDetails, otherPlayerDetails } = setPlayers(
-//       usernameDetais,
-//       friendDetails,
-//       setFirstPlayer,
-//       setSecondPlayer
-//     );
-
-//     const roomName = `${friendUsername}-${username}`;
-
-//     await socket.emit("setAcceptGameInvitation", {
-//       receiverSocketId: findFriend.socketId,
-//       userUsername: username,
-//       roomName,
-//       playersInfo: { startingPlayerDetails, otherPlayerDetails },
-//     });
-
-//     setFriendInvited(friendUsername);
-//     setGameRoomName(roomName);
-//     setIsNewGameStarted(true);
-//   } else {
-//     // If a bug occurs, then this message will show
-//     toast.error(
-//       friendUsername + " is no longer online - please refresh the page"
-//     );
-//   }
-// }
