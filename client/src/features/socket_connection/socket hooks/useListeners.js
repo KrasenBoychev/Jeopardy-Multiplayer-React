75462a905f8b583572db3dc6eab6a12939531fba
Krasen Boychev <krasenboychev11@gmail.com>
2025-05-04 16:15:19 +0100

    new client socket listeners created

--- client/src/features/socket_connection/socket hooks/useListeners.js
+++ client/src/features/socket_connection/socket hooks/useListeners.js
@@ -2,12 +2,22 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import {
   addNewFriend,
+  updateFriendGameInProgress,
   updateFriendStatus,
 } from "../../game/01. play_page/children/friendsList/friendsSlice";
 import { useGetNotificationsQuery } from "../../../features/notifications/notificationsApiSlice";
 import { selectCurrentUser } from "../../authentication/authSlice";
-import { updateGameReqSentBy } from "../../game/gameSlice";
+import {
+  updateGameReqSentBy,
+  updateIsNewGameStarted,
+  updateReadyToPlay,
+  updateRivalPlayer,
+  updateSetStartGameDetails,
+  updateStartingPlayers,
+} from "../../game/gameSlice";
 import toast from "react-hot-toast";
+import { setCategories } from "../../game/04. categories/categoriesSlice";
+import { setSocketReq } from "../socketSlice";
 
 export default function useListeners(socket) {
   const user = useSelector(selectCurrentUser);
@@ -21,6 +31,14 @@ export default function useListeners(socket) {
         dispatch(
           updateGameReqSentBy({
             username: senderInfo.username,
+            updateType: "remove",
+          })
+        );
+
+        dispatch(
+          updateRivalPlayer({
+            username: senderInfo.username,
+            socketId: "",
             updateType: "remove",
           })
         );
@@ -40,7 +58,49 @@ export default function useListeners(socket) {
       });
 
       socket?.on("receiveRejectGameRes", ({ username }) => {
+        dispatch(
+          updateRivalPlayer({ username, socketId: "", updateType: "remove" })
+        );
         toast.error(username + " rejected your game request");
+      });
+
+      socket?.on("getCancelGameInvitation", ({ username }) => {
+        dispatch(updateGameReqSentBy({ username, updateType: "remove" }));
+      });
+
+      socket?.on("getAcceptGameRes", () => {
+        dispatch(updateIsNewGameStarted());
+        dispatch(updateSetStartGameDetails());
+      });
+
+      socket?.on("getFriendGameInProgress", ({ username }) => {
+        dispatch(updateFriendGameInProgress(username));
+      });
+
+      socket?.on("getGameDetails", ({ gameDetails }) => {
+        dispatch(
+          updateStartingPlayers({
+            firstPlayerDetails: gameDetails.firstPlayerDetails,
+            secondPlayerDetails: gameDetails.secondPlayerDetails,
+            updateType: "add",
+          })
+        );
+
+        dispatch(setCategories(gameDetails.newCategories));
+        dispatch(updateReadyToPlay());
+
+        dispatch(
+          setSocketReq({
+            socketReqName: "setReadyToPlay",
+            socketData: {
+              receiverSocketId: gameDetails.userSocketId,
+            },
+          })
+        );
+      });
+
+      socket?.on("getReadyToPlay", () => {
+        dispatch(updateReadyToPlay());
       });
     }
   }, [socket]);
