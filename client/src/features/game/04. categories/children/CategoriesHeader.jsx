2ea3277eff8f0b5fdc19edee848d98a3bd62f78b
Krasen Boychev <krasenboychev11@gmail.com>
2025-05-14 17:24:23 +0100

    redux hooks and state used in 'CategoriesHeader' component

--- client/src/features/game/04. categories/children/CategoriesHeader.jsx
+++ client/src/features/game/04. categories/children/CategoriesHeader.jsx
@@ -1,19 +1,23 @@
-import { useAuthContext } from "../../../../contexts/AuthContext";
+import { useSelector } from "react-redux";
+import { selectCurrentUser } from "../../../authentication/authSlice";
+import { selectActivePlayer } from "../../gameSlice";
+import { selectCategoryCount } from "../categoriesSlice";
 
-export default function CategoriesHeader({ props }) {
-  const { activePlayer, currCategoryCount } = props;
-  const { username } = useAuthContext();
+export default function CategoriesHeader() {
+  const user = useSelector(selectCurrentUser);
+  const activePlayer = useSelector(selectActivePlayer);
+  const categoriesCount = useSelector(selectCategoryCount);
 
   return (
     <p
       className={
-        username == activePlayer && currCategoryCount < 4
+        user.username == activePlayer.username && categoriesCount <= 3
           ? "active_player player_categories"
           : "player_categories"
       }
     >
-      {currCategoryCount < 4
-        ? `${activePlayer} chooses category`
+      {categoriesCount <= 3
+        ? `${activePlayer.username} chooses category`
         : "Loading Questions..."}
     </p>
   );
