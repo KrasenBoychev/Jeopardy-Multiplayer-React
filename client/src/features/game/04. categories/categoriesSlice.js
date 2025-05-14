609e53b1a34745e5f89f6ee3faad8667d62ea538
Krasen Boychev <krasenboychev11@gmail.com>
2025-05-14 17:26:47 +0100

    'updateGameCategories' and 'updateCategoryCount' reducres added to 'categoriesSlice'

--- client/src/features/game/04. categories/categoriesSlice.js
+++ client/src/features/game/04. categories/categoriesSlice.js
@@ -1,22 +1,36 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+export const defaultOption = "--- Choose Category ---";
+
 const categoriesSlice = createSlice({
   name: "categories",
-  initialState: { categories: null },
+  initialState: {
+    categories: null,
+    gameCategories: [
+      defaultOption,
+      defaultOption,
+      defaultOption,
+      defaultOption,
+    ],
+    categoryCount: 0,
+  },
   reducers: {
     setCategories: (state, action) => {
-      const newCategories = action.payload;
-      state.categories = newCategories;
+      const allCategories = action.payload;
+      state.categories = allCategories;
     },
     updateCategories: (state, action) => {
       const categoryName = action.payload;
-      state.categories = state.categories.map((category) => {
-        if (category.name == categoryName) {
-          category.selected = true;
-        }
-
-        return category;
-      });
+      state.categories = state.categories.filter(
+        (category) => category.name !== categoryName
+      );
+    },
+    updateGameCategories: (state, action) => {
+      const { categoryName, index } = action.payload;
+      state.gameCategories.splice(index, 1, categoryName);
+    },
+    updateCategoryCount: (state, action) => {
+      state.categoryCount = state.categoryCount + 1;
     },
     deleteCategories: (state, action) => {
       state.categories = null;
@@ -24,8 +38,16 @@ const categoriesSlice = createSlice({
   },
 });
 
-export const { setCategories, deleteCategories } = categoriesSlice.actions;
+export const {
+  setCategories,
+  updateCategories,
+  updateGameCategories,
+  updateCategoryCount,
+  deleteCategories,
+} = categoriesSlice.actions;
 
 export default categoriesSlice.reducer;
 
 export const selectCategories = (state) => state.categories.categories;
+export const selectGameCategories = (state) => state.categories.gameCategories;
+export const selectCategoryCount = (state) => state.categories.categoryCount;
