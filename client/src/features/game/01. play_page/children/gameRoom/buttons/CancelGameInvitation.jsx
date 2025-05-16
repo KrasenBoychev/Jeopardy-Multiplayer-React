2867d9039e8fad0d7b922a94793d04cde179846a
Krasen Boychev <krasenboychev11@gmail.com>
2025-05-16 15:51:42 +0100

    CancelGameInvitation component uses 'selectFriends' from redux store

--- client/src/features/game/01. play_page/children/gameRoom/buttons/CancelGameInvitation.jsx
+++ client/src/features/game/01. play_page/children/gameRoom/buttons/CancelGameInvitation.jsx
@@ -1,35 +1,29 @@
 import { useDispatch, useSelector } from "react-redux";
 import { selectRivalPlayer, updateRivalPlayer } from "../../../../gameSlice";
 import { selectCurrentUser } from "../../../../../authentication/authSlice";
-import { selectFriends } from "../../friendsList/friendsSlice";
 import { setSocketReq } from "../../../../../socket_connection/socketSlice";
-import toast from "react-hot-toast";
 
 export default function CancelGameInvitation() {
   const user = useSelector(selectCurrentUser);
-  const friends = useSelector(selectFriends);
   const rivalPlayer = useSelector(selectRivalPlayer);
   const dispatch = useDispatch();
 
   const gameCancelationClickHandler = async () => {
-    const findFriend = friends.find((friend) => friend.username == rivalPlayer);
-
-    if (findFriend && findFriend.online && !findFriend.gameInProgress) {
-      dispatch(
-        setSocketReq({
-          socketReqName: "setCancelGameInvitation",
-          socketData: {
-            receiverSocketId: findFriend.socketId,
-            username: user.username,
-          },
-        })
-      );
-    } else {
-      toast.error("Cannot start game with " + rivalPlayer);
-    }
+    dispatch(
+      setSocketReq({
+        socketReqName: "setCancelGameInvitation",
+        socketData: {
+          receiverSocketId: rivalPlayer.socketId,
+          username: user.username,
+        },
+      })
+    );
 
     dispatch(
-      updateRivalPlayer({ username: rivalPlayer, updateType: "remove" })
+      updateRivalPlayer({
+        username: rivalPlayer.username,
+        updateType: "remove",
+      })
     );
   };
 
