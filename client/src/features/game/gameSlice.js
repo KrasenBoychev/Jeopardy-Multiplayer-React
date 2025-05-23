8245376a1e17a77dd1658625823b05d21d53842d
Krasen Boychev <krasenboychev11@gmail.com>
2025-05-23 13:55:01 +0100

    "isGameFinished" added to gameSlice initial state

--- client/src/features/game/gameSlice.js
+++ client/src/features/game/gameSlice.js
@@ -7,6 +7,7 @@ const gameSlice = createSlice({
     isNewGameStarted: false,
     setStartGameDetails: false,
     readyToPlay: false,
+    isGameFinished: false,
   },
   reducers: {
     updateGameReqSentBy: (state, action) => {
@@ -31,11 +32,15 @@ const gameSlice = createSlice({
     updateReadyToPlay: (state, action) => {
       state.readyToPlay = !state.readyToPlay;
     },
+    updateIsGameFinished: (state, action) => {
+      state.isGameFinished = !state.isGameFinished;
+    },
     deleteGameDetails: (state, action) => {
       state.gameReqSentBy = [];
       state.isNewGameStarted = false;
-      state.setStartGameDetails = null;
-      state.readyToPlay = null;
+      state.setStartGameDetails = false;
+      state.readyToPlay = false;
+      state.isGameFinished = false;
     },
   },
 });
@@ -45,6 +50,7 @@ export const {
   updateIsNewGameStarted,
   updateSetStartGameDetails,
   updateReadyToPlay,
+  updateIsGameFinished,
   deleteGameDetails,
 } = gameSlice.actions;
 
@@ -55,3 +61,4 @@ export const selectIsNewGameStarted = (state) => state.game.isNewGameStarted;
 export const selectSetStartGameDetails = (state) =>
   state.game.setStartGameDetails;
 export const selectReadyToPlay = (state) => state.game.readyToPlay;
+export const selectIsGameFinished = (state) => state.game.isGameFinished;
