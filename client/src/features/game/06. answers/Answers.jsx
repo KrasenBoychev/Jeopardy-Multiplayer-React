e1d695a091bde2950dcf980c9c84f058aaef1aa6
Krasen Boychev <krasenboychev11@gmail.com>
2025-05-24 08:25:16 +0100

    Answers.jsx updated to use redux hooks and states

--- client/src/features/game/06. answers/Answers.jsx
+++ client/src/features/game/06. answers/Answers.jsx
@@ -1,62 +1,62 @@
-import useAnswerClicked from "../../../hooks/game_hooks/useAnswerClicked";
-import AnswerModel from "./children/AnswerModel";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  selectQuestionChosen,
+  updateQuestionAnswered,
+  updateQuestionChosen,
+} from "../05. questions/questionsSlice";
 import AnswersHeader from "./children/AnswersHeader";
+import AnswerModel from "./children/AnswerModel";
 import "../game.css";
 import "./answers.css";
+import { useEffect } from "react";
+import { deleteAnswerDetails, selectIsAnswerCorrect } from "./answerSlice";
+import {
+  selectActivePlayer,
+  selectFirstPlayer,
+  updateActivePlayer,
+  updatePlayerPoints,
+} from "../playersSlice";
 
-export default function Answers({ props }) {
-  const {
-    activePlayer,
-    currCategory,
-    currQuestion,
-    isAnswerClicked,
-    isAnswerCorrect,
-    setActivePlayer,
-    setPointsFirstPlayer,
-    setPointsSecondPlayer,
-    setIsAnswerClicked,
-    setIsAnswerCorrect,
-    setRecordAnswer,
-  } = props;
+export default function Answers() {
+  const question = useSelector(selectQuestionChosen);
+  const isAnswerCorrect = useSelector(selectIsAnswerCorrect);
+  const firstPlayer = useSelector(selectFirstPlayer);
+  const activePlayer = useSelector(selectActivePlayer);
+  const dispatch = useDispatch();
 
-  const setAnswerClicked = useAnswerClicked(
-    activePlayer,
-    currQuestion,
-    isAnswerClicked,
-    setActivePlayer,
-    setPointsFirstPlayer,
-    setPointsSecondPlayer,
-    setIsAnswerClicked,
-    setIsAnswerCorrect,
-    setRecordAnswer
-  );
+  useEffect(() => {
+    if (isAnswerCorrect != null) {
+      if (isAnswerCorrect == true) {
+        let playerToUpdate =
+          activePlayer.username == firstPlayer.username
+            ? "firstPlayer"
+            : "secondPlayer";
+
+        dispatch(
+          updatePlayerPoints({
+            player: playerToUpdate,
+            pointsToAdd: question.points,
+          })
+        );
+      }
+
+      setTimeout(() => {
+        dispatch(updateQuestionChosen(null));
+        dispatch(deleteAnswerDetails());
+        dispatch(updateQuestionAnswered(question));
+        dispatch(updateActivePlayer());
+      }, 3000);
+    }
+  }, [isAnswerCorrect]);
 
   return (
     <section className="show_question_container">
-      <AnswersHeader
-        props={{ activePlayer, isAnswerCorrect, currCategory, currQuestion }}
-      />
-
+      <AnswersHeader />
       <div className="show_question_wrapper">
-        <h1>{currQuestion.name}</h1>
-
+        <h1>{question.name}</h1>
         <div className="answers_wrapper">
-          {Object.values(currQuestion.answers).map((answer, index) => {
-            return (
-              <AnswerModel
-                key={answer + index}
-                props={{
-                  answer,
-                  index,
-                  setAnswerClicked,
-                  activePlayer,
-                  currQuestion,
-                  isAnswerClicked,
-                  setIsAnswerClicked,
-                  isAnswerCorrect,
-                }}
-              />
-            );
+          {Object.values(question.answers).map((answer, index) => {
+            return <AnswerModel key={answer + index} answer={answer} />;
           })}
         </div>
       </div>
