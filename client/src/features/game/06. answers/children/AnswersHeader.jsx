3a6090a84677eb22db4272b668d08ca672f6ea7d
Krasen Boychev <krasenboychev11@gmail.com>
2025-05-24 08:25:44 +0100

    AnswersHeader.jsx updated to use redux hooks and states

--- client/src/features/game/06. answers/children/AnswersHeader.jsx
+++ client/src/features/game/06. answers/children/AnswersHeader.jsx
@@ -1,16 +1,29 @@
-import { useAuthContext } from "../../../../contexts/AuthContext";
+import { useSelector } from "react-redux";
+import { selectCurrentUser } from "../../../authentication/authSlice";
+import { selectActivePlayer } from "../../playersSlice";
+import { selectIsAnswerCorrect } from "../answerSlice";
 import "../answers.css";
 import "../../game.css";
+import { selectQuestionChosen } from "../../05. questions/questionsSlice";
+import { selectCategories } from "../../04. categories/categoriesSlice";
 
-export default function AnswersHeader({ props }) {
-  const { activePlayer, isAnswerCorrect, currCategory, currQuestion } = props;
-  const { username } = useAuthContext();
+export default function AnswersHeader() {
+  const activePlayer = useSelector(selectActivePlayer);
+  const user = useSelector(selectCurrentUser);
+  const question = useSelector(selectQuestionChosen);
+  const categories = useSelector(selectCategories);
+  const isAnswerCorrect = useSelector(selectIsAnswerCorrect);
+
+  const findCategoryName = categories.find(
+    (category) => category._id == question.categoryId
+  );
+
   return (
     <>
       <p
         className={
           isAnswerCorrect == null
-            ? username === activePlayer
+            ? user.username === activePlayer.username
               ? "player_categories active_player"
               : "player_categories"
             : isAnswerCorrect
@@ -19,7 +32,7 @@ export default function AnswersHeader({ props }) {
         }
       >
         {isAnswerCorrect == null
-          ? `${activePlayer} answers question`
+          ? `${activePlayer.username} answers question`
           : isAnswerCorrect
           ? "Correct Answer"
           : "Wrong Answer"}
@@ -27,7 +40,7 @@ export default function AnswersHeader({ props }) {
 
       <div className="show_question_category">
         <h3>
-          {currCategory} - {currQuestion.points} points
+          {findCategoryName.name} - {question.points} points
         </h3>
       </div>
     </>
