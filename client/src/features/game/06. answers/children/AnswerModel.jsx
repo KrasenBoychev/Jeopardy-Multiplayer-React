ed84e859df07f2f054c8a3d14834d139419b4cf0
Krasen Boychev <krasenboychev11@gmail.com>
2025-05-24 08:25:28 +0100

    AnswerModel.jsx updated to use redux hooks and states

--- client/src/features/game/06. answers/children/AnswerModel.jsx
+++ client/src/features/game/06. answers/children/AnswerModel.jsx
@@ -1,50 +1,69 @@
-import { useAuthContext } from "../../../../contexts/AuthContext";
-import { useGameContext } from "../../../../contexts/GameContext";
+import { useDispatch, useSelector } from "react-redux";
+import { selectActivePlayer, selectRivalPlayer } from "../../playersSlice";
+import { selectCurrentUser } from "../../../authentication/authSlice";
+import { selectQuestionChosen } from "../../05. questions/questionsSlice";
+import {
+  selectAnswerChosen,
+  selectIsAnswerCorrect,
+  setAnswerChosen,
+  updateIsAnswerCorrect,
+} from "../answerSlice";
 import "../answers.css";
+import { setSocketReq } from "../../../socket_connection/socketSlice";
 
-export default function AnswerModel({ props }) {
-  const {
-    answer,
-    index,
-    setAnswerClicked,
-    activePlayer,
-    currQuestion,
-    isAnswerClicked,
-    setIsAnswerClicked,
-    isAnswerCorrect,
-  } = props;
-
-  const { username } = useAuthContext();
-  const { socket, friendSocketId } = useGameContext();
+export default function AnswerModel({ answer }) {
+  const rivalPlayer = useSelector(selectRivalPlayer);
+  const activePlayer = useSelector(selectActivePlayer);
+  const user = useSelector(selectCurrentUser);
+  const questionChosen = useSelector(selectQuestionChosen);
+  const answerChosen = useSelector(selectAnswerChosen);
+  const isAnswerCorrect = useSelector(selectIsAnswerCorrect);
+  const dispatch = useDispatch();
 
   const answerQuestionClickHandler = async () => {
-    setAnswerClicked(answer);
-    setIsAnswerClicked(true);
+    let setIsAnswerCorrect = false;
+    if (questionChosen.correctAnswer == answer) {
+      setIsAnswerCorrect = true;
+    }
 
-    await socket.emit("sendAnswerClicked", {
-      receiverSocketId: friendSocketId,
-      answerChosen: answer,
-    });
+    dispatch(updateIsAnswerCorrect(setIsAnswerCorrect));
+    dispatch(setAnswerChosen(answer));
+    dispatch(
+      setSocketReq({
+        socketReqName: "sendAnswerChosen",
+        socketData: {
+          receiverSocketId: rivalPlayer.socketId,
+          answer,
+          setIsAnswerCorrect,
+        },
+      })
+    );
   };
 
   return (
     <button
-      key={answer + index}
-      disabled={username != activePlayer || isAnswerClicked ? true : false}
-      onClick={answerQuestionClickHandler}
-      className={
-        isAnswerCorrect == null
-          ? username === activePlayer
-            ? "answer_box answer_box_active"
-            : "answer_box"
-          : isAnswerCorrect && currQuestion.correctAnswer == answer
-          ? "answer_box answer_correct"
-          : !isAnswerCorrect
-          ? currQuestion.correctAnswer == answer
-            ? "answer_box answer_correct"
-            : "answer_box answer_wrong"
-          : "answer_box inactive_player"
+      disabled={
+        user.username != activePlayer.username || answerChosen ? true : false
       }
+      onClick={answerQuestionClickHandler}
+      className={`answer_box 
+        ${
+          isAnswerCorrect == null &&
+          user.username === activePlayer.username &&
+          "answer_box_active"
+        }
+        ${
+          isAnswerCorrect != null &&
+          questionChosen.correctAnswer == answer &&
+          "answer_correct"
+        }
+        ${
+          isAnswerCorrect != null &&
+          questionChosen.correctAnswer != answer &&
+          "answer_wrong"
+        }
+        ${isAnswerCorrect != null && answerChosen == answer && "answer_chosen"}
+      `}
     >
       {answer}
     </button>
