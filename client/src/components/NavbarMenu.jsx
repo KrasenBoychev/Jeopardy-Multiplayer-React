d4d932427ddd767459d0baa0e83a33d84e0218e3
Krasen Boychev <krasenboychev11@gmail.com>
2025-05-25 19:40:49 +0100

    "Header" component replaced by "NavbarMenu" component

---
+++ client/src/components/NavbarMenu.jsx
@@ -0,0 +1,43 @@
+"use client";
+import { useState } from "react";
+import { HoveredLink, Menu, MenuItem, MenuItemLink } from "./ui/navbar-menu";
+import { cn } from "@/lib/utils";
+import { useSelector } from "react-redux";
+import { selectCurrentUser } from "../features/authentication/authSlice";
+
+export function NavbarMenu() {
+  return (
+    <div className="relative w-full flex items-center justify-center">
+      <Navbar className="top-2" />
+    </div>
+  );
+}
+
+function Navbar({ className }) {
+  const [active, setActive] = useState(null);
+  const user = useSelector(selectCurrentUser);
+
+  return (
+    <div
+      className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}
+    >
+      <Menu setActive={setActive}>
+        <MenuItemLink href="/">Home</MenuItemLink>
+        <MenuItemLink href="/about">About</MenuItemLink>
+        {user ? (
+          <>
+            <MenuItemLink href="/play">Play</MenuItemLink>
+            <MenuItemLink href="/logout">Logout</MenuItemLink>
+          </>
+        ) : (
+          <MenuItem setActive={setActive} active={active} item="Account">
+            <div className="flex flex-col space-y-4 text-sm">
+              <HoveredLink href="/login">Login</HoveredLink>
+              <HoveredLink href="/register">Register</HoveredLink>
+            </div>
+          </MenuItem>
+        )}
+      </Menu>
+    </div>
+  );
+}
