0c86aaaab343799bf0d308f7a1f2465380ec031a
Krasen Boychev <krasenboychev11@gmail.com>
2025-05-25 10:36:32 +0100

    ResultGame.jsx uses "firstPlayer" and "secondPlayer" from the redux store

--- client/src/features/game/07. final_result/ResultGame.jsx
+++ client/src/features/game/07. final_result/ResultGame.jsx
@@ -1,32 +1,32 @@
-import { useGameContext } from "../../../contexts/GameContext";
+import { useSelector } from "react-redux";
 import "./resultGame.css";
+import { selectFirstPlayer, selectSecondPlayer } from "../playersSlice";
 
-export default function ResultGame({ props }) {
-  const { pointsFirstPlayer, pointsSecondPlayer } = props;
-
-  const { firstPlayerUsername, secondPlayerUsername } = useGameContext();
+export default function ResultGame() {
+  const firstPlayer = useSelector(selectFirstPlayer);
+  const secondPlayer = useSelector(selectSecondPlayer);
 
   return (
     <>
       <div className="result_game_container">
         <p className="result_winner answer_correct">
-          {pointsFirstPlayer == pointsSecondPlayer
-            ? `${firstPlayerUsername} and ${secondPlayerUsername} share the first place!`
-            : pointsFirstPlayer > pointsSecondPlayer
-            ? `Congratulations ${firstPlayerUsername}!`
-            : `Congratulations ${secondPlayerUsername}!`}
+          {firstPlayer.earnedPoints == secondPlayer.earnedPoints
+            ? `${firstPlayer.username} and ${secondPlayer.username} share the first place!`
+            : firstPlayer.earnedPoints > secondPlayer.earnedPoints
+            ? `Congratulations ${firstPlayer.username}!`
+            : `Congratulations ${secondPlayer.username}!`}
         </p>
         <div className="final_result">
           <p>Final Result</p>
           <p className="final_players">
             <span className="final_first_player">
-              <span>{firstPlayerUsername}:</span>
-              <span>{pointsFirstPlayer} points</span>
+              <span>{firstPlayer.username}:</span>
+              <span>{firstPlayer.earnedPoints} points</span>
             </span>
             <span className="final_vs">VS</span>
             <span className="final_second_player">
-              <span>{secondPlayerUsername}:</span>
-              <span>{pointsSecondPlayer} points</span>
+              <span>{secondPlayer.username}:</span>
+              <span>{secondPlayer.earnedPoints} points</span>
             </span>
           </p>
         </div>
