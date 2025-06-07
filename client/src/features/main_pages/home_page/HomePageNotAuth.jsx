8e10ee19f26c43f41b157fa1579bde2b6f7b33d1
Krasen Boychev <krasenboychev11@gmail.com>
2025-06-07 14:17:26 +0100

    HomePageNotAuth.jsx renders the corerct elements and components based on whether the user is logged in or not

--- client/src/features/main_pages/home_page/HomePageNotAuth.jsx
+++ client/src/features/main_pages/home_page/HomePageNotAuth.jsx
@@ -1,7 +1,11 @@
 import { useNavigate } from "react-router-dom";
 import { Vortex } from "@/components/ui/vortex";
+import { useSelector } from "react-redux";
+import { selectCurrentUser } from "../../authentication/authSlice";
+import { TopPlayers } from "./TopPlayers";
 
 export function HomePageNotAuth() {
+  const user = useSelector(selectCurrentUser);
   const navigate = useNavigate();
 
   const openPageClickHandler = () => {
@@ -9,25 +13,35 @@ export function HomePageNotAuth() {
   };
 
   return (
-    <div className="w-[calc(100%)] mx-auto h-[] overflow-hidden">
-      <Vortex
-        backgroundColor="black"
-        className="flex items-center flex-col justify-center px-2 md:px-10 py-4 w-full h-full"
-      >
+    <div className="w-[calc(100%)] mx-auto h-auto overflow-hidden bg-black">
+      <Vortex className="flex flex-col items-center justify-center px-2 md:px-10 pt-20 pb-4 w-full h-full">
         <h2 className="text-white text-2xl md:text-6xl font-bold text-center uppercase">
-          Jeopardy Multiplayer Game
+          {user ? `Welcome ${user.username}` : "Jeopardy Multiplayer Game"}
         </h2>
         <p className="text-white text-sm md:text-2xl max-w-xl mt-6 text-center">
-          Beat your opponents with knowledge
+          {user
+            ? `Your Points: ${user.gameDetails.points}`
+            : "Beat your opponents with knowledge"}
         </p>
-        <div className="flex flex-col sm:flex-row items-center gap-4 mt-6">
-          <button
-            className="px-15 py-3 bg-blue-600 hover:bg-blue-700 cursor-pointer transition duration-200 rounded-lg text-2xl text-white uppercase shadow-[0px_2px_0px_0px_#FFFFFF40_inset]"
-            onClick={openPageClickHandler}
-          >
-            Play
-          </button>
-        </div>
+        {!user && (
+          <div className="flex flex-col sm:flex-row items-center gap-4 mt-6">
+            <button
+              className="px-15 py-3 bg-blue-600 hover:bg-blue-700 cursor-pointer transition duration-200 rounded-lg text-2xl text-white uppercase shadow-[0px_2px_0px_0px_#FFFFFF40_inset]"
+              onClick={openPageClickHandler}
+            >
+              Play
+            </button>
+          </div>
+        )}
+
+        {user && (
+          <>
+            <h3 className="text-green-500 text-sm md:text-3xl max-w-xl mt-11 text-center font-bold uppercase">
+              Leaderboard
+            </h3>
+            <TopPlayers />
+          </>
+        )}
       </Vortex>
     </div>
   );
