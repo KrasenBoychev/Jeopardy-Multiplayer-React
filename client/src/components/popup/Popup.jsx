7b22706d6e5e8178a754c1fdbd8c7cc049e39084
Krasen Boychev <krasenboychev11@gmail.com>
2025-06-10 20:02:00 +0100

    style udpated for Popup.jsx

--- client/src/components/popup/Popup.jsx
+++ client/src/components/popup/Popup.jsx
@@ -5,13 +5,13 @@ import "./popup.css";
 export default function PopupComp({ openBtnName, heading, content }) {
   return (
     <Popup
-      trigger={<button className="button">{openBtnName}</button>}
+      trigger={<button className="button cursor-pointer bg-black text-white rounded-xl py-2 px-3">{openBtnName}</button>}
       modal
       nested
     >
       {(close) => (
         <div className="modal">
-          <button className="close" onClick={close}>
+          <button className="close text-black" onClick={close}>
             &times;
           </button>
           <div className="header">{heading}</div>
