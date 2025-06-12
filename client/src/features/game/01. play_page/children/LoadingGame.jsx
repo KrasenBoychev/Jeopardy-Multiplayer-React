286c07f25d24fa64e0ca769242071d043c705ecd
Krasen Boychev <krasenboychev11@gmail.com>
2025-06-12 08:50:05 +0100

    LoadingGame.jsx created

---
+++ client/src/features/game/01. play_page/children/LoadingGame.jsx
@@ -0,0 +1,15 @@
+"use client";
+import React from "react";
+import { Button } from "@/components/ui/moving-border";
+
+export function LoadingGame() {
+  return (
+    <div>
+      <Button
+        borderRadius="1.75rem"
+        className="bg-white text-xl uppercase dark:bg-slate-900 text-black dark:text-white border-neutral-200 dark:border-slate-800">
+        Loading Game
+      </Button>
+    </div>
+  );
+}
