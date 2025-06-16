fea460b42b9e592b27044c090ed9b2623611da0c
Krasen Boychev <krasenboychev11@gmail.com>
2025-06-16 08:36:29 +0100

    GameRoom.jsx doesn't use useEffect hook anymore - it has been passed to PlayPage.jsx

--- client/src/features/game/01. play_page/children/game_room/GameRoom.jsx
+++ client/src/features/game/01. play_page/children/game_room/GameRoom.jsx
@@ -1,69 +1,18 @@
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import {
-  useChangeGameInProgressMutation,
-  useGetCategoriesMutation,
-} from "../../../gameApiSlice";
+import { useSelector } from "react-redux";
 import { selectCurrentUser } from "../../../../authentication/authSlice";
-import {
-  selectIsNewGameStarted,
-  selectSetStartGameDetails,
-} from "../../../gameSlice";
 import { selectRivalPlayer } from "../../../playersSlice";
-import CancelGameInvitation from "./buttons/CancelGameInvitation";
-import InviteFriend from "./buttons/InviteFriendBtn";
-import { selectFriends } from "../friends_list/friendsSlice";
-import {
-  setDataToOtherPlayer,
-  setGameInProgress,
-  setPlayersDetails,
-} from "./setGameFunc";
-import "./gameRoom.css";
 import { ThreeDCardDemo } from "./Card";
+import "./gameRoom.css";
 
 export default function GameRoom() {
   const user = useSelector(selectCurrentUser);
-  const friends = useSelector(selectFriends);
   const rivalPlayer = useSelector(selectRivalPlayer);
-  const isNewGameStarted = useSelector(selectIsNewGameStarted);
-  const setStartGameDetails = useSelector(selectSetStartGameDetails);
-  const [changeGameInProgress] = useChangeGameInProgressMutation();
-  const [getCategories] = useGetCategoriesMutation();
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    if (setStartGameDetails) {
-      (async () => {
-        await setGameInProgress(
-          user.username,
-          friends,
-          changeGameInProgress,
-          dispatch
-        );
-        const [firstPlayerDetails, secondPlayerDetails] = setPlayersDetails(
-          user,
-          rivalPlayer,
-          dispatch
-        );
-        setDataToOtherPlayer(
-          firstPlayerDetails,
-          secondPlayerDetails,
-          rivalPlayer,
-          user.gameDetails.socketId,
-          getCategories,
-          dispatch
-        );
-      })();
-    }
-  }, [setStartGameDetails]);
 
   return (
     <div className="game_room_wrapper">
       <ThreeDCardDemo player={user} />
       <p className="text-white">VS</p>
       <ThreeDCardDemo player={rivalPlayer} rivalPlayer={true} />
-
-      {/* {isNewGameStarted && <p className="game_room_btn">Loading Game...</p>} */}
     </div>
   );
 }
