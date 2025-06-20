8484a6442fe8f83f6372974bf9ffee056378b549
Krasen Boychev <krasenboychev11@gmail.com>
2025-06-20 19:00:29 +0100

    CreateCategory.jsx created - the new version uses values from redux store and uses tawilind library components

---
+++ client/src/features/create/CreateCategory.jsx
@@ -0,0 +1,43 @@
+import { useDispatch } from "react-redux";
+import {
+  goToNextPage,
+  setCategoryName,
+} from "./createSlice";
+import { PlaceholdersAndVanishInput } from "@/components/ui/placeholders-and-vanish-input";
+import { useState } from "react";
+
+export default function CreateCategory() {
+  const [categoryNameValue, setCategoryNameValue] = useState("");
+  const dispatch = useDispatch();
+
+  const placeholders = ["What will be the new category called?"];
+
+  const handleChange = (e) => {
+    setCategoryNameValue(e.target.value);
+  };
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    if (categoryNameValue.trim() == "") {
+      return;
+    }
+
+    // Check if this category exists - if yes show error msg
+
+    dispatch(setCategoryName(categoryNameValue.trim()));
+    dispatch(goToNextPage());
+  };
+
+  return (
+    <div className="flex flex-col justify-center  items-center px-4">
+      <h2 className="mb-10 sm:mb-20 text-xl text-center sm:text-5xl dark:text-white text-black uppercase">
+        Category Name
+      </h2>
+      <PlaceholdersAndVanishInput
+        placeholders={placeholders}
+        onChange={handleChange}
+        onSubmit={onSubmit}
+      />
+    </div>
+  );
+}
