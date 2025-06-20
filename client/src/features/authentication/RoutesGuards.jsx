a20191d398f50b2857c557291139c27ed38d7427
Krasen Boychev <krasenboychev11@gmail.com>
2025-06-20 19:02:26 +0100

    NoAuthGuard and AdminGuard added to RoutesGuards.jsx

---
+++ client/src/features/authentication/RoutesGuards.jsx
@@ -0,0 +1,37 @@
+import { useLocation, Navigate, Outlet } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { selectCurrentToken, selectCurrentUser } from "./authSlice";
+import { adminId } from "../../app/credentials";
+
+export function RequireAuthGuard() {
+  const token = useSelector(selectCurrentToken);
+  const location = useLocation();
+
+  return token ? (
+    <Outlet />
+  ) : (
+    <Navigate to="/login" state={{ from: location }} replace />
+  );
+}
+
+export function NoAuthGuard() {
+  const token = useSelector(selectCurrentToken);
+  const location = useLocation();
+
+  return token ? (
+    <Navigate to="/" state={{ from: location }} replace />
+  ) : (
+    <Outlet />
+  );
+}
+
+export function AdminGuard() {
+  const token = useSelector(selectCurrentToken);
+  const user = useSelector(selectCurrentUser);
+
+  return token && user.userId == adminId ? (
+    <Outlet />
+  ) : (
+    <Navigate to="/" state={{ from: location }} replace />
+  );
+}
