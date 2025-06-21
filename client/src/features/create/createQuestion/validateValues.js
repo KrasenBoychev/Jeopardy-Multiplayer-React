c3dffe370b6d78226a08f1ee09038ffeacb4a936
Krasen Boychev <krasenboychev11@gmail.com>
2025-06-21 18:13:42 +0100

    validateValues.jsx completed - no default values accepted for select fields and no empty fields accepted for input fields

---
+++ client/src/features/create/createQuestion/validateValues.js
@@ -0,0 +1,137 @@
+export const gamePoints = ["5", "10", "15", "20"];
+export const correctAnswerValues = [
+  "Answer 1",
+  "Answer 2",
+  "Answer 3",
+  "Answer 4",
+];
+
+export function validateAllValues(
+  formValues,
+  setFormValues,
+  allCategoriesNames,
+  categoryName,
+  currentPage
+) {
+  Object.keys(formValues).forEach((element) => {
+    setNewFormValues(
+      setFormValues,
+      element,
+      formValues[`${element}`].content,
+      allCategoriesNames,
+      categoryName,
+      currentPage
+    );
+  });
+
+  let areFormValuesValid = true;
+
+  Object.values(formValues).forEach((element) => {
+    if (element.error) {
+      areFormValuesValid = false;
+    }
+  });
+
+  return areFormValuesValid;
+}
+
+export const setNewFormValues = (
+  setFormValues,
+  elementChanged,
+  newContent,
+  allCategoriesNames,
+  categoryName,
+  currentPage
+) => {
+  setFormValues((prev) => {
+    let newValues = Object.assign({}, prev);
+    newValues[`${elementChanged}`].content = newContent;
+
+    let isError;
+
+    switch (elementChanged) {
+      case "category":
+        isError = checkCategoryField(
+          newContent,
+          allCategoriesNames,
+          categoryName,
+          currentPage
+        );
+        break;
+      case "points":
+        isError = checkPointsField(newContent);
+        break;
+      case "question":
+        isError = checkQuestionOrAnswerField(newContent);
+        break;
+      case "answerOne":
+        isError = checkQuestionOrAnswerField(newContent);
+        break;
+      case "answerTwo":
+        isError = checkQuestionOrAnswerField(newContent);
+        break;
+      case "answerThree":
+        isError = checkQuestionOrAnswerField(newContent);
+        break;
+      case "answerFour":
+        isError = checkQuestionOrAnswerField(newContent);
+        break;
+      case "correctAnswer":
+        isError = checkCorrectAnswerField(newContent);
+        break;
+    }
+
+    if (isError) {
+      newValues[`${elementChanged}`].error = true;
+    } else {
+      newValues[`${elementChanged}`].error = false;
+    }
+
+    return newValues;
+  });
+};
+
+const checkCategoryField = (
+  category,
+  allCategoriesNames,
+  categoryName,
+  currentPage
+) => {
+  if (currentPage == 0) {
+    if (!allCategoriesNames.includes(category)) {
+      return true;
+    } else {
+      return false;
+    }
+  } else {
+    if (category !== categoryName) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+};
+
+const checkPointsField = (points) => {
+  if (!gamePoints.includes(points)) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const checkQuestionOrAnswerField = (element) => {
+  if (element.trim() == "") {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const checkCorrectAnswerField = (correctAnswer) => {
+  if (!correctAnswerValues.includes(correctAnswer)) {
+    return true;
+  } else {
+    return false;
+  }
+};
