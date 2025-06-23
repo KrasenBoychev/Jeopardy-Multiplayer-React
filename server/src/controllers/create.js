6edbbab637ff43564d8ba64e3c9cf658239c3252
Krasen Boychev <krasenboychev11@gmail.com>
2025-06-23 18:31:52 +0100

    new controllers created when a single question or category and questions are created

--- server/src/controllers/create.js
+++ server/src/controllers/create.js
@@ -1,67 +1,43 @@
-// const { Router } = require('express');
-// const validator = require('validator');
-// const { body, validationResult } = require('express-validator');
-// const { parseError } = require('../util');
+const { Router } = require("express");
+const { parseError } = require("../util");
 
-// const { isAdmin } = require('../middlewares/guards');
+const { isAdmin } = require("../middlewares/guards");
 
-// const {
-//   createCategoryService,
-//   createQuestionService,
-// } = require('../services/create');
+const {
+  recordCategory,
+  recordQuestion,
+  getCategoryId,
+} = require("../services/create");
 
-// const createRouter = Router();
+const createRouter = Router();
 
-// createRouter.post(
-//   '/category',
-//   isAdmin(),
-//   body('name').trim().notEmpty().withMessage('Name is required'),
-//   async (req, res) => {
-//     try {
-//       const validation = validationResult(req);
+createRouter.post("/singleQuestion", isAdmin(), async (req, res) => {
+  try {
+    const categoryId = await getCategoryId(
+      req.body.questionDetails.categoryName
+    );
+    const result = await recordQuestion(req.body.questionDetails, categoryId);
+    res.json(result);
+  } catch (err) {
+    const parsed = parseError(err);
+    res.status(400).json({ code: 400, message: parsed.errors });
+  }
+});
 
-//       if (validation.errors.length) {
-//         throw validation.errors;
-//       }
+createRouter.post("/categoryAndQuestions", isAdmin(), async (req, res) => {
+  try {
+    const items = req.body;
+    const createCategory = await recordCategory(items.categoryName);
 
-//       const result = await createCategoryService(req.body);
-//       res.json(result);
-//     } catch (err) {
-//       const parsed = parseError(err);
-//       res.status(400).json({ code: 400, message: parsed.errors });
-//     }
-//   }
-// );
+    let result;
+    for (let i = 1; i <= 4; i++) {
+      result = await recordQuestion(items[i], createCategory._id);
+    }
+    res.json(result);
+  } catch (err) {
+    const parsed = parseError(err);
+    res.status(400).json({ code: 400, message: parsed.message });
+  }
+});
 
-// createRouter.post(
-//   '/question/:categoryId',
-//   isAdmin(),
-//   body('name').trim().notEmpty().withMessage('Name is required'),
-//   body('points').trim().isIn(['5', '10', '15', '20']).withMessage('Points should be 5, 10, 15 or 20'),
-//   body('answers[answerOne]').trim().notEmpty().withMessage('Answer One is required'),
-//   body('answers[answerTwo]').trim().notEmpty().withMessage('Answer Two is required'),
-//   body('answers[answerThree]').trim().notEmpty().withMessage('Answer Three is required'),
-//   body('answers[answerFour]').trim().notEmpty().withMessage('Answer Four is required'),
-//   body('correctAnswer').trim().notEmpty().withMessage('Correct Answer is required'),
-//   async (req, res) => {
-//     try {
-//       const validation = validationResult(req);
-
-//       if (validation.errors.length) {
-//         throw validation.errors;
-//       }
-
-//       if(!validator.isMongoId(req.params.categoryId)){
-//         throw new Error('Invalid Category');
-//       }
-
-//       const result = await createQuestionService(req.body, req.params.categoryId);
-//       res.json(result);
-//     } catch (err) {
-//       const parsed = parseError(err);
-//       res.status(400).json({ code: 400, message: parsed.message });
-//     }
-//   }
-// );
-
-// module.exports = { createRouter };
+module.exports = { createRouter };
