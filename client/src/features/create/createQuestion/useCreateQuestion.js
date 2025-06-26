fbf057c97cf01ed2395a2c050fbf315dfb6b3e44
Krasen Boychev <krasenboychev11@gmail.com>
2025-06-26 14:14:46 +0100

    "useCreateQuestion" hook sets the form values

--- client/src/features/create/createQuestion/useCreateQuestion.js
+++ client/src/features/create/createQuestion/useCreateQuestion.js
@@ -1,12 +1,33 @@
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
-import { selectCurrentPage } from "../createSlice";
+import toast from "react-hot-toast";
+import {
+  selectCategoryName,
+  selectCreateItemsAllValues,
+  selectCurrentPage,
+} from "../createSlice";
 import { useGetCategoriesMutation } from "../../game/gameApiSlice";
+import { multipleQuestionsInitialValues } from "./initialValues";
 
-export default function useCreateQuestion() {
+export function useCreateQuestion() {
+  const [formValues, setFormValues] = useState({
+    category: { content: "", error: false },
+    points: { content: "", error: false },
+    question: { content: "", error: false },
+    answerOne: { content: "", error: false },
+    answerTwo: { content: "", error: false },
+    answerThree: { content: "", error: false },
+    answerFour: { content: "", error: false },
+    correctAnswer: {
+      content: "",
+      error: false,
+    },
+  });
   const [allCategoriesNames, setAllCategoriesNames] = useState([]);
   const currentPage = useSelector(selectCurrentPage);
   const [getCategories] = useGetCategoriesMutation();
+  const createItemsAllValues = useSelector(selectCreateItemsAllValues);
+  const categoryName = useSelector(selectCategoryName);
 
   useEffect(() => {
     if (currentPage == 0) {
@@ -21,8 +42,28 @@ export default function useCreateQuestion() {
           toast.error(err.message);
         }
       })();
-    }
-  }, []);
+    } else if (currentPage > 0 && currentPage <= 4) {
+      const formInitialValues = multipleQuestionsInitialValues(
+        createItemsAllValues,
+        currentPage,
+        categoryName
+      );
 
-  return allCategoriesNames;
+      setFormValues({
+        category: { content: formInitialValues.categoryName, error: false },
+        points: { content: formInitialValues.points, error: false },
+        question: { content: formInitialValues.questionName, error: false },
+        answerOne: { content: formInitialValues.answerOne, error: false },
+        answerTwo: { content: formInitialValues.answerTwo, error: false },
+        answerThree: { content: formInitialValues.answerThree, error: false },
+        answerFour: { content: formInitialValues.answerFour, error: false },
+        correctAnswer: {
+          content: formInitialValues.correctAnswer,
+          error: false,
+        },
+      });
+    }
+  }, [currentPage]);
+
+  return [allCategoriesNames, formValues, setFormValues];
 }
