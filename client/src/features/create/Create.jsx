8ae3dde21a4f79a2aa83345967d3e8d271add532
Krasen Boychev <krasenboychev11@gmail.com>
2025-06-26 14:16:32 +0100

    Create.jsx dispatches "deleteItems" func when component renders

--- client/src/features/create/Create.jsx
+++ client/src/features/create/Create.jsx
@@ -1,9 +1,17 @@
+import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { deleteItems } from "./createSlice";
 import { BackgroundBeamsWithCollision } from "@/components/ui/background-beams-with-collision";
 import "./create.css";
 
 export default function Create() {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(deleteItems());
+  }, []);
 
   const createCategoryHandler = () => {
     navigate("/createCatAndQ");
