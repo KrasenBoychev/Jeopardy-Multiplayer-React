5eb1d3d4841806524af86e3c37cab82a4c6c8d12
Krasen Boychev <krasenboychev11@gmail.com>
2025-06-26 14:13:56 +0100

    "checkCategory" and "checkQuestion" controllers created

--- server/src/controllers/create.js
+++ server/src/controllers/create.js
@@ -7,9 +7,33 @@ const {
   recordCategory,
   recordQuestion,
   getCategoryId,
+  checkCategory,
+  checkQuestion,
 } = require("../services/create");
 
 const createRouter = Router();
+
+createRouter.post("/checkCategory", isAdmin(), async (req, res) => {
+  try {
+    const categoryName = req.body.categoryName.toLowerCase();
+    const result = await checkCategory(categoryName);
+    res.json(result);
+  } catch (err) {
+    const parsed = parseError(err);
+    res.status(400).json({ code: 400, message: parsed.errors });
+  }
+});
+
+createRouter.post("/checkQuestion", isAdmin(), async (req, res) => {
+  try {
+    const questionName = req.body.questionName.toLowerCase();
+    const result = await checkQuestion(questionName);
+    res.json(result);
+  } catch (err) {
+    const parsed = parseError(err);
+    res.status(400).json({ code: 400, message: parsed.errors });
+  }
+});
 
 createRouter.post("/singleQuestion", isAdmin(), async (req, res) => {
   try {
@@ -27,14 +51,17 @@ createRouter.post("/singleQuestion", isAdmin(), async (req, res) => {
 
 createRouter.post("/categoryAndQuestions", isAdmin(), async (req, res) => {
   try {
-    const items = req.body.items;
-    const categoryName = items[0][1];
-    const createCategory = await recordCategory(categoryName);
-
     let result;
+
+    const items = req.body.items;
+    const categoryName = items[0][1].toLowerCase();
+    const createCategory = await recordCategory(categoryName);
+    result = createCategory;
+
     for (let i = 1; i <= 4; i++) {
       result = await recordQuestion(items[i][1], createCategory._id);
     }
+
     res.json(result);
   } catch (err) {
     const parsed = parseError(err);
