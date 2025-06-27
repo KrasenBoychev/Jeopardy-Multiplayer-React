96205978b2c57a9c8f73518f09ef3c601538bee2
Krasen Boychev <krasenboychev11@gmail.com>
2025-06-27 14:26:34 +0100

    error boundary implemented in main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,12 +4,21 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import { store } from "./app/store.js";
 import { Provider } from "react-redux";
+import { ErrorBoundary } from "react-error-boundary";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        <App />
+        <ErrorBoundary
+          fallback={
+            <div className="flex justify-center mt-20 text-2xl font-bold uppercase">
+              Something went wrong
+            </div>
+          }
+        >
+          <App />
+        </ErrorBoundary>
       </BrowserRouter>
     </Provider>
   </StrictMode>
