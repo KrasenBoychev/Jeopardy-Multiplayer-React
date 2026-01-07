74e20c35035e54d7accbd02f63fd945a4abf7278
Krasen Boychev <krasenboychev11@gmail.com>
2026-01-07 19:55:19 +0200

    gameDetails and notificationsList are not part of the user object anymore after a user is logged in

--- server/src/controllers/auth.js
+++ server/src/controllers/auth.js
@@ -19,11 +19,13 @@ authRouter.post(
         userId: result._id,
         email: result.email,
         username: result.username,
-        gameDetails: result.gameDetails,
-        notificationsList: result.notificationsList,
       };
 
-      const accessToken = createToken(result.email, result.username, result._id);
+      const accessToken = createToken(
+        result.email,
+        result.username,
+        result._id
+      );
 
       res.json({
         user,
@@ -69,11 +71,13 @@ authRouter.post(
         userId: result._id,
         email: result.email,
         username: result.username,
-        gameDetails: result.gameDetails,
-        notificationsList: result.notificationsList,
       };
 
-      const accessToken = createToken(result.email, result.username, result._id);
+      const accessToken = createToken(
+        result.email,
+        result.username,
+        result._id
+      );
 
       res.json({
         user,
