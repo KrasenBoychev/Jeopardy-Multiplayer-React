6450c296b81146f076ad57733441037340d5fb1a
Krasen Boychev <krasenboychev11@gmail.com>
2026-01-07 22:53:37 +0200

    HomePageAuth.jsx imports UserPoints.jsx

--- client/src/features/main_pages/home_page/HomePageAuth.jsx
+++ client/src/features/main_pages/home_page/HomePageAuth.jsx
@@ -2,6 +2,7 @@ import { useSelector } from "react-redux";
 import { selectCurrentUser } from "../../authentication/authSlice";
 import { TopPlayers } from "./TopPlayers";
 import PlayBtn from "./PlayBtn";
+import UserPoints from "./UserPoints";
 
 export default function HomePageAuth() {
   const user = useSelector(selectCurrentUser);
@@ -11,10 +12,7 @@ export default function HomePageAuth() {
         <h2 className="text-white mt-15 w-full text-2xl md:text-6xl font-bold text-center uppercase tracking-wider [word-spacing:8px] font-[Pacifico]">
           Welcome {user.username}
         </h2>
-
-        <p className="shadow-lg shadow-chart-4 text-white text-sm md:text-2xl max-w-xl mt-6 text-center uppercase bg-black p-3 rounded-lg">
-          {user.gameDetails.points} game points
-        </p>
+        <UserPoints />
       </section>
       <section className="flex-1 flex w-full max-h-full">
         <div className="flex flex-1 h-full w-full">
