5c7ca37d73c837898433ae794ab9de29a81383f3
Krasen Boychev <krasenboychev11@gmail.com>
2026-01-07 18:21:02 +0200

    Logout is not responsible anymore to disconnect the socket - it happens in Socket.jsx

--- client/src/features/authentication/Logout.jsx
+++ client/src/features/authentication/Logout.jsx
@@ -1,40 +1,18 @@
 import { Navigate } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { useEffect } from "react";
 import toast from "react-hot-toast";
-import { deleteCredentials, selectCurrentUser } from "./authSlice";
+import { deleteCredentials } from "./authSlice";
 import { useLogOutMutation } from "./authApiSlice";
-import { useChangeOnlineStatusMutation } from "../socket_connection/socketApiSlice";
-import { useGetOnlineFriendsMutation } from "../game/01. play_page/friends_list/friendsApiSlice";
 import Loader from "../../components/Loader";
 
-export default function Logout({ socket }) {
-  const user = useSelector(selectCurrentUser);
+export default function Logout() {
   const [logOut, { isSuccess }] = useLogOutMutation();
-  const [changeOnlineStatus] = useChangeOnlineStatusMutation();
-  const [getOnlineFriends] = useGetOnlineFriendsMutation();
   const dispatch = useDispatch();
 
   useEffect(() => {
     (async function logout() {
       try {
-        const getOnlineFriendsServerRes = await getOnlineFriends(
-          user.gameDetails.friendsList
-        );
-        const onlineFriends = getOnlineFriendsServerRes.data;
-
-        if (onlineFriends) {
-          socket.emit("sendUserStatus", {
-            senderInfo: {
-              username: user.username,
-              socketId: "",
-            },
-            receiverFriends: onlineFriends,
-          });
-        }
-
-        await changeOnlineStatus({ username: user.username, socketId: "" });
-
         localStorage.removeItem("auth");
         await logOut();
         dispatch(deleteCredentials());
