18581b4b0aaa836fb09f24b67929623d8207900c
Krasen Boychev <krasenboychev11@gmail.com>
2026-01-09 21:10:22 +0200

    Footer has responsive design

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -6,12 +6,12 @@ export default function Footer() {
       className="absolute w-full flex
      bottom-0 right-0 p-[5px] uppercase font-bold text-white"
     >
-      <div className="flex-1 text-center z-10 text-[14px]">
+      <div className="flex-1 text-center z-10 text-[14px] max-[1600px]:text-[12px] max-[1400px]:text-[10px]">
         <Link to={"/about"} className="cursor-pointer hover:underline">
           Game Rules
         </Link>
       </div>
-      <div className="absolute right-2 text-[11px]">
+      <div className="absolute right-2 self-end text-[11px] max-[1600px]:text-[9px] max-[1400px]:text-[7px]">
         &copy; Jeopardy 2024 created by Krasen Boychev
       </div>
     </footer>
