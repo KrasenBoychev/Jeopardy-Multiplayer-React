c8aed7a6e544ba2ad1f653936d9d39a213f0436d
Krasen Boychev <krasenboychev11@gmail.com>
2026-01-10 17:00:34 +0200

    PlayPage is simplified and it's used as a middleware

--- client/src/features/game/01. play_page/PlayPage.jsx
+++ client/src/features/game/01. play_page/PlayPage.jsx
@@ -1,21 +1,5 @@
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import {
-  selectIsNewGameStarted,
-  selectReadyToPlay,
-  selectSetStartGameDetails,
-} from "../gameSlice";
-import {
-  useChangeGameInProgressMutation,
-  useGetCategoriesMutation,
-} from "../gameApiSlice";
-import { selectCurrentUser } from "../../authentication/authSlice";
-import { selectRivalPlayer } from "../playersSlice";
-// import {
-//   setDataToOtherPlayer,
-//   setGameInProgress,
-//   setPlayersDetails,
-// } from "./game_room/setGameFunc";
+import { useSelector } from "react-redux";
+import { selectIsNewGameStarted, selectReadyToPlay } from "../gameSlice";
 import { LoadingGame } from "./LoadingGame";
 import StartingPlayer from "../02. starting_player/StartingPlayer";
 import FriendsList from "./friends_list/FriendsList";
@@ -23,44 +7,12 @@ import { GameCard } from "./game_room/GameCard";
 
 export default function PlayPage() {
   const readyToPlay = useSelector(selectReadyToPlay);
-  const user = useSelector(selectCurrentUser);
-  const rivalPlayer = useSelector(selectRivalPlayer);
   const isNewGameStarted = useSelector(selectIsNewGameStarted);
-  const setStartGameDetails = useSelector(selectSetStartGameDetails);
-  const [changeGameInProgress] = useChangeGameInProgressMutation();
-  const [getCategories] = useGetCategoriesMutation();
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    // if (setStartGameDetails) {
-    //   (async () => {
-    //     await setGameInProgress(
-    //       user.username,
-    //       friends,
-    //       changeGameInProgress,
-    //       dispatch
-    //     );
-    //     const [firstPlayerDetails, secondPlayerDetails] = setPlayersDetails(
-    //       user,
-    //       rivalPlayer,
-    //       dispatch
-    //     );
-    //     setDataToOtherPlayer(
-    //       firstPlayerDetails,
-    //       secondPlayerDetails,
-    //       rivalPlayer,
-    //       user.gameDetails.socketId,
-    //       getCategories,
-    //       dispatch
-    //     );
-    //   })();
-    // }
-  }, [setStartGameDetails]);
 
   return (
     <>
       {!readyToPlay ? (
-        <div className="flex-1 flex flex-col px-4 bg-[url(room.png)] bg-cover bg-center">
+        <div className="flex-1 flex flex-col bg-[url(room.png)] bg-cover bg-center">
           {isNewGameStarted ? (
             <LoadingGame />
           ) : (
