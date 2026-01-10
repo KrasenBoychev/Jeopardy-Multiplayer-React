0d8c72ee34523a6cd7f0387479b6cc8f9724cd11
Krasen Boychev <krasenboychev11@gmail.com>
2026-01-10 16:58:37 +0200

    new basic and game listeners added; old listeners deleted

--- client/src/features/socket_connection/socket hooks/useGameListeners.js
+++ client/src/features/socket_connection/socket hooks/useGameListeners.js
@@ -1,9 +1,10 @@
 import { useEffect } from "react";
 import {
+  setCategories,
   updateCategoryCount,
   updateGameCategories,
 } from "../../game/03. categories/categoriesSlice";
-import { updateActivePlayer } from "../../game/playersSlice";
+import { setPlayersAndRoom, updateActivePlayer } from "../../game/playersSlice";
 import { useDispatch } from "react-redux";
 import {
   setQuestions,
@@ -13,7 +14,11 @@ import {
   setAnswerChosen,
   updateIsAnswerCorrect,
 } from "../../game/05. answers/answerSlice";
-import { setPlayers } from "../../game/gameSlice";
+import {
+  updateIsNewGameStarted,
+  updateReadyToPlay,
+} from "../../game/gameSlice";
+import toast from "react-hot-toast";
 
 export default function useGameListeners(socket) {
   const dispatch = useDispatch();
@@ -45,20 +50,38 @@ export default function useGameListeners(socket) {
       dispatch(updateIsAnswerCorrect(setIsAnswerCorrect));
     };
 
+    const handleGameStarted = ({ roomId, players }) => {
+      dispatch(setPlayersAndRoom({ ...players, roomId }));
+      dispatch(updateIsNewGameStarted());
+    };
+
+    const handleOpponentLeft = () => {
+      toast.error("Your opponent has left the game.");
+    };
+
+    const handleOpponentDisconnected = () => {
+      toast.error("Your opponent has disconnected from the game.");
+      console.log("Opponent disconnected");
+    };
+
+    const handleSetGameCategories = ({ allCategories }) => {
+      dispatch(setCategories(allCategories));
+      dispatch(updateReadyToPlay());
+    };
+
+    socket.on("game_started", handleGameStarted);
+    socket.on("opponent_left", handleOpponentLeft);
+    socket.on("opponent_disconnected", handleOpponentDisconnected);
+    socket.on("get_categories", handleSetGameCategories);
     socket.on("getCategorySelected", handleGetCategorySelected);
     socket.on("getQuestionsSelected", handleGetQuestionsSelected);
     socket.on("getQuestionChosen", handleGetQuestionChosen);
     socket.on("getAnswerChosen", handleGetAnswerChosen);
-    socket.on("game_started", (data) => {
-      console.log(`Joined room: ${data.roomId}`);
-      // Navigate to /game/:roomId
-    });
-
-    socket.on("user_list_update", (updatedPlayers) => {
-      dispatch(setPlayers(updatedPlayers)); // Users will now have a 'status' property
-    });
 
     return () => {
+      socket.off("game_started", handleGameStarted);
+      socket.off("opponent_disconnected", handleOpponentDisconnected);
+      socket.off("get_categories", handleSetGameCategories);
       socket.off("getCategorySelected", handleGetCategorySelected);
       socket.off("getQuestionsSelected", handleGetQuestionsSelected);
       socket.off("getQuestionChosen", handleGetQuestionChosen);
--- client/src/features/socket_connection/socket hooks/useListeners.js
+++ client/src/features/socket_connection/socket hooks/useListeners.js
@@ -2,23 +2,17 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import toast from "react-hot-toast";
 import { useGetNotificationsQuery } from "../../../features/notifications/notificationsApiSlice";
-import { selectCurrentUser } from "../../authentication/authSlice";
 import {
   updateGameReqSentBy,
-  updateIsNewGameStarted,
-  updateReadyToPlay,
-  updateSetStartGameDetails,
+  setActiveFriends,
+  selectIsNewGameStarted,
 } from "../../game/gameSlice";
-import {
-  updateRivalPlayer,
-  setFirstSecondActivePlayer,
-} from "../../game/playersSlice";
-import { setSocketReq } from "../socketSlice";
-import { setCategories } from "../../game/03. categories/categoriesSlice";
+import { setRivalPlayer } from "../../game/playersSlice";
 import { useGetFriendsListQuery } from "../../game/01. play_page/friends_list/friendsApiSlice";
 
 export default function useListeners(socket) {
-  const user = useSelector(selectCurrentUser);
+  const isNewGameStarted = useSelector(selectIsNewGameStarted);
+
   const dispatch = useDispatch();
   const { refetch: refetchNotifications } =
     useGetNotificationsQuery("getNotifications");
@@ -26,30 +20,10 @@ export default function useListeners(socket) {
     useGetFriendsListQuery("getFriendsList");
 
   useEffect(() => {
-    if (!socket || user?.gameDetails?.gameInProgress) return;
+    if (!socket || isNewGameStarted) return;
 
-    const handleGetFriendStatus = ({ senderInfo }) => {
-      // dispatch(updateFriendStatus(senderInfo));
-
-      dispatch(
-        updateGameReqSentBy({
-          username: senderInfo.username,
-          updateType: "remove",
-        })
-      );
-
-      dispatch(
-        updateRivalPlayer({
-          username: senderInfo.username,
-          socketId: "",
-          updateType: "remove",
-        })
-      );
-    };
-
-    const handleGetExitUserStatus = ({ senderInfo }) => {
-      // dispatch(updateFriendStatus(senderInfo));
-      // dispatch(updateFriendGameInProgress(senderInfo.username));
+    const handleUserListUpdate = (updatedFriends) => {
+      dispatch(setActiveFriends(updatedFriends));
     };
 
     const handleGetUpdateNotifications = () => {
@@ -66,9 +40,7 @@ export default function useListeners(socket) {
     };
 
     const handleReceiveRejectGameRes = ({ username }) => {
-      dispatch(
-        updateRivalPlayer({ username, socketId: "", updateType: "remove" })
-      );
+      dispatch(setRivalPlayer(null));
       toast.error(username + " rejected your game request");
     };
 
@@ -76,80 +48,20 @@ export default function useListeners(socket) {
       dispatch(updateGameReqSentBy({ username, updateType: "remove" }));
     };
 
-    const handleGetAcceptGameRes = () => {
-      dispatch(updateIsNewGameStarted());
-      dispatch(updateSetStartGameDetails());
-    };
-
-    const handleGetFriendGameInProgress = ({ username }) => {
-      // dispatch(updateFriendGameInProgress(username));
-    };
-
-    const handleGetGameDetails = ({ gameDetails }) => {
-      dispatch(
-        setFirstSecondActivePlayer({
-          firstPlayerDetails: gameDetails.firstPlayerDetails,
-          secondPlayerDetails: gameDetails.secondPlayerDetails,
-        })
-      );
-
-      setTimeout(() => {
-        dispatch(updateReadyToPlay());
-        dispatch(setCategories(gameDetails.allCategories));
-        dispatch(
-          setSocketReq({
-            socketReqName: "setReadyToPlay",
-            socketData: {
-              receiverSocketId: gameDetails.userSocketId,
-            },
-          })
-        );
-      }, 2000);
-    };
-
-    const handleGetReadyToPlay = () => {
-      dispatch(updateReadyToPlay());
-    };
-
-    const handleGetExitGame = async ({ username }) => {
-      toast.error(username + " exit the game. Press 'EXIT' to leave");
-      dispatch(
-        updateRivalPlayer({ username, socketId: "", updateType: "remove" })
-      );
-    };
-
-    const handleOfflineUser = (data) => {
-      console.log(data);
-    };
-
-    socket.on("getFriendStatus", handleGetFriendStatus);
-    socket.on("getExitUserStatus", handleGetExitUserStatus);
-    socket.on("getUpdateNotifications", handleGetUpdateNotifications);
-    socket.on("getFriendReqAccepted", handleGetFriendReqAccepted);
-    socket.on("receiveGameReq", handleReceiveGameReq);
-    socket.on("receiveRejectGameRes", handleReceiveRejectGameRes);
-    socket.on("getCancelGameInvitation", handleGetCancelGameInvitation);
-    socket.on("getAcceptGameRes", handleGetAcceptGameRes);
-    socket.on("getFriendGameInProgress", handleGetFriendGameInProgress);
-    socket.on("getGameDetails", handleGetGameDetails);
-    socket.on("getReadyToPlay", handleGetReadyToPlay);
-    socket.on("getExitGame", handleGetExitGame);
-    socket.on("user_left", handleOfflineUser);
+    socket.on("user_list_update", handleUserListUpdate);
+    socket.on("get_update_notifications", handleGetUpdateNotifications);
+    socket.on("get_friend_req_accepted", handleGetFriendReqAccepted);
+    socket.on("receive_game_req", handleReceiveGameReq);
+    socket.on("receive_reject_game_res", handleReceiveRejectGameRes);
+    socket.on("get_cancel_game_invitation", handleGetCancelGameInvitation);
 
     return () => {
-      socket.off("getFriendStatus", handleGetFriendStatus);
-      socket.off("getExitUserStatus", handleGetExitUserStatus);
-      socket.off("getUpdateNotifications", handleGetUpdateNotifications);
-      socket.off("getFriendReqAccepted", handleGetFriendReqAccepted);
-      socket.off("receiveGameReq", handleReceiveGameReq);
-      socket.off("receiveRejectGameRes", handleReceiveRejectGameRes);
-      socket.off("getCancelGameInvitation", handleGetCancelGameInvitation);
-      socket.off("getAcceptGameRes", handleGetAcceptGameRes);
-      socket.off("getFriendGameInProgress", handleGetFriendGameInProgress);
-      socket.off("getGameDetails", handleGetGameDetails);
-      socket.off("getReadyToPlay", handleGetReadyToPlay);
-      socket.off("getExitGame", handleGetExitGame);
-      socket.off("user_left");
+      socket.off("user_list_update", handleUserListUpdate);
+      socket.off("get_update_notifications", handleGetUpdateNotifications);
+      socket.off("get_friend_req_accepted", handleGetFriendReqAccepted);
+      socket.off("receive_game_req", handleReceiveGameReq);
+      socket.off("receive_reject_game_res", handleReceiveRejectGameRes);
+      socket.off("get_cancel_game_invitation", handleGetCancelGameInvitation);
     };
   }, [socket]);
 }
