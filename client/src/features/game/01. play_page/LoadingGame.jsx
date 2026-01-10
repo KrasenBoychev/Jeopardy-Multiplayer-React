68352a36f0bf583dee358b2e38bef8783d9dc711
Krasen Boychev <krasenboychev11@gmail.com>
2026-01-10 17:00:12 +0200

    LoadingGame.jsx is responsible for the categories fetching

--- client/src/features/game/01. play_page/LoadingGame.jsx
+++ client/src/features/game/01. play_page/LoadingGame.jsx
@@ -1,14 +1,64 @@
-import { Button } from "@/components/ui/moving-border";
+import { ColourfulText } from "@/components/ui/colourful-text";
+import { useGetCategoriesQuery } from "../gameApiSlice";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setSocketReq } from "../../socket_connection/socketSlice";
+import { deletePlayersDetails, selectRoomId } from "../playersSlice";
+import toast from "react-hot-toast";
+import { deleteGameDetails } from "../gameSlice";
 
 export function LoadingGame() {
+  const roomId = useSelector(selectRoomId);
+  const dispatch = useDispatch();
+  const colors = ["rgba(37, 234, 30, 0.73)"];
+
+  console.log(roomId);
+
+  const {
+    data: allCategories,
+    isSuccess,
+    isError,
+  } = useGetCategoriesQuery("getCategories");
+
+  useEffect(() => {
+    // if (isSuccess) {
+    //   dispatch(
+    //     setSocketReq({
+    //       socketReqName: "send_categories",
+    //       socketData: {
+    //         roomId,
+    //         allCategories,
+    //       },
+    //     })
+    //   );
+    // }
+
+    if (isError) {
+      toast.error("Starting the game failed!");
+
+      dispatch(
+        setSocketReq({
+          socketReqName: "leave_game",
+          socketData: {
+            roomId,
+          },
+        })
+      );
+      dispatch(deletePlayersDetails());
+      dispatch(deleteGameDetails());
+    }
+  }, [isSuccess, isError]);
+
   return (
-    <div>
-      <Button
-        borderRadius="1.75rem"
-        className="bg-white text-xl uppercase dark:bg-slate-900 text-black dark:text-white border-neutral-200 dark:border-slate-800"
-      >
-        Loading Game
-      </Button>
+    <div className="flex-1 flex flex-col uppercase items-center justify-center flex font-bold text-[50px] bg-[#00000095]">
+      <div className="flex-1"></div>
+      <div className="flex-1 flex items-center justify-center">
+        <ColourfulText
+          text="Loading Game..."
+          colors={colors}
+          className="tracking-widest"
+        />
+      </div>
     </div>
   );
 }
