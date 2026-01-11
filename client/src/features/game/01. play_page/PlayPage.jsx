31c3e9c359eff5039b9dbca8d119212fef5c20dc
Krasen Boychev <krasenboychev11@gmail.com>
2026-01-11 23:07:43 +0200

    PlayPage renders StartingPlayer when the categories are set

--- client/src/features/game/01. play_page/PlayPage.jsx
+++ client/src/features/game/01. play_page/PlayPage.jsx
@@ -1,17 +1,18 @@
 import { useSelector } from "react-redux";
-import { selectIsNewGameStarted, selectReadyToPlay } from "../gameSlice";
+import { selectIsNewGameStarted } from "../gameSlice";
 import { LoadingGame } from "./LoadingGame";
 import StartingPlayer from "../02. starting_player/StartingPlayer";
 import FriendsList from "./friends_list/FriendsList";
 import { GameCard } from "./game_room/GameCard";
+import { selectCategories } from "../03. categories/categoriesSlice";
 
 export default function PlayPage() {
-  const readyToPlay = useSelector(selectReadyToPlay);
+  const categories = useSelector(selectCategories);
   const isNewGameStarted = useSelector(selectIsNewGameStarted);
 
   return (
     <>
-      {!readyToPlay ? (
+      {!categories ? (
         <div className="flex-1 flex flex-col bg-[url(room.png)] bg-cover bg-center">
           {isNewGameStarted ? (
             <LoadingGame />
