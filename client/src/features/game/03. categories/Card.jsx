7ccc24b5b943cdf111b21b0773ad0f64a00d8350
Krasen Boychev <krasenboychev11@gmail.com>
2026-01-11 23:05:43 +0200

    focus-card reusable component renamed to Card and moved to categories folder because it's not used anywhere else

---
+++ client/src/features/game/03. categories/Card.jsx
@@ -0,0 +1,44 @@
+"use client";
+import React, { useState } from "react";
+import { cn } from "@/lib/utils";
+
+export default function Card({
+  index,
+  gameCategory,
+  categoryCount,
+  user,
+  activePlayer,
+  PopUp,
+}) {
+  return (
+    <div
+      className={cn(
+        "rounded-lg relative bg-gray-100 dark:bg-neutral-900 overflow-hidden h-40 md:h-40 w-full transition-all duration-300 ease-out",
+        (index > categoryCount ||
+          (index == categoryCount &&
+            user.username !== activePlayer.username)) &&
+          "blur-sm scale-[0.98]"
+      )}
+    >
+      <div
+        className={cn(
+          "absolute inset-0 bg-white flex items-center justify-center py-8 px-4 transition-opacity duration-300 uppercase",
+          index <= categoryCount ? "opacity-100" : "opacity-0"
+        )}
+      >
+        <div
+          className={cn(
+            "text-xl md:text-2xl font-medium bg-clip-text text-black bg-gradient-to-b from-neutral-50 to-neutral-200 uppercase",
+            index == categoryCount &&
+              user.username === activePlayer.username &&
+              "cursor-pointer"
+          )}
+        >
+          {index == categoryCount && user.username === activePlayer.username
+            ? PopUp
+            : gameCategory.name}
+        </div>
+      </div>
+    </div>
+  );
+}
