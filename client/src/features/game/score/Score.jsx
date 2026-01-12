5c2c9f15d00ac021029b3f0321b879211ad6726c
Krasen Boychev <krasenboychev11@gmail.com>
2026-01-12 23:11:16 +0200

    Score has updated design and doesn't use external css files, but tailwind css instead

--- client/src/features/game/score/Score.jsx
+++ client/src/features/game/score/Score.jsx
@@ -1,6 +1,5 @@
 import { useSelector } from "react-redux";
 import { selectFirstPlayer, selectSecondPlayer } from "../playersSlice";
-import "../game.css";
 
 export default function Score() {
   const firstPlayer = useSelector(selectFirstPlayer);
@@ -8,16 +7,20 @@ export default function Score() {
 
   return (
     <section>
-      <div className="players_points_wrapper">
-        <h3>Score</h3>
-        <div className="players_points">
-          <p className="points_first_player">
+      <div className="absolute top-5 left-5 w-fit h-fit flex flex-col items-center gap-4 p-5 text-[15px] border border-solid border-white rounded-md shadow-[inset_0_0_13px_orange] max-[1800px]:text-[14px] max-[1600px]:text-[13px] max-[1400px]:text-[12px]">
+        <h3 className="font-bold uppercase text-chart-5">Score</h3>
+        <div className="flex flex-col gap-3">
+          <p className="flex gap-4 justify-between bg-chart-5 p-2 rounded-sm">
             <span>{firstPlayer.username}</span>
-            <span>{firstPlayer.earnedPoints} points</span>
+            <span className="text-black">
+              {firstPlayer.earnedPoints} points
+            </span>
           </p>
-          <p className="points_second_player">
+          <p className="flex gap-4 justify-between bg-chart-5 p-2 rounded-sm">
             <span>{secondPlayer.username}</span>
-            <span>{secondPlayer.earnedPoints} points</span>
+            <span className="text-black">
+              {secondPlayer.earnedPoints} points
+            </span>
           </p>
         </div>
       </div>
