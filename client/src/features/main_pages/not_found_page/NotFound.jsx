0b2a953468b3a14610756eda52874ac979b2cb0c
Krasen Boychev <krasenboychev11@gmail.com>
2026-01-16 17:15:33 +0200

    not found page uses tailwind css instead external css file

--- client/src/features/main_pages/not_found_page/NotFound.jsx
+++ client/src/features/main_pages/not_found_page/NotFound.jsx
@@ -1,13 +1,14 @@
-import "./notFound.css";
 import { Link } from "react-router-dom";
 
 export default function NotFound() {
   return (
-    <div className="not-found-wrapper">
-      <p className="not-found-msg">Page not found</p>
-      <p className="not-found-get-back">
+    <div className="m-auto flex flex-col items-center gap-10">
+      <p className="text-[50px] font-bold uppercase [word-spacing:0.5cm] [letter-spacing: 0.1cm]">
+        Page not found
+      </p>
+      <p className="text-[30px]">
         Get back to{" "}
-        <Link to="/" className="not-found-link">
+        <Link to="/" className="underline cursor-pointer hover:text-chart-2">
           Home Page
         </Link>
       </p>
