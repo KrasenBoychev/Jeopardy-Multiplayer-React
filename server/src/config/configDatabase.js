ab1bb06bd01b8369ddef6c7178523ea408e79f69
Krasen Boychev <krasenboychev11@gmail.com>
2026-01-26 21:07:44 +0200

    mongodb cluster created

--- server/src/config/configDatabase.js
+++ server/src/config/configDatabase.js
@@ -1,13 +1,16 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 async function connectDatabase() {
   await mongoose
-    .connect('mongodb://localhost:27017/', {
-      dbName: 'Jeopardy-Multiplayer'
-    })
+    .connect(
+      "mongodb+srv://krasenboychev11_db_user:snBHKcrezslhqroY@cluster0.qr9biv1.mongodb.net/?appName=Cluster0",
+      {
+        dbName: "Jeopardy-Multiplayer",
+      },
+    )
     // eslint-disable-next-line no-unused-vars
     .then((res) => {
-      console.log('Connected to your database');
+      console.log("Connected to your database");
     })
     .catch((err) => {
       console.log(err);
